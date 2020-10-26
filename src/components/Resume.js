edb3f97190629710d72f234b1aa5ac7fc9dae3a0
Kevin Ung <ung.kevin78@gmail.com>
2020-10-25 20:50:25 -0600

    Update resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import bako from '../assets/bako.jpg';
-const id = '1_MzAF6uXKsBsqrPZOjxg44XXDQ08oS7M';
+const id = '1bDCHy2UFCUI_oy3_tSp1OwubXwv7srba';
 const embed = `https://drive.google.com/file/d/${id}/preview`;
 const open = `https://drive.google.com/file/d/${id}/view?usp=sharing`;
 const download = `https://drive.google.com/uc?export=download&id=${id}`;
