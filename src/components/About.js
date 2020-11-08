6c794245133595cc4c26a1b87fa12258f9c538af
Kevin Ung <ung.kevin78@gmail.com>
2020-11-08 02:00:19 -0700

    Fixing grammar

--- src/components/About.js
+++ src/components/About.js
@@ -55,7 +55,7 @@ const About = (props) => {
         <div className="about-page flex" style={{ backgroundImage: `url(${bako})` }}>
             <div className="about-inner-container justify-around set-top align-ctr">
                 <p className="about-me-summary">
-                    My name is Kevin Ung. I am a software engineer. I love serenading my neighborhood, watching Disney+, learning about various subjects, the sense of accomplishment after a good run or bike ride, cooking/baking a new delicious recipe, and casual tosses of Frisbee or Lacrosse.
+                    My name is Kevin Ung. I am a software engineer. I love serenading my neighborhood, watching Disney+, learning about various subjects, the sense of accomplishment after a good run or bike ride, cooking/baking a new delicious recipe, and casual tosses of a Frisbee or a Lacrosse ball.
                 </p>
                 <div className="about-me-container wrap">
                     <div onClick={() => history.push('/projects')} className="icon-container pointer">
