766dcc39d0948165ca2af09abc8774433aaed1f5
Kevin Ung <ung.kevin78@gmail.com>
2023-05-21 15:23:26 -0600

    fix resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import japan from '../Assets/japan.jpeg';
-const id = '1bDCHy2UFCUI_oy3_tSp1OwubXwv7srba';
+const id = '1jMAfdIYWomKJf6khY9Eq-3pVWVmoXH82';
 const embed = `https://drive.google.com/file/d/${id}/preview`;
 const open = `https://drive.google.com/file/d/${id}/view?usp=sharing`;
 const download = `https://drive.google.com/uc?export=download&id=${id}`;
