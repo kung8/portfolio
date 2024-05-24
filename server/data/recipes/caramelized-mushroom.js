6dd3e3afd1742b1bd3824feab8b84b66f3587363
Kevin Ung <ung.kevin78@gmail.com>
2024-05-23 20:59:43 -0600

    add caramelized onion and mushroom images

--- server/data/recipes/caramelized-mushroom.js
+++ server/data/recipes/caramelized-mushroom.js
@@ -1,10 +1,13 @@
-// const cilantroLimeChicken = '../assets/Products/cilantro-lime-chicken.jpeg';
+const combined = '../assets/Products/caramelized-mushroom-combined.jpeg';
+const cooked = '../assets/Products/caramelized-mushroom-cooked.jpeg';
+const cooking = '../assets/Products/caramelized-mushroom-cooking.jpeg';
+const raw = '../assets/Products/caramelized-mushroom-raw.jpeg';
 const { CATEGORIES, SECTIONS, GENRES, METHODS } = require("./constants");
 
 module.exports = {
     cardName: 'Caramelized Mushroom',
     name: 'Caramelized Mushroom',
-    // img: cilantroLimeChicken,
+    img: cooked,
     available: true,
     category: [CATEGORIES.TOPPING],
     genre: [GENRES.AMERICAN, GENRES.WESTERN],
@@ -29,10 +32,10 @@ module.exports = {
     ],
     directions: [
         { step: 'Over medium-high heat, melt butter in a pan.', type: SECTIONS.TOPPING },
-        { step: 'Add the mushroom and let it brown.', type: SECTIONS.TOPPING },
+        { step: 'Add the mushroom and let it brown.', type: SECTIONS.TOPPING, img: raw },
         { step: 'Add the brown sugar, balsamic vinegar, and soy sauce when the mushroom begins to brown.', type: SECTIONS.TOPPING },
-        { step: 'Stir and cook for about 3 minutes.', type: SECTIONS.TOPPING },
+        { step: 'Stir and cook for about 3 minutes.', type: SECTIONS.TOPPING, img: cooking },
         { step: 'Season with salt and pepper and cook until mushrooms are browned and soft.', type: SECTIONS.TOPPING },
-        { step: 'Serve as a topping for pizza, burgers, or meat.', type: SECTIONS.TOPPING },
+        { step: 'Serve as a topping for pizza, burgers, or meat.', type: SECTIONS.TOPPING, img: combined },
     ]
 };
--- server/data/recipes/caramelized-onion.js
+++ server/data/recipes/caramelized-onion.js
@@ -1,10 +1,14 @@
-// const cilantroLimeChicken = '../assets/Products/cilantro-lime-chicken.jpeg';
+const buttered = '../assets/Products/caramelized-onion-buttered-onion.jpeg';
+const caramelized = '../assets/Products/caramelized-onion-caramelized-cooked.jpeg';
+const combined = '../assets/Products/caramelized-onion-combined.jpeg';
+const cooked = '../assets/Products/caramelized-onion-cooked.jpeg';
+const sliced = '../assets/Products/caramelized-onion-thinly-sliced.jpeg';
 const { CATEGORIES, SECTIONS, GENRES, METHODS } = require("./constants");
 
 module.exports = {
     cardName: 'Caramelized Onion',
     name: 'Caramelized Onion',
-    // img: cilantroLimeChicken,
+    img: caramelized,
     available: true,
     category: [CATEGORIES.TOPPING],
     genre: [GENRES.AMERICAN, GENRES.WESTERN],
@@ -28,11 +32,11 @@ module.exports = {
         { name: 'frying pan' },
     ],
     directions: [
-        { step: 'Over medium-high heat, melt butter in a pan.', type: SECTIONS.TOPPING },
-        { step: 'Add the red onions and let it brown.', type: SECTIONS.TOPPING },
+        { step: 'Over medium-high heat, melt butter in a pan.', type: SECTIONS.TOPPING, img: sliced },
+        { step: 'Add the red onions and let it brown.', type: SECTIONS.TOPPING, img: buttered },
         { step: 'Add the brown sugar, balsamic vinegar, and soy sauce when the onions begins to brown.', type: SECTIONS.TOPPING },
         { step: 'Stir and cook for about 3 minutes.', type: SECTIONS.TOPPING },
-        { step: 'Season with salt and pepper and cook until onions are browned and soft.', type: SECTIONS.TOPPING },
-        { step: 'Serve as a topping for pizza, burgers, or meat.', type: SECTIONS.TOPPING },
+        { step: 'Season with salt and pepper and cook until onions are browned and soft.', type: SECTIONS.TOPPING, img: cooked },
+        { step: 'Serve as a topping for pizza, burgers, or meat.', type: SECTIONS.TOPPING, img: combined },
     ]
 };
