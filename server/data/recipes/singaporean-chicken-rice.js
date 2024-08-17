a5cb649a68d2825075bf418c45501e5035c0ef21
Kevin Ung <ung.kevin78@gmail.com>
2024-08-17 15:35:04 -0600

    add singaporean chicken rice images

--- server/data/recipes/singaporean-chicken-rice.js
+++ server/data/recipes/singaporean-chicken-rice.js
@@ -1,9 +1,12 @@
 const aromatics = '../assets/Products/singaporean-chicken-rice-aromatics.jpeg';
 const broth = '../assets/Products/singaporean-chicken-rice-broth.jpeg';
+const chicken = '../assets/Products/singaporean-chicken-rice-chicken-in-broth.jpeg';
+const mixed = '../assets/Products/singaporean-chicken-rice-cooked-mixed.jpeg';
+const mixed2 = '../assets/Products/singaporean-chicken-rice-cooked-mixed-2.jpeg';
+const lid = '../assets/Products/singaporean-chicken-rice-lid.jpeg';
 const chili = '../assets/Products/singaporean-chicken-rice-chili-sauce.jpeg';
 const cooked = '../assets/Products/singaporean-chicken-rice-cooked.jpeg';
 const plated = '../assets/Products/singaporean-chicken-rice-plated.jpeg';
-const raw = '../assets/Products/singaporean-chicken-rice-raw-pot.jpeg';
 const { GENRES, METHODS, PROTEIN, SECTIONS, TYPES, CATEGORIES } = require('./constants');
 
 const CHICKEN = 'Chicken';
@@ -42,6 +45,7 @@ module.exports = {
         { name: 'lime juice', amount: '1 Tbsp', additionalDetails: '', section: CHILI_SAUCE },
         { name: 'sugar', amount: '2 tsp', additionalDetails: '', section: CHILI_SAUCE },
         { name: 'cucumber', amount: '1', additionalDetails: 'sliced', section: SECTIONS.TOPPINGS },
+        { name: 'tomatoes', amount: '1', additionalDetails: 'sliced', section: SECTIONS.TOPPINGS },
     ],
     appliances: [
         { name: 'stove' },
@@ -57,10 +61,10 @@ module.exports = {
         { step: 'Wash the rice.', type: SECTIONS.RICE },
         { step: 'Prep the garlic, ginger, and green onion.', type: SECTIONS.RICE, img: aromatics },
         { step: 'Place the washed rice, garlic, ginger, green onion, chicken broth, and turmeric in a pot.', type: SECTIONS.RICE, img: broth },
-        { step: 'Place the chicken thigh on top and cover with a lid.', type: SECTIONS.RICE, img: raw },
-        { step: 'Cook until the chicken and rice is cooked (about 15 to 25 minutes).', type: SECTIONS.RICE, img: cooked },
+        { step: 'Place the chicken thigh on top and cover with a lid.', type: SECTIONS.RICE, img: [chicken, lid] },
+        { step: 'Cook until the chicken and rice is cooked (about 15 to 25 minutes).', type: SECTIONS.RICE, img: [cooked, mixed, mixed2] },
+        { step: 'Cut the chicken into slices. If the chicken is still pink/raw, I recommend placing the sliced chicken in a pan filled with a layer of water and cover with a lid. Cook the chicken for 5 or so minutes over medium-high heat.', type: SECTIONS.RICE },
         { step: 'Mix together garlic, ginger, sriracha, chicken stock, lime juice, and sugar in a bowl.', type: CHILI_SAUCE, img: chili },
-        { step: 'Cut the chicken into slices.', type: SECTIONS.SERVE },
-        { step: 'Serve the sliced chicken with rice and sliced cucumbers. Top with chili sauce.', type: SECTIONS.SERVE },
+        { step: 'Serve this dish with cucumbers and tomatoes. Top with chili sauce.', type: SECTIONS.SERVE },
     ]
 };
