857e731d0203b1079e8bf2fadf0454d3ba609959
Kevin Ung <ung.kevin78@gmail.com>
2024-08-31 02:47:24 -0600

    fix category name

--- src/components/recipes/use-category-name.js
+++ src/components/recipes/use-category-name.js
@@ -14,18 +14,27 @@ export const useCategoryName = () => {
             const ingredientIndices = ingredient.name.toLowerCase().split(' ');
             const categoryIndices = ingredient.category.toLowerCase().split(' ').map(c => c.replace(/,/g, ''));
             
-            return ingredientIndices.find(ingredient => searchIndices.includes(ingredient)) ||
-                searchIndices.find(search => ingredientIndices.includes(search)) ||
-                categoryIndices.find(category => searchIndices.find(s => s ===category)) ||
-                searchIndices.find(search => categoryIndices.find(c => c === search));
+            return ingredientIndices.find(ingredient => searchIndices.find(s => s.includes(ingredient))) ||
+                searchIndices.find(search => ingredientIndices.find(i => i.includes(search))) ||
+                categoryIndices.find(category => searchIndices.find(s => s.includes(category))) ||
+                searchIndices.find(search => categoryIndices.find(c => c.includes(search)));
         });
 
         let category = 'Other';
         if (matchingIngredients.find(ingredient => ingredient.name.toLowerCase() === search.toLowerCase())) {
+            // HIT THE EXACT MATCH
             category = matchingIngredients.find(ingredient => ingredient.name.toLowerCase() === search.toLowerCase()).category;
+        } else if (matchingIngredients.find(ingredient => ingredient.name.toLowerCase().includes(search.toLowerCase()))) {
+            // SEARCH IS INCLUDED IN INGREDIENTS (LIKE 'SALT' IN 'SEA SALT');
+            category = matchingIngredients.find(ingredient => ingredient.name.toLowerCase().includes(search.toLowerCase())).category;
+        } else if (matchingIngredients.find(ingredient => search.toLowerCase().includes(ingredient.name.toLowerCase()))) {
+            // INGREDIENT IS INCLUDED IN SEARCH;
+            category = matchingIngredients.find(ingredient => search.toLowerCase().includes(ingredient.name.toLowerCase())).category;
         } else if (matchingIngredients.length === 1) {
+            // SINGLE INGREDIENT
             category = matchingIngredients[0].category;
         } else if (matchingIngredients.length > 1) {
+            // MULTIPLE INGREDIENTS
             const categories = matchingIngredients.map(ingredient => ingredient.category);
             // logic to see if there's only one category type
             const uniqueCategories = [...new Set(categories)];
