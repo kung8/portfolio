bb2de1c5136af0a9654779a83d1ab43d7a765bb9
Kevin Ung <ung.kevin78@gmail.com>
2024-09-11 19:34:05 -0600

    add lemon chicken images

--- server/data/recipes/lemon-chicken-on-lettuce.js
+++ server/data/recipes/lemon-chicken-on-lettuce.js
@@ -1,4 +1,8 @@
-const lemonChickenOnLettuce = '../assets/Products/lemon-chicken-on-lettuce.jpeg';
+const cooked = '../assets/Products/lemon-chicken-on-lettuce-cooked.jpeg';
+const frying = '../assets/Products/lemon-chicken-on-lettuce-frying-chicken.jpeg';
+const partially = '../assets/Products/lemon-chicken-on-lettuce-partially-cooked.jpeg';
+const plated = '../assets/Products/lemon-chicken-on-lettuce-plated.jpeg';
+const raw = '../assets/Products/lemon-chicken-on-lettuce-raw-chicken.jpeg';
 const { CATEGORIES, GENRES, SECTIONS, TYPES, METHODS, PROTEIN } = require("./constants");
 const {
     VEGETABLE_OIL,
@@ -15,7 +19,7 @@ const {
 module.exports = {
     cardName: 'Lettuce Chicken',
     name: 'Lemon Chicken on Lettuce',
-    img: lemonChickenOnLettuce,
+    img: plated,
     available: true,
     recommended: true,
     category: [CATEGORIES.LUNCH, CATEGORIES.DINNER],
@@ -46,10 +50,10 @@ module.exports = {
     directions: [
         { step: 'Heat a wok over medium-high heat.', type: SECTIONS.MAIN },
         { step: 'Add vegetable oil and wait until it is hot.', type: SECTIONS.MAIN },
-        { step: 'Add chicken to the wok and let it cook without flipping (about 1 to 2 minutes).', type: SECTIONS.MAIN },
-        { step: 'Then flip and cook again for a few minutes.', type: SECTIONS.MAIN },
+        { step: 'Add chicken to the wok and let it cook without flipping (about 1 to 2 minutes).', type: SECTIONS.MAIN, img: [raw, frying] },
+        { step: 'Then flip and cook again for a few minutes.', type: SECTIONS.MAIN, img: partially },
         { step: 'Season the chicken with salt and pepper.', type: SECTIONS.MAIN },
-        { step: 'Add soy sauce, water, lemon juice and cornstarch to the chicken.', type: SECTIONS.MAIN },
+        { step: 'Add soy sauce, water, lemon juice and cornstarch to the chicken.', type: SECTIONS.MAIN, img: cooked },
         { step: 'The cornstarch should thicken the sauce slightly. Add more cornstarch to continuing thickening as preferred.', type: SECTIONS.MAIN },
         { step: 'Serve the lemon chicken over leaves of lettuce with a side of rice.', type: SECTIONS.MAIN },
     ]
