d05771b67db36de3de72e9a44acfc19e09355e33
Kevin Ung <ung.kevin78@gmail.com>
2024-09-23 19:44:07 -0600

    add green bean stir fry

--- server/data/recipes/chinese-green-bean-beef.js
+++ server/data/recipes/chinese-green-bean-beef.js
@@ -5,6 +5,10 @@ const {
     BEEF_CHUCK_ROAST,
     SALT,
     BLACK_PEPPER,
+    OLIVE_OIL,
+    GARLIC,
+    COLD_WATER,
+    LIGHT_SOY_SAUCE,
 } = require('./ingredients');
 
 module.exports = {
@@ -20,11 +24,18 @@ module.exports = {
     yields: '4 servings',
     prepTime: '15 m',
     cookTime: '20 m',
+    websites: [
+        "https://barefeetinthekitchen.com/thai-steak-and-green-bean-stir-fry/"
+    ],
     ingredients: [
+        { ...OLIVE_OIL, amount: '', additionalDetails: '', optional: false },
+        { ...BEEF_CHUCK_ROAST, amount: '1 lb', additionalDetails: 'thin strips', optional: false },
         { ...GREEN_BEANS, amount: '24 oz', additionalDetails: 'cut to 2" pieces', optional: false },
-        { ...BEEF_CHUCK_ROAST, amount: '1 lb', additionalDetails: 'lean', optional: false },
-        { ...SALT, amount: '', additionalDetails: '', optional: false },
-        { ...BLACK_PEPPER, amount: '', additionalDetails: '', optional: false },
+        { ...GARLIC, amount: '4 cloves', additionalDetails: 'minced', optional: false },
+        { ...COLD_WATER, amount: '2 Tbsp', additionalDetails: '', optional: false },
+        { ...LIGHT_SOY_SAUCE, amount: '2 Tbsp', additionalDetails: '', optional: false },
+        { ...SALT, amount: '', additionalDetails: 'to taste', optional: false },
+        { ...BLACK_PEPPER, amount: '', additionalDetails: 'to taste', optional: false },
     ],
     appliances: [
         { name: 'stove' },
@@ -32,13 +43,20 @@ module.exports = {
     supplies: [
         { name: 'cutting board' },
         { name: 'knife' },
-        { name: 'large frying pan' },
+        { name: 'wok' },
         { name: 'spatula' },
     ],
     directions: [
-        { step: 'Heat frying pan over medium-high heat, and add beef. Cook until browned.', type: SECTIONS.MAIN },
-        { step: 'Add the green beans into pan and continue to cook until beans are softened. Stir occasionally.', type: SECTIONS.MAIN },
-        { step: 'Add salt and pepper as desired.', type: SECTIONS.MAIN },
+        { step: 'Over medium-high heat, heat oil in a wok.', type: SECTIONS.MAIN },
+        { step: 'Add beef. Cook until browned (about 4 minutes).', type: SECTIONS.MAIN },
+        { step: 'Remove beef and set aside.', type: SECTIONS.MAIN },
+        { step: 'Add the green beans and garlic to the leftover juice in the wok. Give it a good toss.', type: SECTIONS.MAIN },
+        { step: 'Give the wok a good toss to help the vegetables combine.', type: SECTIONS.MAIN },
+        { step: 'Add water and cover the wok. Let it cook for 5 minutes.', type: SECTIONS.MAIN },
+        { step: 'Uncover and continue to toss the vegetables until green beans are crisp and tender (about 5 to 7 minutes).', type: SECTIONS.MAIN },
+        { step: 'Re-add the beef and soy sauce to the wok.', type: SECTIONS.MAIN },
+        { step: 'Toss everything together and cook for about a minute.', type: SECTIONS.MAIN },
+        { step: 'Season with salt and pepper as desired.', type: SECTIONS.MAIN },
         { step: 'Serve over rice and enjoy this Chinese stir-fry.', type: SECTIONS.MAIN },
     ]
 }
