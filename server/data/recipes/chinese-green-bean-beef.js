a58f080c1adc170c08eab943b7aabb18a290a790
Kevin Ung <ung.kevin78@gmail.com>
2024-09-27 22:14:36 -0600

    add green bean and beef images

--- server/data/recipes/chinese-green-bean-beef.js
+++ server/data/recipes/chinese-green-bean-beef.js
@@ -1,4 +1,17 @@
-const greenBeanWithBeef = '../assets/Products/green-bean-with-beef.jpeg';
+const chopped = '../assets/Products/chinese-green-bean-and-beef-chopped-green-beans.jpeg';
+const combinedZoomed1 = '../assets/Products/chinese-green-bean-and-beef-cooked-combined-zoomed-1.jpeg';
+const combinedZoomed2 = '../assets/Products/chinese-green-bean-and-beef-cooked-combined-zoomed-2.jpeg';
+const combined = '../assets/Products/chinese-green-bean-and-beef-cooked-combined.jpeg';
+const garlic = '../assets/Products/chinese-green-bean-and-beef-garlic.jpeg';
+const greenOnionPan = '../assets/Products/chinese-green-bean-and-beef-green-onions-in-pan.jpeg';
+const greenOnionsGarlic = '../assets/Products/chinese-green-bean-and-beef-green-onions-with-garlic.jpeg';
+const greenOnions = '../assets/Products/chinese-green-bean-and-beef-green-onions.jpeg';
+const plated1 = '../assets/Products/chinese-green-bean-and-beef-plated-1.jpeg';
+const plated2 = '../assets/Products/chinese-green-bean-and-beef-plated-2.jpeg';
+const beef = '../assets/Products/chinese-green-bean-and-beef-raw-beef.jpeg';
+const sauce = '../assets/Products/chinese-green-bean-and-beef-sauce.jpeg';
+const sautedBeans = '../assets/Products/chinese-green-bean-and-beef-sauted-green-beans.jpeg';
+const whole = '../assets/Products/chinese-green-bean-and-beef-whole-beans.jpeg';
 const { CATEGORIES, GENRES, SECTIONS, TYPES, METHODS, PROTEIN } = require('./constants');
 const {
     GREEN_BEANS,
@@ -8,7 +21,6 @@ const {
     GARLIC,
     LIGHT_SOY_SAUCE,
     GREEN_ONIONS,
-    GINGER,
     CHICKEN_BROTH,
     OYSTER_SAUCE,
     RICE_WINE_VINEGAR,
@@ -19,7 +31,7 @@ const {
 module.exports = {
     cardName: 'Green Bean Beef',
     name: 'Green Bean Beef',
-    img: greenBeanWithBeef,
+    img: combinedZoomed2,
     available: true,
     category: [CATEGORIES.LUNCH, CATEGORIES.DINNER],
     genre: [GENRES.ASIAN, GENRES.CHINESE],
@@ -39,15 +51,13 @@ module.exports = {
         { ...LIGHT_SOY_SAUCE, amount: '2 Tbsp', additionalDetails: '', optional: false, section: SECTIONS.SAUCE },
         { ...RICE_WINE_VINEGAR, amount: '2 tsp', additionalDetails: '', optional: false, section: SECTIONS.SAUCE },
         { ...RED_PEPPER_FLAKES, amount: '1 tsp', additionalDetails: '', optional: false, section: SECTIONS.SAUCE },
-        
-        { ...VEGETABLE_OIL, amount: '2 Tbsp', additionalDetails: '', optional: false, section: SECTIONS.SAUCE },
-        { ...BEEF_CHUCK_ROAST, amount: '1 lb', additionalDetails: 'thin strips', optional: false, section: SECTIONS.SAUCE },
-        { ...GREEN_BEANS, amount: '24 oz', additionalDetails: 'cut to 2" pieces', optional: false, section: SECTIONS.SAUCE },
-        { ...GARLIC, amount: '4 cloves', additionalDetails: 'minced', optional: false, section: SECTIONS.SAUCE },
-        { ...GINGER, amount: '1 inch', additionalDetails: 'minced', optional: false, section: SECTIONS.SAUCE },
-        { ...SALT, amount: '', additionalDetails: 'to taste', optional: false, section: SECTIONS.SAUCE },
-        { ...BLACK_PEPPER, amount: '', additionalDetails: 'to taste', optional: false, section: SECTIONS.SAUCE },
-        { ...GREEN_ONIONS, amount: '6', additionalDetails: 'diagonally sliced', optional: false, section: SECTIONS.SAUCE },
+        { ...VEGETABLE_OIL, amount: '3 Tbsp', additionalDetails: '', optional: false, section: SECTIONS.MAIN },
+        { ...BEEF_CHUCK_ROAST, amount: '1 lb', additionalDetails: 'thin strips', optional: false, section: SECTIONS.MAIN },
+        { ...GREEN_BEANS, amount: '24 oz', additionalDetails: 'cut to 2" pieces', optional: false, section: SECTIONS.MAIN },
+        { ...GREEN_ONIONS, amount: '6', additionalDetails: 'diagonally sliced', optional: false, section: SECTIONS.MAIN },
+        { ...GARLIC, amount: '4 cloves', additionalDetails: 'minced', optional: false, section: SECTIONS.MAIN },
+        { ...SALT, amount: '', additionalDetails: 'to taste', optional: false, section: SECTIONS.MAIN },
+        { ...BLACK_PEPPER, amount: '', additionalDetails: 'to taste', optional: false, section: SECTIONS.MAIN },
     ],
     appliances: [
         { name: 'stove' },
@@ -58,18 +68,18 @@ module.exports = {
         { name: 'small bowl' },
     ],
     directions: [
-        { step: 'In a bowl, combine broth, sauces, vinegar, and red pepper flakes. Set aside.', type: SECTIONS.SAUCE },
+        { step: 'In a bowl, combine broth, sauces, vinegar, and red pepper flakes. Set aside.', type: SECTIONS.SAUCE, img: sauce },
         { step: 'Over medium-high heat, heat 1 Tbsp of oil in a wok.', type: SECTIONS.MAIN },
-        { step: 'Add green beans and cook for 4 minutes. Remove beans and set aside.', type: SECTIONS.MAIN },
+        { step: 'Add green beans and cook for 4 minutes. Remove beans and set aside.', type: SECTIONS.MAIN, img: [whole, chopped, sautedBeans] },
         { step: 'Add 1 Tbsp of oil to the wok.', type: SECTIONS.MAIN },
-        { step: 'Add beef and cook for one minute. Do not stir.', type: SECTIONS.MAIN },
+        { step: 'Add beef and cook for one minute. Do not stir.', type: SECTIONS.MAIN, img: beef },
         { step: 'Stir beef and cook for 3 to 4. Remove beef and set aside.', type: SECTIONS.MAIN },
         { step: 'Add 1 Tbsp of oil to the wok.', type: SECTIONS.MAIN },
-        { step: 'Add green onions. Cook for 1 minute.', type: SECTIONS.MAIN },
-        { step: 'Add garlic and ginger. Cook for 30 seconds.', type: SECTIONS.MAIN },
-        { step: 'Re-add the beef and green beans to the wok.', type: SECTIONS.MAIN },
-        { step: 'Add the sauce. Cook and stir until the sauce thickens (about 1 to 2 minutes).', type: SECTIONS.MAIN },
+        { step: 'Add green onions. Cook for 1 minute.', type: SECTIONS.MAIN, img: [greenOnions, greenOnionPan] },
+        { step: 'Add garlic and ginger. Cook for 30 seconds.', type: SECTIONS.MAIN, img: [garlic, greenOnionsGarlic] },
+        { step: 'Re-add the beef and green beans to the wok.', type: SECTIONS.MAIN, img: combined },
+        { step: 'Add the sauce. Cook and stir until the sauce thickens (about 1 to 2 minutes).', type: SECTIONS.MAIN, img: combinedZoomed1 },
         { step: 'Season with salt and pepper as desired.', type: SECTIONS.MAIN },
-        { step: 'Serve over rice and enjoy this Chinese stir-fry.', type: SECTIONS.MAIN },
+        { step: 'Serve over rice and enjoy this Chinese stir-fry.', type: SECTIONS.MAIN, img: [plated1, plated2] },
     ]
 }
