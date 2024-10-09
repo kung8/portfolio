d6e4c9ed01309076b80189f69d952c4e7724bfe4
Kevin Ung <ung.kevin78@gmail.com>
2024-10-08 22:34:31 -0600

    add popcorn balls

--- server/data/recipes/popcorn-balls.js
+++ server/data/recipes/popcorn-balls.js
@@ -1,12 +1,23 @@
-const popcornBalls = '../assets/Products/popcorn-balls.jpeg';
+const bagBowls = '../assets/Products/popcorn-balls-in-bags-in-bowls.jpeg';
+const bagZoomed = '../assets/Products/popcorn-balls-in-bags-zoomed.jpeg';
+const pouredZoomed = '../assets/Products/popcorn-balls-melted-poured-over-popcorn-zoomed.jpeg';
+const poured = '../assets/Products/popcorn-balls-melted-poured-over-popcorn.jpeg';
+const melting = '../assets/Products/popcorn-balls-melting.jpeg';
+const moreMelted = '../assets/Products/popcorn-balls-more-melted.jpeg';
+const rackStacked = '../assets/Products/popcorn-balls-on-rack-stacked.jpeg';
+const rack = '../assets/Products/popcorn-balls-on-rack.jpeg';
 const popped = '../assets/Products/popcorn-balls-popped-corn.jpeg';
+const rackZoomed = '../assets/Products/popcorn-balls-rack-zoomed.jpeg';
+const mixed = '../assets/Products/popcorn-balls-unmelted-mixed.jpeg';
+const unmeltedZoomed = '../assets/Products/popcorn-balls-unmelted-zoomed.jpeg';
+const unmelted = '../assets/Products/popcorn-balls-unmelted.jpeg';
 const { GENRES, METHODS, SECTIONS, TYPES, CATEGORIES } = require('./constants');
 const { POPCORN, CORN_SYRUP, UNSALTED_BUTTER, COLD_WATER, POWDERED_SUGAR, MINI_MARSHMALLOWS, COOKING_SPRAY } = require('./ingredients');
 
 module.exports = {
     cardName: 'Popcorn Balls',
     name: 'Popcorn Balls',
-    img: popcornBalls,
+    img: rackStacked,
     available: true,
     category: [CATEGORIES.DESSERT],
     genre: [GENRES.AMERICAN],
@@ -39,12 +50,12 @@ module.exports = {
     directions: [
         { step: 'Remove any unpopped kernels so you don\'t break your teeth.', type: SECTIONS.MAIN, img: popped },
         { step: 'Prepare a baking sheet and line with parchment paper before beginning to cook.' },
-        { step: 'Over medium heat, mix together corn syrup, butter, water, powdered sugar and marshmallows in a large pot.', type: SECTIONS.MAIN },
-        { step: 'Stir and continue to let the mixture melt.', type: SECTIONS.MAIN },
-        { step: 'Slowly pour mixture over popcorn and mix until the popcorn is evenly coated.', type: SECTIONS.MAIN },
+        { step: 'Over medium heat, mix together corn syrup, butter, water, powdered sugar and marshmallows in a large pot.', type: SECTIONS.MAIN, img: [unmelted, unmeltedZoomed, mixed] },
+        { step: 'Stir and continue to let the mixture melt.', type: SECTIONS.MAIN, img: [melting, moreMelted] },
+        { step: 'Slowly pour mixture over popcorn and mix until the popcorn is evenly coated.', type: SECTIONS.MAIN, img: [poured, pouredZoomed] },
         { step: 'Quickly grease hands with cooking spray and form the popcorn into balls.', type: SECTIONS.MAIN },
-        { step: 'Place popcorn balls on the prepared baking sheet.', type: SECTIONS.MAIN },
-        { step: 'Wrap each ball in plastic wrap or in individual sandwich ziploc bags and keep at room temperature.', type: SECTIONS.MAIN },
+        { step: 'Place popcorn balls on the prepared baking sheet.', type: SECTIONS.MAIN, img: [rack, rackZoomed] },
+        { step: 'Wrap each ball in plastic wrap or in individual sandwich ziploc bags and keep at room temperature.', type: SECTIONS.MAIN, img: [bagBowls, bagZoomed] },
         { step: 'Enjoy these sweet treats while you watch a show or movie!', type: SECTIONS.MAIN },
     ]
 };
