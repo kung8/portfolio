254248c795222fa0beaa368735a1db5fbd38bbc3
Kevin Ung <ung.kevin78@gmail.com>
2024-10-13 23:30:45 -0600

    add arrows controller

--- src/components/recipes/recipe/RecipeImageModal.js
+++ src/components/recipes/recipe/RecipeImageModal.js
@@ -1,12 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { handleModalClass } from '../utils/handle-modal-class';
 import { ModalBody, ModalContent } from '../../modal/ModalContent';
 import xBtn from '../../../Assets/x.png';
+import arrow from '../../../Assets/arrow.png';
+import disabledArrow from '../../../Assets/disabled-arrow.png';
 
-export const RecipeImageModal = ({ closeModal, name, image, images, show }) => {
+export const RecipeImageModal = ({ closeModal, name, image: initialImage, images, show }) => {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
     useEffect(() => {
         handleModalClass(show, '.recipe-image-modal', 'recipe-image-modal-overlay');
     }, [show]);
+
+    useEffect(() => {
+        const index = images.indexOf(initialImage);
+        if (index > -1) setCurrentImageIndex(index);
+    }, [images, initialImage]);
 
     return (
         <div className="recipe-image-modal-container">
@@ -18,11 +26,34 @@ export const RecipeImageModal = ({ closeModal, name, image, images, show }) => {
             <div className="recipe-image-modal">
                 <ModalContent>
                     <ModalBody>
-                        <img className="recipe-image-modal-image" src={image} alt={name} />
+                        <img className="recipe-image-modal-image" src={images[currentImageIndex]} alt={name} />
                         <button className="close" onClick={closeModal}>
                             <img src={xBtn} alt="close" />
                         </button>
-                        {/* <div></div> */}
+                        <div className="arrows-container">
+                            <button
+                                className="arrow-btn"
+                                disabled={currentImageIndex <= 0}
+                                onClick={() => setCurrentImageIndex(currentImageIndex - 1)}
+                            >
+                                {currentImageIndex <= 0 ? (
+                                    <img className="disabled-arrow" src={disabledArrow} alt="previous" />
+                                ) : (
+                                    <img className="prev-arrow" src={arrow} alt="previous" />
+                                )}
+                            </button>
+                            <button
+                                className="arrow-btn"
+                                disabled={currentImageIndex >= images.length - 1}
+                                onClick={() => setCurrentImageIndex(currentImageIndex + 1)}
+                            >
+                                {currentImageIndex >= images.length - 1 ? (
+                                    <img className="disabled-arrow next" src={disabledArrow} alt="next" />
+                                ) : (
+                                    <img className="next-arrow" src={arrow} alt="next" />
+                                )}
+                            </button>
+                        </div>
                     </ModalBody>
                 </ModalContent>
             </div>
