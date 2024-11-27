94d089af85763aff7ca839d921da1a5890e166b1
Kevin Ung <ung.kevin78@gmail.com>
2024-11-26 19:44:04 -0700

    add clearing to the search bar

--- src/components/search-bar/SearchBar.js
+++ src/components/search-bar/SearchBar.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import xBtn from '../../Assets/x.png';
 
 export const SearchBar = ({ search, setSearch }) => (
     <div className={`search-bar ${search ? 'contains-search' : ''}`}>
-        <input type="text" placeholder="Search" onChange={(e) => setSearch(e.target.value)} />
+        <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+        <button 
+            className={`clear-btn ${search ? 'contains-search' : ''}`} 
+            onClick={() => setSearch('')}
+        >
+            <img src={xBtn} alt="close" />
+        </button>
     </div>
 );
