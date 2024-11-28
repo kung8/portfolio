c9a7326e626ad6add4d48ef14030e8e66bd86a78
Kevin Ung <ung.kevin78@gmail.com>
2024-11-27 22:17:46 -0700

    update green bean casserole

--- server/data/recipes/green-bean-casserole.js
+++ server/data/recipes/green-bean-casserole.js
@@ -1,11 +1,20 @@
-// const example = '../assets/Products/example.jpeg';
+const casserole1 = '../assets/Products/green-bean-casserole-1.jpeg';
+const casserole2 = '../assets/Products/green-bean-casserole-2.jpeg';
+const casserole3 = '../assets/Products/green-bean-casserole-3.jpeg';
+const casserole4 = '../assets/Products/green-bean-casserole-4.jpeg';
+const mix1 = '../assets/Products/green-bean-casserole-mix-1.jpeg';
+const mix2 = '../assets/Products/green-bean-casserole-mix-2.jpeg';
+const mix3 = '../assets/Products/green-bean-casserole-mix-3.jpeg';
+const mix4 = '../assets/Products/green-bean-casserole-mix-4.jpeg';
 const { GENRES, METHODS, SECTIONS, TYPES, CATEGORIES } = require('./constants');
 const { CANNED_GREEN_BEANS, CREAM_OF_MUSHROOM, MILK, LIGHT_SOY_SAUCE, BLACK_PEPPER, SALT, CRISPY_FRIED_ONIONS } = require('./ingredients');
+
+const CASSEROLE = 'Casserole';
 
 module.exports = {
     cardName: 'Green Bean Casserole',
     name: 'Green Bean Casserole',
-    img: '',
+    img: casserole4,
     available: true,
     category: [CATEGORIES.SIDE_DISH],
     genre: [GENRES.AMERICAN],
@@ -19,13 +28,13 @@ module.exports = {
     ],
     separated: true,
     ingredients: [
-        { ...CANNED_GREEN_BEANS, amount: '4 c can', additionalDetails: '', section: SECTIONS.MAIN },
-        { ...CREAM_OF_MUSHROOM, amount: '10.5 oz can', additionalDetails: '', section: SECTIONS.MAIN },
-        { ...MILK, amount: '1/2 c', additionalDetails: '', section: SECTIONS.MAIN },
-        { ...LIGHT_SOY_SAUCE, amount: '1 tsp', additionalDetails: '', section: SECTIONS.MAIN },
-        { ...BLACK_PEPPER, amount: '1/2 tsp', additionalDetails: '', section: SECTIONS.MAIN },
-        { ...SALT, amount: '1/4 tsp', additionalDetails: 'to taste', section: SECTIONS.MAIN },
-        { ...CRISPY_FRIED_ONIONS, amount: '1 c', additionalDetails: '', section: SECTIONS.MAIN },
+        { ...CANNED_GREEN_BEANS, amount: '4 - 14.5 oz', additionalDetails: 'drained', section: CASSEROLE },
+        { ...CREAM_OF_MUSHROOM, amount: '10.5 oz can', additionalDetails: '', section: CASSEROLE },
+        { ...MILK, amount: '1/2 c', additionalDetails: '', section: CASSEROLE },
+        { ...LIGHT_SOY_SAUCE, amount: '1 tsp', additionalDetails: '', section: CASSEROLE },
+        { ...BLACK_PEPPER, amount: '1/2 tsp', additionalDetails: '', section: CASSEROLE },
+        { ...SALT, amount: '1/4 tsp', additionalDetails: 'to taste', section: CASSEROLE },
+        { ...CRISPY_FRIED_ONIONS, amount: '1 c', additionalDetails: '', section: CASSEROLE },
         { ...CRISPY_FRIED_ONIONS, amount: '1/2 c', additionalDetails: '', section: SECTIONS.TOPPINGS },
     ],
     appliances: [
@@ -35,12 +44,12 @@ module.exports = {
         { name: 'baking pan' },
     ],
     directions: [
-        { step: 'Preheat the oven to 350ºF.', type: SECTIONS.MAIN },
-        { step: 'Combine together all the ingredients in the baking dish (only the 1 cup fried onions).', type: SECTIONS.MAIN },
-        { step: 'Bake until bubbly (about 30 to 35 minutes).', type: SECTIONS.MAIN },
-        { step: 'Remove from the oven and mix.', type: SECTIONS.MAIN },
-        { step: 'Top with the 1/2 cup crispy fried onions.', type: SECTIONS.MAIN },
-        { step: 'Bake until golden (about 10 minutes).', type: SECTIONS.MAIN },
+        { step: 'Preheat the oven to 350ºF.', type: CASSEROLE },
+        { step: 'Combine all the "Casserole" ingredients in the baking dish.', type: CASSEROLE, img: [mix1, mix2, mix3] },
+        { step: 'Bake until bubbly (about 30 to 35 minutes).', type: CASSEROLE },
+        { step: 'Remove from the oven and mix.', type: CASSEROLE, img: mix4 },
+        { step: 'Top with the crispy fried onions.', type: SECTIONS.TOPPINGS, img: [casserole1, casserole2, casserole3] },
+        { step: 'Bake until golden (about 10 minutes).', type: SECTIONS.TOPPINGS },
         { step: 'Serve this dish warm at your next holiday feast.', type: SECTIONS.SERVE },
     ]
 };
