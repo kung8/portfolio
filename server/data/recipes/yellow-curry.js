8a1ba6970dbf0b375186acae1a78363a0418f42d
Kevin Ung <ung.kevin78@gmail.com>
2024-12-27 21:21:15 -0700

    add yellow curry images

--- server/data/recipes/yellow-curry.js
+++ server/data/recipes/yellow-curry.js
@@ -1,4 +1,22 @@
-// const example = '../assets/Products/example.jpeg';
+const curry1 = '../assets/Products/yellow-curry-1.jpeg';
+const curry2 = '../assets/Products/yellow-curry-2.jpeg';
+const curry3 = '../assets/Products/yellow-curry-3.jpeg';
+const curry4 = '../assets/Products/yellow-curry-4.jpeg';
+const curry5 = '../assets/Products/yellow-curry-5.jpeg';
+const curry6 = '../assets/Products/yellow-curry-6.jpeg';
+const curry7 = '../assets/Products/yellow-curry-7.jpeg';
+const curry8 = '../assets/Products/yellow-curry-8.jpeg';
+const curry9 = '../assets/Products/yellow-curry-9.jpeg';
+const curry10 = '../assets/Products/yellow-curry-10.jpeg';
+const curry11 = '../assets/Products/yellow-curry-11.jpeg';
+const curry12 = '../assets/Products/yellow-curry-12.jpeg';
+const curry13 = '../assets/Products/yellow-curry-13.jpeg';
+const curry14 = '../assets/Products/yellow-curry-14.jpeg';
+const curry15 = '../assets/Products/yellow-curry-15.jpeg';
+const curry16 = '../assets/Products/yellow-curry-16.jpeg';
+const curry17 = '../assets/Products/yellow-curry-17.jpeg';
+const curry18 = '../assets/Products/yellow-curry-18.jpeg';
+
 const { CATEGORIES, GENRES, METHODS, PROTEIN, SECTIONS, TYPES } = require('./constants');
 const { OLIVE_OIL, SMALL_YELLOW_ONION, BONELESS_AND_SKINLESS_CHICKEN_BREAST, YUKON_GOLD_POTATOES, CARROTS, YELLOW_CURRY_PASTE, GINGER, GARLIC, COCONUT_MILK, CORNSTARCH, FISH_SAUCE, LIME_JUICE, BROWN_SUGAR, CILANTRO, SRIRACHA_SAUCE } = require('./ingredients');
 
@@ -6,13 +24,12 @@ const YELLOW_CURRY_SECTION = 'Yellow Curry';
 const MAKE_CURRY = 'Make Curry';
 
 module.exports = {
-    wip: true,
     cardName: 'Yellow Curry',
     name: 'Thai Yellow Curry',
-    img: '',
+    img: curry17,
     recipeCredit: '',
     available: true,
-    recommended: false,
+    recommended: true,
     category: [CATEGORIES.DINNER, CATEGORIES.LUNCH],
     genre: [GENRES.THAI],
     method: [METHODS.PAN_FRY, METHODS.SIMMER],
@@ -49,15 +66,15 @@ module.exports = {
         { name: 'pot' },
     ],
     directions: [
-        { step: 'Over medium-low heat, heat oil in a large pot.', type: MAKE_CURRY },
-        { step: 'Add onion and saute until soften (about 3 to 4 minutes).', type: MAKE_CURRY },
-        { step: 'Add chicken, carrots, and potatoes. Cook for 1 to 2 minutes.', type: MAKE_CURRY },
-        { step: 'Add ginger, garlic and curry paste. Saute for 3 minutes.', type: MAKE_CURRY },
-        { step: 'Add 3/4 of coconut milk.', type: MAKE_CURRY },
-        { step: 'Mix cornstarch into the last quarter of coconut milk. Add this to the pot.', type: MAKE_CURRY },
+        { step: 'Over medium-low heat, heat oil in a large pot.', type: MAKE_CURRY, img: curry1 },
+        { step: 'Add onion and saute until soften (about 3 to 4 minutes).', type: MAKE_CURRY, img: [curry2, curry3, curry4, curry6] },
+        { step: 'Add chicken, carrots, and potatoes. Cook for 1 to 2 minutes.', type: MAKE_CURRY, img: [curry5, curry7, curry8] },
+        { step: 'Add ginger, garlic and curry paste. Saute for 3 minutes.', type: MAKE_CURRY, img: [curry9, curry10] },
+        { step: 'Add 3/4 of coconut milk.', type: MAKE_CURRY, img: curry11 },
+        { step: 'Mix cornstarch into the last quarter of coconut milk. Add this to the pot.', type: MAKE_CURRY, img: [curry12, curry13] },
         { step: 'Bring to a boil.', type: MAKE_CURRY },
-        { step: 'Reduce heat to low and simmer until chicken and potatoes are cooked through (about 20 to 30 minutes). Thin curry by adding water if needed.', type: MAKE_CURRY },
-        { step: 'Add fish sauce, brown sugar and lime juice. Simmer for 5 minutes.', type: MAKE_CURRY },
-        { step: 'Serve with jasmine rice (or your preferred rice).', type: SECTIONS.SERVE },
+        { step: 'Reduce heat to low and simmer until chicken and potatoes are cooked through (about 20 to 30 minutes). Thin curry by adding water if needed.', type: MAKE_CURRY, img: curry14 },
+        { step: 'Add fish sauce, brown sugar and lime juice. Simmer for 5 minutes.', type: MAKE_CURRY, img: [curry15, curry16] },
+        { step: 'Serve with jasmine rice (or your preferred rice).', type: SECTIONS.SERVE, img: curry18 },
     ]
 };
