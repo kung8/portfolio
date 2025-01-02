a343d6989cf91689557b224c65d664a35c64d399
Kevin Ung <ung.kevin78@gmail.com>
2025-01-01 18:21:13 -0700

    simplify chili images

--- server/data/recipes/chili.js
+++ server/data/recipes/chili.js
@@ -1,24 +1,15 @@
-const beans = '../assets/Products/chili-beans.jpeg';
 const beef = '../assets/Products/chili-beef.jpeg';
 const cubes = '../assets/Products/chili-cubes.jpeg';
 const diced = '../assets/Products/chili-diced-tomatoes.jpeg';
 const fragrantPot = '../assets/Products/chili-fragrant-in-pot.jpeg';
 const fragrant = '../assets/Products/chili-fragrant.jpeg';
 const pot1 = '../assets/Products/chili-in-pot-1.jpeg';
-const pot2 = '../assets/Products/chili-in-pot-2.jpeg';
 const pot3 = '../assets/Products/chili-in-pot-3.jpeg';
-const pot4 = '../assets/Products/chili-in-pot-4.jpeg';
 const pot5 = '../assets/Products/chili-in-pot-5.jpeg';
 const pot6 = '../assets/Products/chili-in-pot-6.jpeg';
 const pot7 = '../assets/Products/chili-in-pot-7.jpeg';
-const pot8 = '../assets/Products/chili-in-pot-8.jpeg';
-const peppers = '../assets/Products/chili-peppers.jpeg';
-const plated1 = '../assets/Products/chili-plated-1.jpeg';
-const plated2 = '../assets/Products/chili-plated-2.jpeg';
 const plated3 = '../assets/Products/chili-plated-3.jpeg';
 const plated4 = '../assets/Products/chili-plated-4.jpeg';
-const plated5 = '../assets/Products/chili-plated-5.jpeg';
-const plated6 = '../assets/Products/chili-plated-6.jpeg';
 const { CATEGORIES, SECTIONS, GENRES, TYPES, METHODS, PROTEIN } = require("./constants");
 const {
     GROUND_BEEF,
@@ -90,11 +81,11 @@ module.exports = {
     ],
     directions: [
         { step: 'In a large pot over medium-high heat, combine beef, chili powder, onion, jalapeno, and garlic.', type: BEEF, img: [beef, fragrant, fragrantPot] },
-        { step: 'Brown the content, mix occasionally, and drain any fat.', type: BEEF, img: [pot1, pot2, pot3] },
-        { step: 'Add the cumin, bell pepper, different tomato variations, beans, broth, brown sugar, salt and pepper.', type: VEGGIES, img: [peppers, cubes, beans, diced] },
-        { step: 'Bring to a boil.', type: VEGGIES, img: [pot7, pot8] },
-        { step: 'Lower heat and cook for 45 - 60 minutes or until it has become the desired thickness.', type: VEGGIES, img: [pot4, pot6] },
-        { step: 'Serve with your favorite toppings and feel free to add as much spice as you can handle or desire.', type: SECTIONS.TOPPINGS, img: [plated1, plated2, plated3, plated4, plated5, plated6] },
+        { step: 'Brown the content, mix occasionally, and drain any fat.', type: BEEF, img: [pot1, pot3] },
+        { step: 'Add the cumin, bell pepper, different tomato variations, beans, broth, brown sugar, salt and pepper.', type: VEGGIES, img: [cubes, diced] },
+        { step: 'Bring to a boil.', type: VEGGIES, img: pot7 },
+        { step: 'Lower heat and cook for 45 - 60 minutes or until it has become the desired thickness.', type: VEGGIES, img: pot6 },
+        { step: 'Serve with your favorite toppings and feel free to add as much spice as you can handle or desire.', type: SECTIONS.TOPPINGS, img: [plated3, plated4] },
     ],
     notes: [
         { note: 'With one jalapeno pepper it is very mild and a great base for those who do not like spicy food.' },
