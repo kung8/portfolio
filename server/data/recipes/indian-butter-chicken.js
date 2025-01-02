d20af46eed514535bb6b2afce36fcf7781aa5608
Kevin Ung <ung.kevin78@gmail.com>
2025-01-01 18:28:53 -0700

    simplify butter chicken images

--- server/data/recipes/indian-butter-chicken.js
+++ server/data/recipes/indian-butter-chicken.js
@@ -1,24 +1,17 @@
 const browned = '../assets/Products/indian-butter-chicken-browned-onions.jpeg';
 const browning = '../assets/Products/indian-butter-chicken-browning-chicken.jpeg';
-const chickenSpicesAdded = '../assets/Products/indian-butter-chicken-chicken-and-spices-added.jpeg';
-const cooked1 = '../assets/Products/indian-butter-chicken-cooked-1.jpeg';
-const cooked2 = '../assets/Products/indian-butter-chicken-cooked-1.jpeg';
 const cooked3 = '../assets/Products/indian-butter-chicken-cooked-3.jpeg';
-const cooked4 = '../assets/Products/indian-butter-chicken-cooked-4.jpeg';
 const creamAdded = '../assets/Products/indian-butter-chicken-cream-added.jpeg';
 const cream = '../assets/Products/indian-butter-chicken-cream.jpeg';
 const fragrantAdded = '../assets/Products/indian-butter-chicken-fragrant-added.jpeg';
 const fragrant = '../assets/Products/indian-butter-chicken-fragrant.jpeg';
 const oils = '../assets/Products/indian-butter-chicken-oils.jpeg';
 const onion = '../assets/Products/indian-butter-chicken-onion.jpeg';
-const plated1 = '../assets/Products/indian-butter-chicken-plated-1.jpeg';
-const plated2 = '../assets/Products/indian-butter-chicken-plated-2.jpeg';
 const plated3 = '../assets/Products/indian-butter-chicken-plated-3.jpeg';
-const plated4 = '../assets/Products/indian-butter-chicken-plated-4.jpeg';
-const raw = '../assets/Products/indian-butter-chicken-raw-chicken.jpeg';
 const simmering = '../assets/Products/indian-butter-chicken-simmering.jpeg';
 const spices = '../assets/Products/indian-butter-chicken-spices.jpeg';
 const paste = '../assets/Products/indian-butter-chicken-tomato-paste.jpeg';
+
 const { CATEGORIES, GENRES, SECTIONS, TYPES, METHODS, PROTEIN } = require('./constants');
 const {
     VEGETABLE_OIL,
@@ -86,8 +79,8 @@ module.exports = {
         { step: 'Add oil and butter to a large cooking pan on medium-high heat.', type: SECTIONS.MAIN, img: oils },
         { step: 'Add the diced onion and cook until light brown.', type: SECTIONS.MAIN, img: [onion, browned] },
         { step: 'Add the ginger and garlic to the pan and cook until aromatic (about 30 seconds).', img: [fragrant, fragrantAdded] },
-        { step: 'Add the chicken, spices, and tomato paste to the pan. Cook until the chicken is browned (about 5 to 6 minutes).', img: [raw, spices, chickenSpicesAdded, paste, browning] },
-        { step: 'Add the heavy cream and let simmer for 8 to 10 minutes.', type: SECTIONS.MAIN, img: [cream, creamAdded, simmering, cooked1, cooked2, cooked4] },
-        { step: 'Serve with rice (and naan or roti is optional and delicious).', type: SECTIONS.MAIN, img: [plated1, plated2, plated3, plated4] },
+        { step: 'Add the chicken, spices, and tomato paste to the pan. Cook until the chicken is browned (about 5 to 6 minutes).', img: [spices, paste, browning] },
+        { step: 'Add the heavy cream and let simmer for 8 to 10 minutes.', type: SECTIONS.MAIN, img: [cream, creamAdded, simmering] },
+        { step: 'Serve with rice (and naan or roti is optional and delicious).', type: SECTIONS.MAIN, img: plated3 },
     ]
 };
