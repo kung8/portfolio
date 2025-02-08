fec47efbe0ddadd699213f8955bebeaedd18757d
Kevin Ung <ung.kevin78@gmail.com>
2025-02-08 00:32:22 -0700

    add collapsing filter logic

--- src/components/recipes/recipes/RecipeSearchBar.js
+++ src/components/recipes/recipes/RecipeSearchBar.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { SearchBar } from '../../search-bar/SearchBar';
+import xImg from '../../../Assets/x.png';
 
 export const RecipeSearchBar = ({ search, setSearch, setShowFilters, showFilters }) => (
     <div className="search-and-filter-visible-toggle-container">
         <SearchBar search={search} setSearch={setSearch} />
-        <p className="filter-visible-toggle" onClick={() => setShowFilters(!showFilters)}>{showFilters ? 'Collapse' : 'Expand'}</p>
+        <img
+            className={`x-img ${showFilters ? 'rotate' : ''}`}
+            onClick={() => setShowFilters(!showFilters)}
+            src={xImg}
+            alt="Show filters"
+        />
     </div>
 );
