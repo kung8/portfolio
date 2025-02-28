f9c7affe6bae03de86474dada38afd6cbb6f40de
Kevin Ung <ung.kevin78@gmail.com>
2025-02-27 22:43:30 -0700

    reduce marry me chicken and mexican cilantro lime rice images

--- server/data/recipes/marry-me-chicken.js
+++ server/data/recipes/marry-me-chicken.js
@@ -1,33 +1,23 @@
 const chicken1 = '../assets/Products/marry-me-chicken-chicken-1.jpeg';
-const chicken2 = '../assets/Products/marry-me-chicken-chicken-2.jpeg';
 const chicken3 = '../assets/Products/marry-me-chicken-chicken-3.jpeg';
-const chicken4 = '../assets/Products/marry-me-chicken-chicken-4.jpeg';
 const chicken5 = '../assets/Products/marry-me-chicken-chicken-5.jpeg';
 const chicken6 = '../assets/Products/marry-me-chicken-chicken-6.jpeg';
 const chicken7 = '../assets/Products/marry-me-chicken-chicken-7.jpeg';
 const chicken8 = '../assets/Products/marry-me-chicken-chicken-8.jpeg';
 const chicken9 = '../assets/Products/marry-me-chicken-chicken-9.jpeg';
-const sauce1 = '../assets/Products/marry-me-chicken-sauce-1.jpeg';
 const sauce2 = '../assets/Products/marry-me-chicken-sauce-2.jpeg';
 const sauce3 = '../assets/Products/marry-me-chicken-sauce-3.jpeg';
-const sauce4 = '../assets/Products/marry-me-chicken-sauce-4.jpeg';
 const sauce5 = '../assets/Products/marry-me-chicken-sauce-5.jpeg';
 const sauce6 = '../assets/Products/marry-me-chicken-sauce-6.jpeg';
-const sauce7 = '../assets/Products/marry-me-chicken-sauce-7.jpeg';
 const sauce8 = '../assets/Products/marry-me-chicken-sauce-8.jpeg';
-const sauce9 = '../assets/Products/marry-me-chicken-sauce-9.jpeg';
-const sauce10 = '../assets/Products/marry-me-chicken-sauce-10.jpeg';
 const sauce11 = '../assets/Products/marry-me-chicken-sauce-11.jpeg';
 const sauce12 = '../assets/Products/marry-me-chicken-sauce-12.jpeg';
 const combined1 = '../assets/Products/marry-me-chicken-1.jpeg';
 const combined2 = '../assets/Products/marry-me-chicken-2.jpeg';
 const combined3 = '../assets/Products/marry-me-chicken-3.jpeg';
-const combined4 = '../assets/Products/marry-me-chicken-4.jpeg';
-const combined5 = '../assets/Products/marry-me-chicken-5.jpeg';
-const plated1 = '../assets/Products/marry-me-chicken-plated-1.jpeg';
 const plated2 = '../assets/Products/marry-me-chicken-plated-2.jpeg';
 const plated3 = '../assets/Products/marry-me-chicken-plated-3.jpeg';
-const plated4 = '../assets/Products/marry-me-chicken-plated-4.jpeg';
+
 const { GENRES, METHODS, PROTEIN, SECTIONS, TYPES, CATEGORIES } = require('./constants');
 const { BONELESS_AND_SKINLESS_CHICKEN_BREAST, SALT, BLACK_PEPPER, OLIVE_OIL, GARLIC, CHICKEN_BROTH, HEAVY_CREAM, PARMESAN_CHEESE, OREGANO, RED_PEPPER_FLAKES, BASIL_LEAVES, SUN_DRIED_TOMATOES } = require('./ingredients');
 
@@ -36,7 +26,7 @@ const SUN_DRIED_TOMATO_CREAM_SAUCE = 'Sun-Dried Tomato Cream Sauce';
 module.exports = {
     cardName: 'Marry Me Chicken',
     name: 'Marry Me Chicken',
-    img: combined4,
+    img: plated2,
     available: true,
     recommended: true,
     category: [CATEGORIES.DINNER, CATEGORIES.LUNCH],
@@ -76,7 +66,7 @@ module.exports = {
     ],
     directions: [
         { step: 'Preheat the oven to 375ºF. Arrange the rack so the dutch oven pot will be cooked in the center of the oven.', type: SECTIONS.CHICKEN, img: chicken1 },
-        { step: 'Prepare chicken by removing any fat and pat dry.', type: SECTIONS.CHICKEN, img: [chicken2, chicken3, chicken4] },
+        { step: 'Prepare chicken by removing any fat and pat dry.', type: SECTIONS.CHICKEN, img: chicken3 },
         { step: 'Season with salt and pepper generously on both sides.', type: SECTIONS.CHICKEN, img: chicken6 },
         { step: 'Over medium-high heat, add olive oil and spread until covered.', type: SECTIONS.CHICKEN, img: chicken5 },
         { step: 'Sear chicken until lightly golden (about 3 to 4 minutes).', type: SECTIONS.CHICKEN, img: chicken7 },
@@ -84,20 +74,20 @@ module.exports = {
         { step: 'Transfer chicken to plate (chicken should not be cooked all the way through).', type: SECTIONS.CHICKEN, img: chicken9 },
         
         { step: 'Reduce to medium-low heat and wait until dutch oven pot cools.', type: SUN_DRIED_TOMATO_CREAM_SAUCE },
-        { step: 'Add garlic. Saute until fragrant (about 1 to 2 minutes).', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: [sauce1, sauce2] },
+        { step: 'Add garlic. Saute until fragrant (about 1 to 2 minutes).', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: sauce2 },
         { step: 'Add broth and deglaze the pan.', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: sauce3 },
         { step: 'Remove pan from heat.', type: SUN_DRIED_TOMATO_CREAM_SAUCE },
-        { step: 'Add cream and mix.', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: [sauce4, sauce5] },
-        { step: 'Add cheese and stir until the cheese melts.', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: [sauce6, sauce7] },
-        { step: 'Move pot back to burner and stir for a minute (while the pot returns to heat).', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: [sauce8, sauce9] },
-        { step: 'Add sun-dried tomatoes, oregano and red pepper flakes. Stir.', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: [sauce10, sauce11] },
+        { step: 'Add cream and mix.', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: sauce5 },
+        { step: 'Add cheese and stir until the cheese melts.', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: sauce6 },
+        { step: 'Move pot back to burner and stir for a minute (while the pot returns to heat).', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: sauce8 },
+        { step: 'Add sun-dried tomatoes, oregano and red pepper flakes. Stir.', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: sauce11 },
         { step: 'Taste sauce and adjust with seasoning as needed.', type: SUN_DRIED_TOMATO_CREAM_SAUCE },
         { step: 'Simmer over medium-low heat and stir occasionally.', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: sauce12 },
         { step: 'Once simmering, add chicken and flip several times until the chicken is coated.', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: combined1 },
         { step: 'Transfer the pot to the oven.', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: combined2 },
         { step: 'Bake until the chicken reaches 160ºF (about 12 to 15 minutes).', type: SUN_DRIED_TOMATO_CREAM_SAUCE },
-        { step: 'Carefully remove pot from oven and spoon sauce over chicken.', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: [combined3, combined4, combined5] },
+        { step: 'Carefully remove pot from oven and spoon sauce over chicken.', type: SUN_DRIED_TOMATO_CREAM_SAUCE, img: combined3 },
         { step: 'Let chicken rest for 5 minutes (residual heat will cook the chicken, wait until it is at least 165ºF).', type: SUN_DRIED_TOMATO_CREAM_SAUCE },
-        { step: 'Serve with your favorite sides and be sure to ladle sauce over the chicken.', type: SECTIONS.SERVE, img: [plated1, plated2, plated3, plated4] },
+        { step: 'Serve with your favorite sides and be sure to ladle sauce over the chicken.', type: SECTIONS.SERVE, img: plated3 },
     ]
 };
--- server/data/recipes/mexican-cilantro-lime-chicken.js
+++ server/data/recipes/mexican-cilantro-lime-chicken.js
@@ -1,21 +1,10 @@
-const blended = '../assets/Products/mexican-cilantro-lime-chicken-blended.jpeg';
-const blender = '../assets/Products/mexican-cilantro-lime-chicken-blender.jpeg';
 const cilantro1 = '../assets/Products/mexican-cilantro-lime-chicken-cilantro-1.jpeg';
-const cilantro2 = '../assets/Products/mexican-cilantro-lime-chicken-cilantro-2.jpeg';
 const dressing = '../assets/Products/mexican-cilantro-lime-chicken-dressing.jpeg';
-const marinade1 = '../assets/Products/mexican-cilantro-lime-chicken-marinade-1.jpeg';
-const marinade2 = '../assets/Products/mexican-cilantro-lime-chicken-marinade-2.jpeg';
 const marinade3 = '../assets/Products/mexican-cilantro-lime-chicken-marinade-3.jpeg';
-const wings = '../assets/Products/mexican-cilantro-lime-chicken-wings.jpeg';
-const baking = '../assets/Products/mexican-cilantro-lime-chicken-baking.jpeg';
 const broiled1 = '../assets/Products/mexican-cilantro-lime-chicken-broiled-1.jpeg';
-const broiled2 = '../assets/Products/mexican-cilantro-lime-chicken-broiled-2.jpeg';
-const zoomed = '../assets/Products/mexican-cilantro-lime-chicken-raw-on-baking-rack-zoomed.jpeg';
 const rack = '../assets/Products/mexican-cilantro-lime-chicken-raw-on-baking-rack.jpeg';
-const chicken = '../assets/Products/mexican-cilantro-lime-chicken-plated-just-chicken.jpeg';
 const set1 = '../assets/Products/mexican-cilantro-lime-chicken-plated-set-1.jpeg';
 const set2 = '../assets/Products/mexican-cilantro-lime-chicken-plated-set-2.jpeg';
-const rice = '../assets/Products/mexican-cilantro-lime-chicken-plated-with-rice.jpeg';
 
 const { GENRES, METHODS, PROTEIN, SECTIONS, TYPES, CATEGORIES } = require('./constants');
 const {
@@ -69,13 +58,13 @@ module.exports = {
         { name: 'tongs' },
     ],
     directions: [
-        { step: 'Combine all the dressing ingredients in a blender.', type: SECTIONS.DRESSING, img: [blender, cilantro1, cilantro2, blended, dressing] },
-        { step: 'Pour 1/2 of the dressing into a ziploc bag and place the chicken wings and drum.', type: SECTIONS.MEAT, img: [wings, marinade1, marinade2, marinade3] },
+        { step: 'Combine all the dressing ingredients in a blender.', type: SECTIONS.DRESSING, img: [cilantro1, dressing] },
+        { step: 'Pour 1/2 of the dressing into a ziploc bag and place the chicken wings and drum.', type: SECTIONS.MEAT, img: marinade3 },
         { step: 'Let the chicken marinate for 4 hours.', type: SECTIONS.MEAT },
         { step: 'Preheat the oven to 400ºF.', type: SECTIONS.MAIN },
         { step: 'Place a wire rack on top of a baking pan.', type: SECTIONS.MAIN },
-        { step: 'Use tongs to transfer chicken to wire rack in a single layer.', type: SECTIONS.MAIN, img: [rack, zoomed] },
-        { step: 'Bake for 30 to 35 minutes. Feel free to broil the wings for 2 to 3 minutes in addition to make it crispier.', type: SECTIONS.MAIN, img: [baking, broiled1, broiled2] },
-        { step: 'Dip this chicken in the dressing and serve it with a side of salad and cilantro rice.', type: SECTIONS.SERVE, img: [chicken, set2, rice] },
+        { step: 'Use tongs to transfer chicken to wire rack in a single layer.', type: SECTIONS.MAIN, img: rack },
+        { step: 'Bake for 30 to 35 minutes. Feel free to broil the wings for 2 to 3 minutes in addition to make it crispier.', type: SECTIONS.MAIN, img: broiled1 },
+        { step: 'Dip this chicken in the dressing and serve it with a side of salad and cilantro rice.', type: SECTIONS.SERVE, img: set2 },
     ]
 };
