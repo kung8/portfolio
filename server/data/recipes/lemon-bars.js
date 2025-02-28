913681e16502f97360984bbdf1485a998262f99f
Kevin Ung <ung.kevin78@gmail.com>
2025-02-27 22:22:34 -0700

    reduced lemon bars images

--- server/data/recipes/lemon-bars.js
+++ server/data/recipes/lemon-bars.js
@@ -1,13 +1,8 @@
-const aerial = '../assets/Products/lemon-bars-baked-aerial.jpeg';
 const bar = '../assets/Products/lemon-bars-baked-bar.jpeg';
-const side = '../assets/Products/lemon-bars-baked-side.jpeg';
-const zoomed = '../assets/Products/lemon-bars-baked-zoomed.jpeg';
 const baked = '../assets/Products/lemon-bars-baked.jpeg';
 const dough = '../assets/Products/lemon-bars-dough.jpeg';
-const mixed = '../assets/Products/lemon-bars-dry-ingredients-mixed.jpeg';
 const dry = '../assets/Products/lemon-bars-dry-ingredients.jpeg';
 const eggsMixed = '../assets/Products/lemon-bars-eggs-mixed.jpeg';
-const eggs = '../assets/Products/lemon-bars-eggs.jpeg';
 const flattened = '../assets/Products/lemon-bars-flattened-dough.jpeg';
 const flour = '../assets/Products/lemon-bars-flour-added.jpeg';
 const juiceMixed = '../assets/Products/lemon-bars-lemon-juice-mixed.jpeg';
@@ -15,12 +10,10 @@ const juice = '../assets/Products/lemon-bars-lemon-juice.jpeg';
 const melted = '../assets/Products/lemon-bars-melted-butter.jpeg';
 const plated1 = '../assets/Products/lemon-bars-plated-1.jpeg';
 const plated2 = '../assets/Products/lemon-bars-plated-2.jpeg';
-const plated3 = '../assets/Products/lemon-bars-plated-3.jpeg';
-const plated4 = '../assets/Products/lemon-bars-plated-4.jpeg';
 const rack = '../assets/Products/lemon-bars-rack.jpeg';
 const filling = '../assets/Products/lemon-bars-raw-filling.jpeg';
 const zestMixed = '../assets/Products/lemon-bars-sugar-and-zest-mixed.jpeg';
-const zest = '../assets/Products/lemon-bars-sugar-and-zest.jpeg';
+
 const { GENRES, METHODS, SECTIONS, TYPES, CATEGORIES } = require('./constants');
 const { ALL_PURPOSE_FLOUR, POWDERED_SUGAR, SALT, UNSALTED_BUTTER, LEMON_ZEST, WHITE_SUGAR, EGGS, LEMON_JUICE } = require('./ingredients');
 
@@ -66,19 +59,19 @@ module.exports = {
     directions: [
         { step: 'Preheat the oven to 350ºF.', type: SECTIONS.CRUST },
         { step: 'Line a baking pan with parchment paper.', type: SECTIONS.CRUST },
-        { step: 'In a mixing bowl, combine the flour, powdered sugar and salt.', type: SECTIONS.CRUST, img: [dry, mixed] },
+        { step: 'In a mixing bowl, combine the flour, powdered sugar and salt.', type: SECTIONS.CRUST, img: dry },
         { step: 'Add the butter and mix.', type: SECTIONS.CRUST, img: [melted] },
         { step: 'Pour batter into baking pan and spread the batter until it coats the entire bottom and is an even layer.', type: SECTIONS.CRUST, img: [dough, flattened] },
         { step: 'Bake until it is a light golden color (about 20 minutes).', type: SECTIONS.CRUST, img: bar },
-        { step: 'In a mixing bowl, combine sugar and lemon zest.', type: SECTIONS.FILLING, img: [zest, zestMixed] },
+        { step: 'In a mixing bowl, combine sugar and lemon zest.', type: SECTIONS.FILLING, img: zestMixed },
         { step: 'Add flour and mix.', type: SECTIONS.FILLING, img: flour },
-        { step: 'Add lemon juice and eggs. Mix.', type: SECTIONS.FILLING, img: [eggs, eggsMixed, juice, juiceMixed] },
+        { step: 'Add lemon juice and eggs. Mix.', type: SECTIONS.FILLING, img: [eggsMixed, juice, juiceMixed] },
         { step: 'Pour the filling mixture onto the cooked crust.', type: SECTIONS.MAIN, img: filling },
         { step: 'Bake for 25 minutes (turning the pan about halfway). They will continue to set as they cool.', type: SECTIONS.MAIN },
         { step: 'Remove from the oven and let it cool for an hour.', type: SECTIONS.MAIN },
         { step: 'Chill in the fridge for 2 hours', type: SECTIONS.MAIN },
-        { step: 'Top with powdered sugar.', type: SECTIONS.MAIN, img: [rack, baked, aerial, side, zoomed] },
+        { step: 'Top with powdered sugar.', type: SECTIONS.MAIN, img: [rack, baked] },
         { step: 'Wet a sharp knife between cuts.', type: SECTIONS.MAIN },
-        { step: 'Enjoy these sweet and tart treats.', type: SECTIONS.TOPPINGS, img: [plated1, plated3, plated4] },
+        { step: 'Enjoy these sweet and tart treats.', type: SECTIONS.TOPPINGS, img: plated1 },
     ]
 };
