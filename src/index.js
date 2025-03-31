a56963f5a50190b33080478f6d5653ec4b5519cb
Kevin Ung <ung.kevin78@gmail.com>
2025-03-30 23:27:37 -0600

    fix @tanstack/react-query package

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { ReactQueryDevtools } from "@tanstack/react-query/devtools";
 const queryClient = new QueryClient();
 
 // const environment = process.env.NODE_ENV;
