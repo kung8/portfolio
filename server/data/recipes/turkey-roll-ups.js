34d87e93e5586649e43f4a52f1f6f4e5e7f7b263
Kevin Ung <ung.kevin78@gmail.com>
2025-04-11 20:29:15 -0600

    add turkey roll up images

--- server/data/recipes/turkey-roll-ups.js
+++ server/data/recipes/turkey-roll-ups.js
@@ -1,4 +1,9 @@
-// const example = '../assets/Products/example.jpeg';
+const roll1 = '../assets/Products/turkey-roll-up-1.jpeg';
+const roll2 = '../assets/Products/turkey-roll-up-2.jpeg';
+const roll3 = '../assets/Products/turkey-roll-up-3.jpeg';
+const roll4 = '../assets/Products/turkey-roll-up-4.jpeg';
+const roll5 = '../assets/Products/turkey-roll-up-5.jpeg';
+
 const { CATEGORIES, GENRES, METHODS, PROTEIN, SECTIONS, TYPES, INGREDIENT_UNITS, YIELD_UNITS, TIME_UNITS } = require('./constants');
 const { CREAM_CHEESE, FLOUR_TORTILLA, ROMAINE_LETTUCE, TURKEY, SWISS_CHEESE, SLICER_TOMATO, CRANBERRY_SAUCE } = require('./ingredients');
 
@@ -6,7 +11,7 @@ module.exports = {
     wip: true,
     cardName: 'Turkey Roll Ups',
     name: 'Turkey Roll Ups',
-    img: '',
+    img: roll4,
     recipeAuthor: '',
     recipeFinder: 'Kevin Ung',
     available: true,
@@ -37,12 +42,12 @@ module.exports = {
         { name: 'knife' },
     ],
     directions: [
-        { step: 'In a medium bowl, combine cream cheese and cranberry sauce.', type: SECTIONS.ASSEMBLE },
+        { step: 'In a medium bowl, combine cream cheese and cranberry sauce.', type: SECTIONS.ASSEMBLE, img: roll1 },
         { step: 'On a cutting board, lay a tortilla.', type: SECTIONS.ASSEMBLE },
-        { step: 'Smear the sauce on the tortilla.', type: SECTIONS.ASSEMBLE },
-        { step: 'Layer 1/4 of the ingredients in the center of the tortilla.', type: SECTIONS.ASSEMBLE },
+        { step: 'Smear the sauce on the tortilla. The sauce helps hold the wrap together.', type: SECTIONS.ASSEMBLE, img: roll2 },
+        { step: 'Layer 1/4 of the ingredients in the center of the tortilla.', type: SECTIONS.ASSEMBLE, img: roll3 },
         { step: 'Roll the tortilla tightly.', type: SECTIONS.ASSEMBLE },
         { step: 'Wrap in plastic wrap and chill in the fridge until serving.', type: SECTIONS.CHILL },
-        { step: 'Cut each into 8 equal pieces and enjoy.', type: SECTIONS.SERVE },
+        { step: 'Cut each into 8 equal pieces and enjoy.', type: SECTIONS.SERVE, img: roll5 },
     ]
 };
