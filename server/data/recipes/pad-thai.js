aa473b0e1d53b45f23c1062a53903238de542077
Kevin Ung <ung.kevin78@gmail.com>
2025-04-25 23:27:21 -0600

    correct pad thai recipe

--- server/data/recipes/pad-thai.js
+++ server/data/recipes/pad-thai.js
@@ -17,7 +17,7 @@ const plated3 = '../assets/Products/pad-thai-plated-3.jpeg';
 const rinsing = '../assets/Products/pad-thai-rinsing-noodles.jpeg';
 const sauce = '../assets/Products/pad-thai-sauce.jpeg';
 
-const { CATEGORIES, GENRES, SECTIONS, TYPES, METHODS, PROTEIN , INGREDIENT_UNITS, YIELD_UNITS , TIME_UNITS } = require('./constants');
+const { CATEGORIES, GENRES, SECTIONS, TYPES, METHODS, PROTEIN, INGREDIENT_UNITS, YIELD_UNITS, TIME_UNITS } = require('./constants');
 const {
     UNSALTED_BUTTER,
     GARLIC,
@@ -26,7 +26,6 @@ const {
     BROWN_SUGAR,
     RICE_WINE_VINEGAR,
     LIME,
-    // GREEN_ONION,
     CILANTRO,
     BROWN_RICE_NOODLES,
     RED_BELL_PEPPER,
@@ -38,8 +37,17 @@ const {
     BEAN_SPROUT,
 } = require('./ingredients');
 
-const CHICKEN_AND_VEGGIES = 'Chicken and Veggies';
-const PEANUTS = 'Peanuts';
+const CHICKEN_AND_VEGGIES_SECTION = 'Chicken and Veggies';
+const EGG_SECTION = 'Eggs';
+const NOODLES_SECTION = 'Noodles';
+const SAUCE_SECTION = 'Sauce';
+
+const COOK_CHICKEN_AND_VEGGIES = 'Cook Chicken and Veggies';
+const COOK_NOODLES = 'Cook Noodles';
+const PREP_SAUCE = 'Prep Sauce';
+const COOK_EGGS = 'Cook Eggs';
+const ASSEMBLE = 'Assemble';
+const ROAST_PEANUTS = 'Roast Peanuts';
 
 module.exports = {
     cardName: 'Pad Thai',
@@ -59,18 +67,22 @@ module.exports = {
     cookTime: { amount: 50, unit: TIME_UNITS.MINUTE },
     separated: true,
     ingredients: [
-        { ...BROWN_RICE_NOODLES, amount: 16, unit: INGREDIENT_UNITS.OUNCE, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...UNSALTED_BUTTER, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...GARLIC, amount: 9, unit: INGREDIENT_UNITS.CLOVE, additionalDetails: 'minced', section: SECTIONS.MAIN },
-        { ...CHICKEN_BREAST, amount: 1, unit: INGREDIENT_UNITS.POUND, additionalDetails: 'sliced', section: SECTIONS.MAIN },
-        { ...EGG, amount: 6, unit: '', additionalDetails: '', section: SECTIONS.MAIN },
-        { ...RED_BELL_PEPPER, amount: 4, unit: '', additionalDetails: 'sliced', section: SECTIONS.MAIN },
-        { ...SRIRACHA_SAUCE, amount: 3, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.SAUCE },
-        { ...FISH_SAUCE, amount: 9, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.SAUCE },
-        { ...LIGHT_SOY_SAUCE, amount: 3, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.SAUCE },
-        { ...BROWN_SUGAR, amount: 15, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.SAUCE },
-        { ...RICE_WINE_VINEGAR, amount: 6, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.SAUCE },
-        { ...PEANUT_BUTTER, amount: 6, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.SAUCE },
+        { ...UNSALTED_BUTTER, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: CHICKEN_AND_VEGGIES_SECTION },
+        { ...CHICKEN_BREAST, amount: 1, unit: INGREDIENT_UNITS.POUND, additionalDetails: 'sliced', section: CHICKEN_AND_VEGGIES_SECTION },
+        { ...GARLIC, amount: 9, unit: INGREDIENT_UNITS.CLOVE, additionalDetails: 'minced', section: CHICKEN_AND_VEGGIES_SECTION },
+        { ...RED_BELL_PEPPER, amount: 4, unit: '', additionalDetails: 'sliced', section: CHICKEN_AND_VEGGIES_SECTION },
+
+        { ...BROWN_RICE_NOODLES, amount: 16, unit: INGREDIENT_UNITS.OUNCE, additionalDetails: '', section: NOODLES_SECTION },
+
+        { ...EGG, amount: 6, unit: '', additionalDetails: '', section: EGG_SECTION },
+
+        { ...SRIRACHA_SAUCE, amount: 3, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SAUCE_SECTION },
+        { ...FISH_SAUCE, amount: 9, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SAUCE_SECTION },
+        { ...LIGHT_SOY_SAUCE, amount: 3, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SAUCE_SECTION },
+        { ...BROWN_SUGAR, amount: 15, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SAUCE_SECTION },
+        { ...RICE_WINE_VINEGAR, amount: 6, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SAUCE_SECTION },
+        { ...PEANUT_BUTTER, amount: 6, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SAUCE_SECTION },
+
         { ...UNSALTED_PEANUTS, amount: 2, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.TOPPINGS },
         { ...LIME, amount: 1, unit: '', additionalDetails: 'wedged', section: SECTIONS.TOPPINGS },
         { ...BEAN_SPROUT, amount: 6, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.TOPPINGS },
@@ -88,27 +100,30 @@ module.exports = {
         { name: 'measuring spoons' },
     ],
     directions: [
-        { step: 'Melt 1 Tbsp of butter in a large cooking pan on medium-high heat.', type: CHICKEN_AND_VEGGIES, img: butter },
-        { step: 'Cook the chicken.', type: CHICKEN_AND_VEGGIES, img: [chickenPan, partialChicken] },
-        { step: 'Add garlic and bell peppers to the pan after the chicken cooks a little.', type: CHICKEN_AND_VEGGIES, img: veggiesChicken },
+        { step: 'Over medium-high heat, melt butter in a wok.', type: COOK_CHICKEN_AND_VEGGIES, img: butter },
+        { step: 'Add chicken. Brown the chicken for a few minutes.', type: COOK_CHICKEN_AND_VEGGIES, img: [chickenPan, partialChicken] },
+        { step: 'Add garlic and bell peppers. Cook for a few minutes.', type: COOK_CHICKEN_AND_VEGGIES, img: veggiesChicken },
+        { step: 'Set aside.', type: COOK_CHICKEN_AND_VEGGIES },
 
-        { step: 'Cook rice noodles until tender (about 3 to 5 minutes) in strainer. Rinse noodles under cold water.', type: SECTIONS.NOODLES, img: boilingNoodles },
-        { step: 'Rinse noodles under cold water.', type: SECTIONS.NOODLES, img: [rinsing, noodles] },
+        { step: 'Cook rice noodles until tender (about 3 to 5 minutes) in strainer.', type: COOK_NOODLES, img: boilingNoodles },
+        { step: 'Rinse noodles under cold water.', type: COOK_NOODLES, img: [rinsing, noodles] },
 
-        { step: 'In a medium bowl, mix together the sauce ingredients.', type: SECTIONS.SAUCE, img: sauce },
-        { step: 'Set aside sauce.', type: SECTIONS.SAUCE },
+        { step: 'In a medium bowl, mix together the sauce ingredients.', type: PREP_SAUCE, img: sauce },
+        { step: 'Set aside sauce.', type: PREP_SAUCE },
 
-        { step: 'Over medium-high heat, add a little butter to the wok.', type: SECTIONS.EGGS },
-        { step: 'In a medium bowl, beat eggs.', type: SECTIONS.EGG, img: mixedEggs },
-        { step: 'Pour eggs in wok and cook. Spread the eggs and lift up the edges.', type: SECTIONS.EGG, img: partialEggs },
-        { step: 'Flip and cook for 30 seconds before turning off heat. Continue to let eggs cook with the residual heat.', type: SECTIONS.EGG, img: cookedEggs },
-        { step: 'Remove the eggs from wok and cut eggs into thin strips.', type: SECTIONS.EGG, img: strips },
+        { step: 'Over medium-high heat, add a little butter to the wok.', type: COOK_EGGS },
+        { step: 'In a medium bowl, beat eggs.', type: COOK_EGGS, img: mixedEggs },
+        { step: 'Pour eggs in wok and cook. Spread the eggs and lift up the edges.', type: COOK_EGGS, img: partialEggs },
+        { step: 'Flip and cook for 30 seconds before turning off heat. Continue to let eggs cook with the residual heat.', type: COOK_EGGS, img: cookedEggs },
+        { step: 'Remove the eggs from wok and cut eggs into thin strips.', type: COOK_EGGS, img: strips },
 
-        { step: 'In the wok, mix together noodles, chicken, and vegetables.', type: SECTIONS.MAIN, img: mixed },
-        { step: 'Add eggs.', type: SECTIONS.MAIN, img: mixedNoSauce },
-        { step: 'Add sauce.', type: SECTIONS.MAIN, img: mixedSauce4 },
+        { step: 'In the wok, mix together noodles, chicken, and vegetables.', type: ASSEMBLE, img: mixed },
+        { step: 'Add eggs. Carefully mix.', type: ASSEMBLE, img: mixedNoSauce },
+        { step: 'Add sauce. Mix together.', type: ASSEMBLE, img: mixedSauce4 },
 
-        { step: 'Roast the peanuts in a pan and set aside to top.', type: PEANUTS, img: peanuts },
+        { step: 'Roast the peanuts in a pan.', type: ROAST_PEANUTS, img: peanuts },
+        { step: 'Set aside.', type: ROAST_PEANUTS, img: peanuts },
+
         { step: 'Serve and garnish with peanuts, bean sprouts, cilantro, and squeeze fresh lime juice. Enjoy this majestic Thai dish!', type: SECTIONS.SERVE, img: plated3 },
     ]
 };
