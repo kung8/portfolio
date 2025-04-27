2a0684b3ae7fad9496a0087f35c9e1cada4765f3
Kevin Ung <ung.kevin78@gmail.com>
2025-04-26 18:34:23 -0600

    Revert "add german schnitzel images"
    This reverts commit 61c30d83dab90b24963507b6b9e382bcc872b3fb.

--- server/data/recipes/german-schnitzel.js
+++ server/data/recipes/german-schnitzel.js
@@ -1,29 +1,24 @@
-const schnitzel1 = '../assets/Products/german-schnitzel-1.jpeg';
-const schnitzel2 = '../assets/Products/german-schnitzel-2.jpeg';
-const schnitzel3 = '../assets/Products/german-schnitzel-3.jpeg';
-const schnitzel4 = '../assets/Products/german-schnitzel-4.jpeg';
-const schnitzel5 = '../assets/Products/german-schnitzel-5.jpeg';
-
+// const example = '../assets/Products/example.jpeg';
 const { CATEGORIES, GENRES, METHODS, PROTEIN, SECTIONS, TYPES, INGREDIENT_UNITS, YIELD_UNITS, TIME_UNITS } = require('./constants');
 const { BONELESS_PORK_CHOPS, SALT, BLACK_PEPPER, ALL_PURPOSE_FLOUR, EGG, BREADCRUMBS, VEGETABLE_OIL, LEMON, PARSLEY } = require('./ingredients');
 
 const PORK_CHOPS_SECTION = 'Pork Chops';
 const FLOUR_MIXTURE_SECTION = 'Flour Mixture';
-const EGG_DREDGING_SECTION = 'Egg Dredging';
-const BREADCRUMB_DREDGING_SECTION = 'Breadcrumb Dredging';
+const DREDGING_STATION = 'Dredging Station';
 const FRYING_OIL_SECTION = 'Frying Oil';
 
 const PREP_PORK_CHOPS = 'Prep Pork Chops';
 const FRY_PORK_CHOPS = 'Fry Pork Chops';
 
 module.exports = {
+    wip: true,
     cardName: 'German Schnitzel',
     name: 'German Schnitzel',
-    img: schnitzel5,
+    img: '',
     recipeAuthor: '',
     recipeFinder: 'Kevin Ung',
     available: true,
-    recommended: true,
+    recommended: false,
     category: [CATEGORIES.LUNCH, CATEGORIES.DINNER],
     genre: [GENRES.AUSTRIAN, GENRES.GERMAN],
     method: [METHODS.DEEP_FRY],
@@ -43,8 +38,8 @@ module.exports = {
         { ...BLACK_PEPPER, amount: '', unit: '', additionalDetails: 'to taste', section: PORK_CHOPS_SECTION },
         { ...ALL_PURPOSE_FLOUR, amount: 1 / 2, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: FLOUR_MIXTURE_SECTION },
         { ...SALT, amount: 1, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: FLOUR_MIXTURE_SECTION },
-        { ...EGG, amount: 2, unit: '', additionalDetails: 'lightly beaten', section: EGG_DREDGING_SECTION },
-        { ...BREADCRUMBS, amount: 3 / 4, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: BREADCRUMB_DREDGING_SECTION },
+        { ...EGG, amount: 2, unit: '', additionalDetails: 'lightly beaten', section: DREDGING_STATION },
+        { ...BREADCRUMBS, amount: 3 / 4, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: DREDGING_STATION },
         { ...VEGETABLE_OIL, amount: '', unit: '', additionalDetails: 'for frying', section: FRYING_OIL_SECTION },
         { ...LEMON, amount: '', unit: '', additionalDetails: 'sliced', section: SECTIONS.TOPPINGS },
         { ...PARSLEY, amount: '', unit: '', additionalDetails: '', section: SECTIONS.TOPPINGS },
@@ -58,19 +53,17 @@ module.exports = {
         { name: 'pan' },
         { name: '3 small bowls' },
         { name: 'paper towels' },
-        { name: 'baking sheet' },
-        { name: 'wire rack' },
+        { name: 'plates' },
     ],
     directions: [
         { step: 'Place pork chops between two pieces of plastic wrap.', type: PREP_PORK_CHOPS },
         { step: 'Pound the chops to 1/4 inch thick with a meat mallet.', type: PREP_PORK_CHOPS },
         { step: 'Season both sides with salt and pepper.', type: PREP_PORK_CHOPS },
         { step: 'Heat a little less than 1/4 inch of oil in a pan. Bring the oil to 330ºF.', type: FRY_PORK_CHOPS },
-        { step: 'Line a baking sheet with paper towels and place a wire rack on top.', type: FRY_PORK_CHOPS },
-        { step: 'Prepare dredging station bowls: (1) "Flour Mixture", (2) eggs, and (3) breadcrumbs.', type: FRY_PORK_CHOPS, img: schnitzel1 },
-        { step: 'Dredge both sides of the pork chops in flour, eggs and breadcrumbs. Avoid pressing the breadcrumbs into the meat. Shake off excess breadcrumbs. Fry immediately.', type: FRY_PORK_CHOPS, img: schnitzel2 },
-        { step: 'Fry the schnitzel until golden brown (about 2 to 3 minutes each side).', type: FRY_PORK_CHOPS, img: schnitzel3 },
-        { step: 'Transfer to the wire rack to drain excess oil.', type: FRY_PORK_CHOPS, img: schnitzel4 },
+        { step: 'Prepare dredging station bowls: (1) "Flour Mixture", (2) eggs, and (3) breadcrumbs.', type: FRY_PORK_CHOPS },
+        { step: 'Dredge both sides of the pork chops in flour, eggs and breadcrumbs. Avoid pressing the breadcrumbs into the meat. Shake off excess breadcrumbs. Fry immediately.', type: FRY_PORK_CHOPS },
+        { step: 'Fry the schnitzel until golden brown (about 2 to 3 minutes each side).', type: FRY_PORK_CHOPS },
+        { step: 'Transfer to a paper towel-lined plate to drain excess oil.', type: FRY_PORK_CHOPS },
         { step: 'Top with lemon slices and parsley. Optionally serve with fries.', type: SECTIONS.SERVE },
     ]
 };
