6c3e5eb059f187c221132a0db8856f70facfee65
Kevin Ung <ung.kevin78@gmail.com>
2025-04-28 20:51:46 -0600

    add fish and chips images

--- server/data/recipes/fish-and-chips.js
+++ server/data/recipes/fish-and-chips.js
@@ -1,4 +1,10 @@
-// const example = '../assets/Products/example.jpeg';
+const chips1 = '../assets/Products/fish-and-chips-1.jpeg';
+const chips2 = '../assets/Products/fish-and-chips-2.jpeg';
+const chips3 = '../assets/Products/fish-and-chips-3.jpeg';
+const chips4 = '../assets/Products/fish-and-chips-4.jpeg';
+const chips5 = '../assets/Products/fish-and-chips-5.jpeg';
+const chips6 = '../assets/Products/fish-and-chips-6.jpeg';
+
 const { CATEGORIES, GENRES, METHODS, PROTEIN, SECTIONS, TYPES, INGREDIENT_UNITS, YIELD_UNITS, TIME_UNITS } = require('./constants');
 const { COD_FISH, SALT, BLACK_PEPPER, ALL_PURPOSE_FLOUR, GARLIC_POWDER, PAPRIKA, EGG, BEER, VEGETABLE_OIL } = require('./ingredients');
 
@@ -9,10 +15,9 @@ const PREP_FISH = 'Prep Fish';
 const DEEP_FRY = 'Deep Fry';
 
 module.exports = {
-    wip: true,
     cardName: 'Fish and Chips',
     name: 'Fish and Chips',
-    img: '',
+    img: chips6,
     recipeAuthor: 'Shawn',
     recipeFinder: 'Kevin Ung',
     available: true,
@@ -54,12 +59,12 @@ module.exports = {
         { step: 'Heat about 2 to 3 inches of oil in a deep fryer or in a pot. Bring it to 375ºF.', type: PREP_FISH },
         { step: 'Cut the fish to 1 inch wide and 3 inches long.', type: PREP_FISH },
         { step: 'Pat dry with a paper towel.', type: PREP_FISH },
-        { step: 'Season with salt and pepper.', type: PREP_FISH },
+        { step: 'Season with salt and pepper.', type: PREP_FISH, img: chips1 },
         { step: 'Line a baking sheet with paper towels and place a wire rack on top.', type: PREP_FISH },
-        { step: 'In a mixing bowl, combine the "Beer Batter" section ingredients until it is smooth.', type: PREP_FISH },
-        { step: 'Dip the fish into the beer batter, one at a time, then immediately into the hot oil. Be carefully to not crowd the oil.', type: PREP_FISH },
-        { step: 'Deep fry the fish until they are golden brown (about 3 to 4 minutes).', type: DEEP_FRY },
-        { step: 'Transfer the fish to the wire rack to drip.', type: DEEP_FRY },
+        { step: 'In a mixing bowl, combine the "Beer Batter" section ingredients until it is smooth.', type: PREP_FISH, img: chips2 },
+        { step: 'Dip the fish into the beer batter, one at a time, then immediately into the hot oil. Be carefully to not crowd the oil.', type: PREP_FISH, img: chips3 },
+        { step: 'Deep fry the fish until they are golden brown (about 3 to 4 minutes).', type: DEEP_FRY, img: chips4 },
+        { step: 'Transfer the fish to the wire rack to drip.', type: DEEP_FRY, img: chips5 },
         { step: 'Enjoy the fish with your favorite type of fries.', type: SECTIONS.SERVE },
     ]
 };
