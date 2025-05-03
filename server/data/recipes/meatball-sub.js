ab40e10e04cf8db555a9a894f6fdcf392e638ac5
Kevin Ung <ung.kevin78@gmail.com>
2025-05-02 21:54:18 -0600

    add meatball sub images

--- server/data/recipes/meatball-sub.js
+++ server/data/recipes/meatball-sub.js
@@ -1,4 +1,12 @@
-// const example = '../assets/Products/example.jpeg';
+const sub1 = '../assets/Products/meatball-sub-1.jpeg';
+const sub2 = '../assets/Products/meatball-sub-2.jpeg';
+const sub3 = '../assets/Products/meatball-sub-3.jpeg';
+const sub4 = '../assets/Products/meatball-sub-4.jpeg';
+const sub5 = '../assets/Products/meatball-sub-5.jpeg';
+const sub6 = '../assets/Products/meatball-sub-6.jpeg';
+const sub7 = '../assets/Products/meatball-sub-7.jpeg';
+const sub8 = '../assets/Products/meatball-sub-8.jpeg';
+
 const { CATEGORIES, GENRES, METHODS, PROTEIN, SECTIONS, TYPES, INGREDIENT_UNITS, YIELD_UNITS, TIME_UNITS } = require('./constants');
 const { TOMATO_SAUCE, HOAGIE_BREAD, MOZZARELLA_CHEESE, GROUND_BEEF, EGG, BREADCRUMBS, ITALIAN_SEASONING, ONION_POWDER, SALT, BLACK_PEPPER } = require('./ingredients');
 const { DAIRY } = require('./ingredient-categories');
@@ -13,14 +21,13 @@ const PREP_ROLLS = 'Prep Rolls';
 const ASSEMBLE_SUBS = 'Assemble Subs';
 
 module.exports = {
-    wip: true,
     cardName: 'Meatball Sub',
     name: 'Meatball Sub',
-    img: '',
+    img: sub8,
     recipeAuthor: '',
     recipeFinder: 'Kevin Ung',
     available: true,
-    recommended: false,
+    recommended: true,
     category: [CATEGORIES.LUNCH, CATEGORIES.DINNER],
     genre: [GENRES.AMERICAN, GENRES.ITALIAN],
     method: [METHODS.BAKE, METHODS.PAN_FRY],
@@ -42,7 +49,7 @@ module.exports = {
         { ...ONION_POWDER, amount: 1 / 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: MEATBALL_SECTION },
         { ...SALT, amount: '', unit: '', additionalDetails: 'to taste', section: MEATBALL_SECTION },
         { ...BLACK_PEPPER, amount: '', unit: '', additionalDetails: 'to taste', section: MEATBALL_SECTION },
-        
+
         { ...TOMATO_SAUCE, amount: 2, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SUB_SECTION },
         { ...GARLIC_BUTTER, amount: 4, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SUB_SECTION, link: { id: 'garlic-butter', url: 'recipes/garlic-butter' } },
         { ...HOAGIE_BREAD, amount: 4, unit: '', additionalDetails: '', section: SUB_SECTION },
@@ -63,14 +70,17 @@ module.exports = {
         { step: 'Preheat oven to 350ºF.', type: PREP_MEATBALLS },
         { step: 'In a mixing bowl, combine the "Meatball" section ingredients.', type: PREP_MEATBALLS },
         { step: 'Line a baking sheet with parchment paper.', type: PREP_MEATBALLS },
-        { step: 'Form 1 inch balls and place on sheet.', type: PREP_MEATBALLS },
-        { step: 'Bake until cooked through (about 20 minutes).', type: PREP_MEATBALLS },
-        { step: 'Over medium-low heat, add meatball and pasta sauce in a saucepan.', type: PREP_SAUCE },
+        { step: 'Form 1 inch balls and place on sheet.', type: PREP_MEATBALLS, img: sub1 },
+        { step: 'Bake until cooked through (about 20 minutes).', type: PREP_MEATBALLS, img: sub3 },
+        { step: 'Over medium-low heat, add meatball and pasta sauce in a saucepan.', type: PREP_SAUCE, img: sub4 },
         { step: 'Cover and simmer for 10 minutes.', type: PREP_SAUCE },
-        { step: 'Spread garlic butter inside roll.', type: PREP_ROLLS },
-        { step: 'Place rolls in a baking pan and broil until golden (about 3 minutes).', type: PREP_ROLLS },
-        { step: 'Add 4 meatballs in each roll with a little sauce and cheese.', type: ASSEMBLE_SUBS },
-        { step: 'Broil until cheese is melted (about 2 to 3 minutes).', type: ASSEMBLE_SUBS },
+        { step: 'Spread garlic butter inside roll.', type: PREP_ROLLS, img: sub2 },
+        { step: 'Place rolls in a baking pan and broil until golden (about 2 minutes).', type: PREP_ROLLS, img: sub5 },
+        { step: 'Add 3 meatballs in each roll with a little sauce and cheese.', type: ASSEMBLE_SUBS, img: sub6 },
+        { step: 'Broil until cheese is melted (about 2 minutes).', type: ASSEMBLE_SUBS, img: sub7 },
         { step: 'Serve with more sauce and cheese.', type: SECTIONS.SERVE },
+    ],
+    notes: [
+        { note: 'The original recipe says to broil for 3 minutes, but it burned the bread pretty badly. I recommend starting it at 2 minutes.' },
     ]
 };
