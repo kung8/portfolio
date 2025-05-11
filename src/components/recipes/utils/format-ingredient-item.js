4604dddb0658a716130d426b0e92cee614c52484
Kevin Ung <ung.kevin78@gmail.com>
2025-05-11 00:54:53 -0600

    fix pluralization fallback

--- src/components/recipes/utils/format-ingredient-item.js
+++ src/components/recipes/utils/format-ingredient-item.js
@@ -10,13 +10,12 @@ export const formatIngredientItem = (item) => {
     const unitlessPlural = !unit && isPlural;
     const pluralName = item.plural;
     const nameNotEndWithS = item.name[item.name.length - 1] !== 's';
-    const name = unitlessPlural ? (
-        pluralName ? pluralName : (nameNotEndWithS ? item.name + 's' : item.name)
-    ) : item.name;
+    const finalPluralName = pluralName ? pluralName : (nameNotEndWithS ? item.name + 's' : item.name);
+    const name = unitlessPlural ? finalPluralName : item.name;
 
     // if the unit is "small", "medium", or "large", we need to check if it is plural and then pluralize the name
     if (unit.trim() == 'small' || unit.trim() == 'medium' || unit.trim() == 'large') {
-        return amount + unit + (isPlural ? pluralName : item.name);
+        return amount + unit + (isPlural ? finalPluralName : item.name);
     }
 
     return amount + unit + name;
