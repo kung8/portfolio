1ba6d47120197e1f4457d3316ad9e7cf99c30caa
Kevin Ung <ung.kevin78@gmail.com>
2025-05-11 00:41:54 -0600

    pluralize the name when the unit is small, medium or large

--- src/components/recipes/utils/format-ingredient-item.js
+++ src/components/recipes/utils/format-ingredient-item.js
@@ -5,13 +5,19 @@ export const formatIngredientItem = (item) => {
     const isPlural = item.amount > 1;
     const referencedUnit = isPlural ? item.unit?.p + ' ' : item.unit?.s + ' ';
     const unit = item.unit ? referencedUnit : '';
-    
-    // if the unit doesn't exist, we need to check if the is plural and determine which name to use
+
+    // if the unit doesn't exist, we need to check if it is plural and determine which name to use
     const unitlessPlural = !unit && isPlural;
     const pluralName = item.plural;
     const nameNotEndWithS = item.name[item.name.length - 1] !== 's';
     const name = unitlessPlural ? (
         pluralName ? pluralName : (nameNotEndWithS ? item.name + 's' : item.name)
     ) : item.name;
+
+    // if the unit is "small", "medium", or "large", we need to check if it is plural and then pluralize the name
+    if (unit.trim() == 'small' || unit.trim() == 'medium' || unit.trim() == 'large') {
+        return amount + unit + (isPlural ? pluralName : item.name);
+    }
+
     return amount + unit + name;
 }
