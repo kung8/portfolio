8258ceb6a1854f6a5e07e4e1026299661844fe9c
Kevin Ung <ung.kevin78@gmail.com>
2025-05-31 12:43:57 -0600

    add lomo saltado images

--- server/data/recipes/lomo-saltado.js
+++ server/data/recipes/lomo-saltado.js
@@ -1,16 +1,23 @@
-// const example = '../assets/Products/example.jpeg';
+const saltado1 = '../assets/Products/lomo-saltado-1.jpeg';
+const saltado2 = '../assets/Products/lomo-saltado-2.jpeg';
+const saltado3 = '../assets/Products/lomo-saltado-3.jpeg';
+const saltado4 = '../assets/Products/lomo-saltado-4.jpeg';
+const saltado5 = '../assets/Products/lomo-saltado-5.jpeg';
+const saltado6 = '../assets/Products/lomo-saltado-6.jpeg';
+const saltado7 = '../assets/Products/lomo-saltado-7.jpeg';
+const saltado8 = '../assets/Products/lomo-saltado-8.jpeg';
+
 const { CATEGORIES, GENRES, METHODS, PROTEIN, SECTIONS, TYPES, INGREDIENT_UNITS, YIELD_UNITS, TIME_UNITS } = require('./constants');
 const { BEEF_SIRLOIN, LIGHT_SOY_SAUCE, GARLIC, OYSTER_SAUCE, RED_WINE_VINEGAR, AJI_AMARILLO_PASTE, CORNSTARCH, RED_ONION, ROMA_TOMATO, GREEN_ONION, VEGETABLE_OIL, CILANTRO, WHITE_RICE, FRENCH_FRIES } = require('./ingredients');
 
 module.exports = {
-    wip: true,
     cardName: 'Lomo Saltado',
     name: 'Lomo Saltado',
-    img: '',
+    img: saltado8,
     recipeAuthor: '',
     recipeFinder: 'Kevin Ung',
     available: true,
-    recommended: false,
+    recommended: true,
     category: [CATEGORIES.LUNCH, CATEGORIES.DINNER],
     genre: [GENRES.PERUVIAN],
     method: [METHODS.STIR_FRY],
@@ -34,7 +41,7 @@ module.exports = {
         { ...AJI_AMARILLO_PASTE, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.SAUCE },
         { ...CORNSTARCH, amount: 1, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.SAUCE },
 
-        { ...VEGETABLE_OIL, amount: '', unit: '', additionalDetails: '', section: SECTIONS.STIR_FRY },
+        { ...VEGETABLE_OIL, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.STIR_FRY },
         { ...RED_ONION, amount: 1, unit: INGREDIENT_UNITS.MEDIUM, additionalDetails: 'thinly sliced', section: SECTIONS.STIR_FRY },
         { ...GARLIC, amount: 3, unit: INGREDIENT_UNITS.CLOVE, additionalDetails: 'minced', section: SECTIONS.STIR_FRY },
         { ...ROMA_TOMATO, amount: 3, unit: '', additionalDetails: 'thinly sliced', section: SECTIONS.STIR_FRY },
@@ -53,19 +60,19 @@ module.exports = {
         { name: 'wok' },
     ],
     directions: [
-        { step: `In a mixing bowl, combine the "${SECTIONS.BEEF}" section ingredients.`, section: SECTIONS.MARINATE_BEEF },
+        { step: `In a mixing bowl, combine the "${SECTIONS.BEEF}" section ingredients.`, section: SECTIONS.MARINATE_BEEF, img: saltado1 },
         { step: 'Let it marinate for 5 to 7 minutes.', section: SECTIONS.MARINATE_BEEF },
 
         { step: `In a small bowl, combine the "${SECTIONS.SAUCE}" section ingredients. Stir and set aside.`, section: SECTIONS.PREP_SAUCE },
 
         { step: 'Over medium-high heat, add 1 tablespoon of oil to a wok.', section: SECTIONS.STIR_FRY },
-        { step: 'Add the beef. Sear both sides of the beef (about 1 to 2 minutes). Cook in batches if needed and add more between batches as needed.', section: SECTIONS.STIR_FRY },
+        { step: 'Add the beef. Sear both sides of the beef (about 1 to 2 minutes). Cook in batches if needed and add more between batches as needed. If you want more browned beef, you may not want to pour in the marinade with it.', section: SECTIONS.STIR_FRY, img: saltado2 },
         { step: 'Remove beef from pan. Set aside.', section: SECTIONS.STIR_FRY },
-        { step: 'Add onions. Sauté for 1 minute.', section: SECTIONS.STIR_FRY },
-        { step: 'Add garlic and tomatoes. Sauté for 1 minute.', section: SECTIONS.STIR_FRY },
-        { step: 'Re-add the beef.', section: SECTIONS.STIR_FRY },
-        { step: 'If you want the french fries in the sauce, add the french fries now. Or leave out if you want it to be crispy.', section: SECTIONS.STIR_FRY },
-        { step: 'Add the sauce and combine until everything is coated.', section: SECTIONS.STIR_FRY },
+        { step: 'Add onions. Sauté for 1 minute.', section: SECTIONS.STIR_FRY, img: saltado3 },
+        { step: 'Add garlic and tomatoes. Sauté for 1 minute.', section: SECTIONS.STIR_FRY, img: saltado4 },
+        { step: 'Re-add the beef.', section: SECTIONS.STIR_FRY, img: saltado6 },
+        { step: 'If you want the french fries in the sauce, add the french fries now. Or leave out if you want it to be crispy.', section: SECTIONS.STIR_FRY, img: saltado5 },
+        { step: 'Add the sauce and combine until everything is coated.', section: SECTIONS.STIR_FRY, img: saltado7 },
 
         { step: 'Serve this delicious dish with rice and crispy french fries. Top with green onions and cilantro.', section: SECTIONS.SERVE },
     ]
