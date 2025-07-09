5a5dbfe3955dba6beb484f00271ef4136a8a5585
Kevin Ung <ung.kevin78@gmail.com>
2025-07-08 22:52:03 -0600

    update macaroni salad recipe and images

--- server/data/recipes/macaroni-salad.js
+++ server/data/recipes/macaroni-salad.js
@@ -1,4 +1,11 @@
-const macaroni = '../assets/Products/macaroni-salad.jpeg';
+const salad1 = '../assets/Products/macaroni-salad-1.jpeg';
+const salad2 = '../assets/Products/macaroni-salad-2.jpeg';
+const salad3 = '../assets/Products/macaroni-salad-3.jpeg';
+const salad4 = '../assets/Products/macaroni-salad-4.jpeg';
+const salad5 = '../assets/Products/macaroni-salad-5.jpeg';
+const salad6 = '../assets/Products/macaroni-salad-6.jpeg';
+// const salad7 = '../assets/Products/macaroni-salad-7.jpeg';
+
 const { CATEGORIES, SECTIONS, GENRES, TYPES, METHODS, INGREDIENT_UNITS, YIELD_UNITS, TIME_UNITS, STORAGE_DURATION_UNIT, STORAGE_LOCATION, STORAGE_CONTAINER, REHEAT_METHODS } = require('./constants');
 const {
     ELBOW_MACARONI,
@@ -13,10 +20,12 @@ const {
     GREEN_BELL_PEPPER,
 } = require('./ingredients');
 
+const MAYO_MIXTURE = 'Mayo Mixture';
+
 module.exports = {
     cardName: 'Macaroni Salad',
     name: 'Macaroni Salad',
-    img: macaroni,
+    img: salad5,
     recipeAuthor: '',
     recipeFinder: 'Kevin Ung',
     available: true,
@@ -28,17 +37,20 @@ module.exports = {
     yields: { amount: 10, unit: YIELD_UNITS.SERVING },
     prepTime: { amount: 20, unit: TIME_UNITS.MINUTE },
     cookTime: { amount: 10, unit: TIME_UNITS.MINUTE },
+    separated: true,
     ingredients: [
-        { ...ELBOW_MACARONI, amount: 4, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', optional: false },
-        { ...MAYONNAISE, amount: 1, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', optional: false },
-        { ...WHITE_VINEGAR, amount: 1 / 4, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', optional: false },
-        { ...WHITE_SUGAR, amount: 2 / 3, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', optional: false },
-        { ...YELLOW_MUSTARD, amount: 5 / 2, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', optional: false },
-        { ...SALT, amount: 3 / 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', optional: false },
-        { ...BLACK_PEPPER, amount: 1 / 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', optional: false },
-        { ...YELLOW_ONION, amount: 1, unit: INGREDIENT_UNITS.MEDIUM, additionalDetails: 'diced', optional: false },
-        { ...CELERY, amount: 2, unit: INGREDIENT_UNITS.STALK, additionalDetails: 'sliced', optional: false },
-        { ...GREEN_BELL_PEPPER, amount: 1, unit: '', additionalDetails: 'diced', optional: false },
+        { ...ELBOW_MACARONI, amount: 4, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.PASTA },
+
+        { ...MAYONNAISE, amount: 1, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: MAYO_MIXTURE },
+        { ...WHITE_VINEGAR, amount: 1 / 4, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: MAYO_MIXTURE },
+        { ...WHITE_SUGAR, amount: 2 / 3, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: MAYO_MIXTURE },
+        { ...YELLOW_MUSTARD, amount: 5 / 2, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: MAYO_MIXTURE },
+        { ...SALT, amount: 3 / 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: MAYO_MIXTURE },
+        { ...BLACK_PEPPER, amount: 1 / 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: MAYO_MIXTURE },
+
+        { ...YELLOW_ONION, amount: 1, unit: INGREDIENT_UNITS.MEDIUM, additionalDetails: 'diced', section: SECTIONS.VEGGIES },
+        { ...CELERY, amount: 2, unit: INGREDIENT_UNITS.STALK, additionalDetails: 'sliced', section: SECTIONS.VEGGIES },
+        { ...GREEN_BELL_PEPPER, amount: 1, unit: '', additionalDetails: 'diced', section: SECTIONS.VEGGIES },
     ],
     appliances: [
         { name: 'stove' },
@@ -50,13 +62,17 @@ module.exports = {
         { name: 'strainer' },
     ],
     directions: [
-        { step: 'Over a high heat boil water and salt in a large pot.', section: SECTIONS.MAIN },
-        { step: 'Once the water boils, add the pasta and cook for about 8 - 10 minutes or until tender.', section: SECTIONS.MAIN },
-        { step: 'Rinse the pasta under cold water and drain in colander.', section: SECTIONS.MAIN },
-        { step: 'In the mixing bowl, combine the mayo, vinegar, sugar, mustard, salt, and pepper until well mixed.', section: SECTIONS.MAIN },
-        { step: 'Then combine the onions, celery, green pepper, and pasta.', section: SECTIONS.MAIN },
-        { step: 'Best to refrigerate at least 4 hours (or even overnight).', section: SECTIONS.MAIN },
-        { step: 'Enjoy this sweet, creamy, fresh appetizer while you relax with some relaxing Hawaiian music.', section: SECTIONS.MAIN },
+        { step: 'Over high heat, boil water and salt in a large pot.', section: SECTIONS.COOK_PASTA },
+        { step: 'Once the water boils, add the pasta and cook until tender (about 8 to 10 minutes).', section: SECTIONS.COOK_PASTA },
+        { step: 'Rinse the pasta under cold water.', section: SECTIONS.COOK_PASTA },
+        { step: 'Drain in colander.', section: SECTIONS.COOK_PASTA, img: salad1 },
+
+        { step: `In the mixing bowl, combine the "${MAYO_MIXTURE}" section ingredients.`, section: SECTIONS.ASSEMBLE, img: salad2 },
+        { step: `Add the "${SECTIONS.VEGGIES}" section ingredients. Mix until coated.`, section: SECTIONS.ASSEMBLE, img: salad3 },
+        { step: 'Add the pasta. Mix until coated.', section: SECTIONS.ASSEMBLE, img: salad4 },
+        { step: 'Cover. Chill overnight (or at least 4 hours).', section: SECTIONS.ASSEMBLE, img: salad6 },
+
+        { step: 'Enjoy this sweet, creamy, fresh appetizer while you relax with some relaxing Hawaiian music.', section: SECTIONS.SERVE },
     ],
     store: [
         {
