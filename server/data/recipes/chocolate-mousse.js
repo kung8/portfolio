2ad18ac6c4a19d76d429dd0b257e49e1a516edc3
Kevin Ung <ung.kevin78@gmail.com>
2025-07-09 22:47:46 -0600

    update mousse recipe

--- server/data/recipes/chocolate-mousse.js
+++ server/data/recipes/chocolate-mousse.js
@@ -1,23 +1,25 @@
-const pieces = '../assets/Products/chocolate-mousse-chocolate-pieces.jpeg';
-const raspberries = '../assets/Products/chocolate-mousse-clear-raspberries.jpeg';
-const cream = '../assets/Products/chocolate-mousse-cream.jpeg';
-const cups = '../assets/Products/chocolate-mousse-cups.jpeg';
-const whites = '../assets/Products/chocolate-mousse-egg-whites.jpeg';
-const yolksMixed = '../assets/Products/chocolate-mousse-egg-yolks-mixed.jpeg';
-const yolks = '../assets/Products/chocolate-mousse-egg-yolks.jpeg';
-const chocolate = '../assets/Products/chocolate-mousse-folded-chocolate.jpeg';
-const foldedWhites = '../assets/Products/chocolate-mousse-folded-whites.jpeg';
-const foldedYolk = '../assets/Products/chocolate-mousse-folded-yolk.jpeg';
-const foldedYolk2 = '../assets/Products/chocolate-mousse-folded-yolk-2.jpeg';
-const melted = '../assets/Products/chocolate-mousse-melted-chocolate.jpeg';
-const mixedChocolate = '../assets/Products/chocolate-mousse-mixed-chocolate.jpeg';
-const mousse = '../assets/Products/chocolate-mousse-mixed-mousse.jpeg';
-const plated = '../assets/Products/chocolate-mousse-plated.jpeg';
-const shaved = '../assets/Products/chocolate-mousse-shaved-chocolate.jpeg';
-const peaks = '../assets/Products/chocolate-mousse-stiff-peaks.jpeg';
-const topped = '../assets/Products/chocolate-mousse-topped.jpeg';
-const whipped = '../assets/Products/chocolate-mousse-whipped-cream.jpeg';
-const yolkInCream = '../assets/Products/chocolate-mousse-yolk-in-cream.jpeg';
+const mousse1 = '../assets/Products/chocolate-mousse-1.jpeg';
+const mousse2 = '../assets/Products/chocolate-mousse-2.jpeg';
+const mousse3 = '../assets/Products/chocolate-mousse-3.jpeg';
+const mousse4 = '../assets/Products/chocolate-mousse-4.jpeg';
+const mousse5 = '../assets/Products/chocolate-mousse-5.jpeg';
+const mousse6 = '../assets/Products/chocolate-mousse-6.jpeg';
+const mousse7 = '../assets/Products/chocolate-mousse-7.jpeg';
+const mousse8 = '../assets/Products/chocolate-mousse-8.jpeg';
+const mousse9 = '../assets/Products/chocolate-mousse-9.jpeg';
+const mousse10 = '../assets/Products/chocolate-mousse-10.jpeg';
+const mousse11 = '../assets/Products/chocolate-mousse-11.jpeg';
+const mousse12 = '../assets/Products/chocolate-mousse-12.jpeg';
+const mousse13 = '../assets/Products/chocolate-mousse-13.jpeg';
+const mousse14 = '../assets/Products/chocolate-mousse-14.jpeg';
+const mousse15 = '../assets/Products/chocolate-mousse-15.jpeg';
+const mousse16 = '../assets/Products/chocolate-mousse-16.jpeg';
+const mousse17 = '../assets/Products/chocolate-mousse-17.jpeg';
+const mousse18 = '../assets/Products/chocolate-mousse-18.jpeg';
+const mousse19 = '../assets/Products/chocolate-mousse-19.jpeg';
+const mousse20 = '../assets/Products/chocolate-mousse-20.jpeg';
+const mousse21 = '../assets/Products/chocolate-mousse-21.jpeg';
+
 const { GENRES, METHODS, PROTEIN, SECTIONS, TYPES, CATEGORIES, INGREDIENT_UNITS, YIELD_UNITS, TIME_UNITS, STORAGE_DURATION_UNIT, STORAGE_LOCATION, STORAGE_CONTAINER } = require('./constants');
 const {
     EGG,
@@ -31,11 +33,12 @@ const {
 
 const CHOCOLATE = 'Chocolate';
 const WHIPPED_CREAM = 'Whipped Cream';
+const SWEETENER_SECTION = 'Sweetener';
 
 module.exports = {
     cardName: 'Chocolate Mousse',
     name: 'Chocolate Mousse',
-    img: topped,
+    img: mousse21,
     recipeAuthor: '',
     recipeFinder: 'Kevin Ung',
     available: true,
@@ -51,13 +54,18 @@ module.exports = {
     ],
     separated: true,
     ingredients: [
-        { ...EGG, amount: 3, unit: '', additionalDetails: '', section: SECTIONS.MAIN },
-        { ...HEAVY_CREAM, amount: 1 / 2, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...WHITE_SUGAR, amount: 3, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.MAIN },
         { ...DARK_COOKING_CHOCOLATE, amount: 4, unit: INGREDIENT_UNITS.OUNCE, additionalDetails: '', section: CHOCOLATE },
         { ...UNSALTED_BUTTER, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: CHOCOLATE },
+        
+        { ...EGG, amount: 3, unit: '', additionalDetails: '', section: SECTIONS.MAIN },
+        { ...HEAVY_CREAM, amount: 1 / 2, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.MAIN },
+        { ...WHITE_SUGAR, amount: 4, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.MAIN },
+        
+        { ...WHITE_SUGAR, amount: 2, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SWEETENER_SECTION },
+
         { ...HEAVY_CREAM, amount: 1 / 2, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: WHIPPED_CREAM },
         { ...WHITE_SUGAR, amount: 3, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: WHIPPED_CREAM },
+
         { ...SEMI_SWEET_CHOCOLATE_CHIPS, amount: '', unit: '', additionalDetails: 'finely chopped', section: SECTIONS.TOPPINGS },
         { ...RASPBERRY, amount: '', unit: '', additionalDetails: 'washed', section: SECTIONS.TOPPINGS },
     ],
@@ -73,22 +81,23 @@ module.exports = {
         { name: 'serving bowls or cups' },
     ],
     directions: [
-        { step: 'Break up dark cooking chocolate bar into pieces and place in a microwavable bowl.', section: CHOCOLATE, img: pieces },
+        { step: 'Break up dark cooking chocolate bar into pieces and place in a microwavable bowl.', section: CHOCOLATE, img: mousse1 },
         { step: 'Add butter into the chocolate bar.', section: CHOCOLATE },
-        { step: 'Microwave the chocolate for 30 second interval and stir in between. Continue this until the chocolate and butter have completely melted and become uniform. Set aside.', section: CHOCOLATE, img: melted },
-        { step: 'While the eggs are cold, separate the egg whites and yolk. Place the yolks in one bowl and whites in another.', section: SECTIONS.MAIN, img: [whites, yolks] },
-        { step: 'Whisk the yolks until combined.', section: SECTIONS.MAIN, img: yolksMixed },
-        { step: 'Beat the heavy cream and sugar in a mixing bowl until stiff peaks form. I did not have a mixer so I used the old-fashion manual whisk and arm muscles to mix this together. You will want a mixer if you do not want tired arms.', section: SECTIONS.MAIN, img: [cream, peaks] },
-        { step: 'Fold egg yolks into the cream (about 8 to 10 folds). Streaks will still exist.', section: SECTIONS.MAIN, img: [yolkInCream, foldedYolk, foldedYolk2] },
+        { step: 'Microwave the chocolate for 30 second interval and stir in between. Continue this until the chocolate and butter have completely melted and become uniform. Set aside.', section: CHOCOLATE, img: mousse2 },
+        { step: 'While the eggs are cold, separate the egg whites and yolk. Place the yolks in one bowl and whites in another.', section: SECTIONS.MAIN, img: [mousse3, mousse4] },
+        { step: 'Whisk the yolks until combined.', section: SECTIONS.MAIN, img: mousse5 },
+        { step: 'Beat the heavy cream and sugar in a mixing bowl until stiff peaks form. I did not have a mixer so I used the old-fashion manual whisk and arm muscles to mix this together. You will want a mixer if you do not want tired arms.', section: SECTIONS.MAIN, img: [mousse6, mousse7] },
+        { step: 'Fold egg yolks into the cream (about 8 to 10 folds). Streaks will still exist.', section: SECTIONS.MAIN, img: [mousse8, mousse9, mousse10] },
         { step: 'Check the chocolate is still warm and runny. If it is not, microwave the chocolate for 5 second intervals and stir in between until chocolate is ready.', section: SECTIONS.MAIN },
-        { step: 'Fold chocolate into the cream mixture (about 8 to 10 folds).', section: SECTIONS.MAIN, img: [chocolate, mixedChocolate] },
-        { step: 'Fold in 1/4 of the egg whites into the chocolate mixture (about 10 folds).', section: SECTIONS.MAIN, img: foldedWhites },
-        { step: 'Pour the remaining egg whites into the chocolate mixture and fold until no egg whites are visible. The egg whites might not look like it is going to combine but it will.', section: SECTIONS.MAIN, img: mousse },
-        { step: 'Divide the mousse into serving containers.', section: SECTIONS.MAIN, img: cups },
+        { step: 'Fold chocolate into the cream mixture (about 8 to 10 folds).', section: SECTIONS.MAIN, img: [mousse11, mousse12] },
+        { step: 'Fold in 1/4 of the egg whites into the chocolate mixture (about 10 folds).', section: SECTIONS.MAIN, img: mousse13 },
+        { step: 'Pour the remaining egg whites into the chocolate mixture and fold until no egg whites are visible. The egg whites might not look like it is going to combine but it will.', section: SECTIONS.MAIN, img: mousse14 },
+        { step: `Add the "${SWEETENER_SECTION}" section ingredients to the mixture. This balances out the bitterness of the dark baking chocolate.`, section: SECTIONS.MAIN },
+        { step: 'Divide the mousse into serving containers.', section: SECTIONS.MAIN, img: mousse15 },
         { step: 'Refrigerate for at least 6 hours.', section: SECTIONS.MAIN },
         { step: 'Combine heavy cream and sugar in a bowl.', section: WHIPPED_CREAM },
-        { step: 'Whisk together until stiff peaks form. Set aside to top the mousse.', section: WHIPPED_CREAM, whipped },
-        { step: 'Top the mousse with whipped cream, chopped chocolate shavings, raspberries, and sprig of mint. I used 100% cocoa dark chocolate so it was really bitter. You can use whatever preferred ratio and subsidize the amount of sweetness with the toppings.', section: SECTIONS.SERVE, img: [shaved, raspberries, plated] },
+        { step: 'Whisk together until stiff peaks form. Set aside to top the mousse.', section: WHIPPED_CREAM, img: mousse16 },
+        { step: 'Top the mousse with whipped cream, chopped chocolate shavings, raspberries, and sprig of mint. I used 100% cocoa dark chocolate so it was really bitter. You can use whatever preferred ratio and subsidize the amount of sweetness with the toppings.', section: SECTIONS.SERVE, img: [mousse17, mousse18, mousse19, mousse20] },
     ],
     store: [
         {
