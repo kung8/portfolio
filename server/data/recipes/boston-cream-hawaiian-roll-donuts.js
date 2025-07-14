a0530e40704cf0154b57325612dd623a94e8d3f4
Kevin Ung <ung.kevin78@gmail.com>
2025-07-13 22:13:36 -0600

    add boston cream hawaiian rolls donut

--- server/data/recipes/boston-cream-hawaiian-roll-donuts.js
+++ server/data/recipes/boston-cream-hawaiian-roll-donuts.js
@@ -1,4 +1,16 @@
-// const example = '../assets/Products/example.jpeg';
+const donut1 = '../assets/Products/boston-cream-hawaiian-rolls-donuts-1.jpeg';
+const donut2 = '../assets/Products/boston-cream-hawaiian-rolls-donuts-2.jpeg';
+const donut3 = '../assets/Products/boston-cream-hawaiian-rolls-donuts-3.jpeg';
+const donut4 = '../assets/Products/boston-cream-hawaiian-rolls-donuts-4.jpeg';
+const donut5 = '../assets/Products/boston-cream-hawaiian-rolls-donuts-5.jpeg';
+const donut6 = '../assets/Products/boston-cream-hawaiian-rolls-donuts-6.jpeg';
+const donut7 = '../assets/Products/boston-cream-hawaiian-rolls-donuts-7.jpeg';
+const donut8 = '../assets/Products/boston-cream-hawaiian-rolls-donuts-8.jpeg';
+const donut9 = '../assets/Products/boston-cream-hawaiian-rolls-donuts-9.jpeg';
+const donut10 = '../assets/Products/boston-cream-hawaiian-rolls-donuts-10.jpeg';
+const donut11 = '../assets/Products/boston-cream-hawaiian-rolls-donuts-11.jpeg';
+const donut12 = '../assets/Products/boston-cream-hawaiian-rolls-donuts-12.jpeg';
+
 const { CATEGORIES, GENRES, METHODS, SECTIONS, TYPES, INGREDIENT_UNITS, YIELD_UNITS, TIME_UNITS, STORAGE_DURATION_UNIT, STORAGE_LOCATION, STORAGE_CONTAINER } = require('./constants');
 const { HAWAIIAN_ROLLS, UNSALTED_BUTTER, VANILLA_INSTANT_PUDDING_MIX, MILK, VANILLA_EXTRACT, SALT, SEMI_SWEET_CHOCOLATE_CHIPS, WHIPPED_TOPPING } = require('./ingredients');
 
@@ -13,14 +25,13 @@ const PREP_GLAZE = 'Prep Glaze';
 const GLAZE_ROLLS = 'Glaze Rolls';
 
 module.exports = {
-    wip: true,
     cardName: 'Boston Cream Donuts',
     name: 'Boston Cream Hawaiian Rolls Donuts',
-    img: '',
+    img: donut12,
     recipeAuthor: '',
     recipeFinder: 'Kevin Ung',
     available: true,
-    recommended: false,
+    recommended: true,
     category: [CATEGORIES.DESSERT],
     genre: [GENRES.AMERICAN],
     method: [METHODS.BAKE],
@@ -65,22 +76,22 @@ module.exports = {
         { step: 'Preheat the oven to 350ºF.', section: PREP_ROLLS },
         { step: 'Line a baking sheet with a silicon baking mat.', section: PREP_ROLLS },
         { step: 'Place rolls on a baking sheet.', section: PREP_ROLLS },
-        { step: 'Brush rolls with melted butter on all sides.', section: PREP_ROLLS },
-        { step: 'Bake until lightly brown and toasted (about 5 to 7 minutes).', section: PREP_ROLLS },
+        { step: 'Brush rolls with melted butter on all sides.', section: PREP_ROLLS, img: donut1 },
+        { step: 'Bake until lightly brown and toasted (about 5 to 7 minutes).', section: PREP_ROLLS, img: donut2 },
 
-        { step: `In a mixing bowl, combine the "${PUDDING_MIX_SECTION}" section ingredients until thickened.`, section: SECTIONS.PREP_FILLING },
-        { step: 'Fold in whipped topping.', section: SECTIONS.PREP_FILLING },
-        { step: 'Chill until it sets (about 10 minutes).', section: SECTIONS.PREP_FILLING },
+        { step: `In a mixing bowl, combine the "${PUDDING_MIX_SECTION}" section ingredients until thickened.`, section: SECTIONS.PREP_FILLING, img: donut3 },
+        { step: 'Fold in whipped topping.', section: SECTIONS.PREP_FILLING, img: donut4 },
+        { step: 'Chill until it sets (about 10 minutes).', section: SECTIONS.PREP_FILLING, img: donut5 },
 
-        { step: 'Make a small hole in each roll (do not go all the way through).', section: FILL_ROLLS },
-        { step: 'Fill each hole with the filling (possibly with a piping bag).', section: FILL_ROLLS },
+        { step: 'Make a small hole in each roll (do not go all the way through).', section: FILL_ROLLS, img: donut6 },
+        { step: 'Fill each hole with the filling (possibly with a piping bag or a funnel).', section: FILL_ROLLS, img: [donut7, donut10] },
 
         { step: 'In a medium bowl, microwave milk until hot (about 30 to 40 seconds).', section: PREP_GLAZE },
-        { step: 'Add chocolate chips to milk. Let it melt (about 10 seconds).', section: PREP_GLAZE },
-        { step: 'Stir. If needed, microwave until it completely melts (about 15 seconds).', section: PREP_GLAZE },
+        { step: 'Add chocolate chips to milk. Let it melt (about 10 seconds).', section: PREP_GLAZE, img: donut8 },
+        { step: 'Stir. If needed, microwave until it completely melts (about 15 seconds).', section: PREP_GLAZE, img: donut9 },
 
         { step: 'Dip the tops in the chocolate.', section: GLAZE_ROLLS },
-        { step: 'Let the chocolate harden slightly (about 20 minutes at room temperature or 5 to 10 minutes in the fridge).', section: GLAZE_ROLLS },
+        { step: 'Let the chocolate harden slightly (about 20 minutes at room temperature or 5 to 10 minutes in the fridge).', section: GLAZE_ROLLS, img: donut11 },
 
         { step: 'Enjoy these simplified donuts without having to fry anything.', section: SECTIONS.SERVE },
     ],
