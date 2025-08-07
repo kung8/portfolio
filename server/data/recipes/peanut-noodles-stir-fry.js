e061fb3f5a371fc4d9073d3c5e297051abb320f0
Kevin Ung <ung.kevin78@gmail.com>
2025-08-06 21:32:09 -0600

    remove dash

--- server/data/recipes/peanut-noodles-stir-fry.js
+++ server/data/recipes/peanut-noodles-stir-fry.js
@@ -7,8 +7,8 @@ const SECOND_VEGETABLES_SECTION = 'Second Vegetables';
 
 module.exports = {
     wip: true,
-    cardName: 'Peanut Noodles Stir-Fry',
-    name: 'Peanut Noodles Stir-Fry',
+    cardName: 'Peanut Noodles Stir Fry',
+    name: 'Peanut Noodles Stir Fry',
     img: '',
     recipeAuthor: 'Kyrie Patterson',
     recipeFinder: 'Kevin Ung',
@@ -24,7 +24,7 @@ module.exports = {
     cookTime: { amount: 10, unit: TIME_UNITS.MINUTE },
     waitTime: { amount: 0, unit: TIME_UNITS.MINUTE },
     websites: [
-        { label: 'Peanut Noodles Stir-Fry', link: 'https://farmerxfoodie.com/easy-peanut-noodles-stir-fry/' }
+        { label: 'Peanut Noodles Stir Fry', link: 'https://farmerxfoodie.com/easy-peanut-noodles-stir-fry/' }
     ],
     separated: true,
     ingredients: [
