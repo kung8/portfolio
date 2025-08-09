92bba992d87897da4a3529d855a3263992183c19
Kevin Ung <ung.kevin78@gmail.com>
2025-08-08 20:18:42 -0600

    remove duplicate toast

--- src/components/recipes/recipe/CookMode.jsx
+++ src/components/recipes/recipe/CookMode.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { SwitchToggle } from "../../switch-toggle/SwitchToggle";
-import { Slide, ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 
 export const CookMode = () => {
     const [wakeLock, setWakeLock] = useState(null);
@@ -35,15 +35,6 @@ export const CookMode = () => {
                         releaseWakeLock();
                     }
                 }}
-            />
-            <ToastContainer
-                position="top-right"
-                hideProgressBar={true}
-                closeOnClick={true}
-                autoClose={1500}
-                transition={Slide}
-                draggable={true}
-                draggablePercent={80}
             />
         </div>
     )
