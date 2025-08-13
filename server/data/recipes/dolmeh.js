7439dce13ba1018aa7446201c38641d7ba032943
Kevin Ung <ung.kevin78@gmail.com>
2025-08-12 20:19:45 -0600

    add dolmeh images

--- server/data/recipes/dolmeh.js
+++ server/data/recipes/dolmeh.js
@@ -1,4 +1,9 @@
-// const example = '../assets/Products/example.jpeg';
+const dolmeh1 = '../assets/Products/dolmeh-1.jpeg';
+const dolmeh2 = '../assets/Products/dolmeh-2.jpeg';
+const dolmeh3 = '../assets/Products/dolmeh-3.jpeg';
+const dolmeh4 = '../assets/Products/dolmeh-4.jpeg';
+const dolmeh5 = '../assets/Products/dolmeh-5.jpeg';
+
 const { CATEGORIES, GENRES, METHODS, PROTEIN, SECTIONS, TYPES, INGREDIENT_UNITS, YIELD_UNITS, TIME_UNITS, STORAGE_DURATION_UNIT, STORAGE_LOCATION, STORAGE_CONTAINER, REHEAT_METHODS } = require('./constants');
 const { GRAPE_LEAF, OLIVE_OIL, YELLOW_ONION, GARLIC, GROUND_BEEF, BASMATI_RICE, CHICKPEA, SALT, BLACK_PEPPER, TARRAGON, PARSLEY, CILANTRO, GREEN_ONION, DILL, WATER, WHITE_SUGAR, LEMON_JUICE } = require('./ingredients');
 
@@ -7,10 +12,9 @@ const LEMON_SUGAR_MIX = 'Lemon Sugar Mix';
 const COOK_DOLMEH = 'Cook Dolmeh';
 
 module.exports = {
-    wip: true,
     cardName: 'Dolmeh',
     name: 'Dolmeh',
-    img: '',
+    img: dolmeh5,
     recipeAuthor: "Shadi HasanzadeNemati",
     recipeFinder: "Kasra Kamravani's Mom",
     available: true,
@@ -62,20 +66,20 @@ module.exports = {
     directions: [
         { step: 'Over medium heat, heat oil in pan.', section: SECTIONS.PREP_FILLING },
         { step: 'Add onion and garlic. Saute until golden brown.', section: SECTIONS.PREP_FILLING },
-        { step: 'Add beef. Brown.', section: SECTIONS.PREP_FILLING },
+        { step: 'Add beef. Brown.', section: SECTIONS.PREP_FILLING, img: dolmeh1 },
         { step: `Add "${SECTIONS.RICE}" section ingredients.`, section: SECTIONS.PREP_FILLING },
         { step: `Add "${SECTIONS.SEASONINGS}" section ingredients.`, section: SECTIONS.PREP_FILLING },
-        { step: `Add "${SECTIONS.VEGGIES}" section ingredients. Cook for a few minutes.`, section: SECTIONS.PREP_FILLING },
+        { step: `Add "${SECTIONS.VEGGIES}" section ingredients. Cook for a few minutes.`, section: SECTIONS.PREP_FILLING, img: dolmeh2 },
 
         { step: 'On a clean surface, lay grape leaves flat.', section: SECTIONS.WRAP },
         { step: 'Add 1 tablespoon of filling in the center.', section: SECTIONS.WRAP },
         { step: 'Fold over the sides.', section: SECTIONS.WRAP },
-        { step: 'Place folded side down.', section: SECTIONS.WRAP },
+        { step: 'Place folded side down.', section: SECTIONS.WRAP, img: dolmeh3 },
 
         { step: 'In a pot, add oil and cover the bottom with grape leaves.', section: COOK_DOLMEH },
         { step: 'Place stuffed dolmeh on the grape leaves tightly (folded side down). Layer until all dolmeh are in the pot.', section: COOK_DOLMEH },
         { step: 'Press down the dolmeh with a lid or plate.', section: COOK_DOLMEH },
-        { step: `In a small bowl, combine "${LEMON_SUGAR_MIX}" section ingredients. Pour into pot.`, section: COOK_DOLMEH },
+        { step: `In a small bowl, combine "${LEMON_SUGAR_MIX}" section ingredients. Pour into pot.`, section: COOK_DOLMEH, img: dolmeh4 },
         { step: 'Cover with a lid. Place over medium heat.', section: COOK_DOLMEH },
         { step: 'Once water simmers, turn heat to low.', section: COOK_DOLMEH },
         { step: 'Cook until all the water has been absorbed and grape leaves should be cooked (about 1 hour). Dolmeh should be soft and rice is cooked completely.', section: COOK_DOLMEH },
