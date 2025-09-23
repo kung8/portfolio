865d499e08ce5cfb076a2ea5f18b45ed20fde004
Kevin Ung <ung.kevin78@gmail.com>
2025-09-22 23:39:59 -0600

    add gyoza images

--- server/data/recipes/gyoza.js
+++ server/data/recipes/gyoza.js
@@ -1,4 +1,13 @@
-// const example = '../assets/Products/example.jpeg';
+const gyoza1 = '../assets/Products/gyoza-1.jpeg';
+const gyoza2 = '../assets/Products/gyoza-2.jpeg';
+const gyoza3 = '../assets/Products/gyoza-3.jpeg';
+const gyoza4 = '../assets/Products/gyoza-4.jpeg';
+const gyoza5 = '../assets/Products/gyoza-5.jpeg';
+const gyoza6 = '../assets/Products/gyoza-6.jpeg';
+const gyoza7 = '../assets/Products/gyoza-7.jpeg';
+const gyoza8 = '../assets/Products/gyoza-8.jpeg';
+
+const { img } = require('./choy-sum-with-garlic-sauce');
 const { CATEGORIES, GENRES, METHODS, PROTEIN, SECTIONS, TYPES, INGREDIENT_UNITS, YIELD_UNITS, TIME_UNITS, STORAGE_DURATION_UNIT, STORAGE_LOCATION, STORAGE_CONTAINER, REHEAT_METHODS } = require('./constants');
 const { GROUND_PORK, GREEN_ONION, CABBAGE, GARLIC, SESAME_OIL, LIGHT_SOY_SAUCE, SALT, BLACK_PEPPER, VEGETABLE_OIL, WATER, WONTON_WRAPPER } = require('./ingredients');
 
@@ -6,14 +15,13 @@ const WRAPPER_SECTION = 'Wrapper';
 const COOK_GYOZAS = 'Cook Gyozas';
 
 module.exports = {
-    wip: true,
     cardName: 'Gyoza',
     name: 'Gyoza',
-    img: '',
+    img: gyoza8,
     recipeAuthors: ['Natalya Drozhzhin'],
     recipeFinder: 'Kevin Ung',
     available: true,
-    recommended: false,
+    recommended: true,
     category: [CATEGORIES.APPETIZER, CATEGORIES.SIDE_DISH],
     genre: [GENRES.ASIAN, GENRES.JAPANESE],
     method: [METHODS.PAN_FRY],
@@ -48,14 +56,14 @@ module.exports = {
         { name: 'plate' },
     ],
     directions: [
-        { step: `In a mixing bowl, combine the "${SECTIONS.FILLING}" section ingredients.`, section: SECTIONS.ASSEMBLE },
-        { step: 'Place a spoonful of filling in the center of each wrapper.', section: SECTIONS.ASSEMBLE },
+        { step: `In a mixing bowl, combine the "${SECTIONS.FILLING}" section ingredients.`, section: SECTIONS.ASSEMBLE, img: gyoza1 },
+        { step: 'Place a spoonful of filling in the center of each wrapper.', section: SECTIONS.ASSEMBLE, img: gyoza2 },
         { step: 'Brush the wrapper edges with water using your finger and crimp the edges until the gyozas are sealed.', section: SECTIONS.ASSEMBLE },
-        { step: 'Place gyozas on a plate(s) and leave space between them to avoid them sticking.', section: SECTIONS.ASSEMBLE },
+        { step: 'Place gyozas on a plate(s) and leave space between them to avoid them sticking.', section: SECTIONS.ASSEMBLE, img: gyoza3 },
         { step: 'Over medium-high heat, add oil to a pan.', section: COOK_GYOZAS },
-        { step: 'Pan fry gyozas until golden brown (about 3 minutes).', section: COOK_GYOZAS },
-        { step: 'Add water. Cover with a lid.', section: COOK_GYOZAS },
-        { step: 'Steam for about 3 to 4 minutes.', section: COOK_GYOZAS },
+        { step: 'Pan fry gyozas until golden brown (about 3 minutes).', section: COOK_GYOZAS, img: [gyoza4, gyoza5] },
+        { step: 'Add water. Cover with a lid.', section: COOK_GYOZAS, img: gyoza6 },
+        { step: 'Steam for about 3 to 4 minutes.', section: COOK_GYOZAS, img: gyoza7 },
         { step: 'Serve with your favorite sauce.', section: SECTIONS.SERVE },
     ],
     store: [
