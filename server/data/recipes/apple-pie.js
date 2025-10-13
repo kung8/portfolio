f9e2408750a4ec55f5818f4c1c67b64ea716ad82
Kevin Ung <ung.kevin78@gmail.com>
2025-10-12 21:08:00 -0600

    rename apple pie images

--- server/data/recipes/apple-pie.js
+++ server/data/recipes/apple-pie.js
@@ -1,15 +1,15 @@
-const apples = '../assets/Products/apple-pie-apples.jpeg';
-const zoomed2 = '../assets/Products/apple-pie-baked-zoomed-2.jpeg';
-const baked = '../assets/Products/apple-pie-baked.jpeg';
-const melted = '../assets/Products/apple-pie-melted-butter.jpeg';
-const melting = '../assets/Products/apple-pie-melting-butter.jpeg';
-const crust = '../assets/Products/apple-pie-pastry-dough.jpeg';
-const plated = '../assets/Products/apple-pie-plated.jpeg';
-const lattice = '../assets/Products/apple-pie-raw-lattice.jpeg';
-const rawSauce = '../assets/Products/apple-pie-raw-with-sauce.jpeg';
-const sauce = '../assets/Products/apple-pie-sauce.jpeg';
-const sliced = '../assets/Products/apple-pie-sliced.jpeg';
-const sugars = '../assets/Products/apple-pie-sugars.jpeg';
+const pie1 = '../assets/Products/apple-pie-1.jpeg';
+const pie2 = '../assets/Products/apple-pie-2.jpeg';
+const pie3 = '../assets/Products/apple-pie-3.jpeg';
+const pie4 = '../assets/Products/apple-pie-4.jpeg';
+const pie5 = '../assets/Products/apple-pie-5.jpeg';
+const pie6 = '../assets/Products/apple-pie-6.jpeg';
+const pie7 = '../assets/Products/apple-pie-7.jpeg';
+const pie8 = '../assets/Products/apple-pie-8.jpeg';
+const pie9 = '../assets/Products/apple-pie-9.jpeg';
+const pie10 = '../assets/Products/apple-pie-10.jpeg';
+const pie11 = '../assets/Products/apple-pie-11.jpeg';
+const pie12 = '../assets/Products/apple-pie-12.jpeg';
 
 const { ALLERGIES, CATEGORIES, DIET, GENRES, INGREDIENT_UNITS, METHODS, REHEAT_METHODS, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
 const { ALL_PURPOSE_FLOUR, BROWN_SUGAR, DOUBLE_CRUST_PIE_CRUST, GRANNY_SMITH_APPLE, OVEN, PIE_PAN, SAUCEPAN, STOVE, UNSALTED_BUTTER, WATER, WHITE_SUGAR } = require('./ingredients');
@@ -17,7 +17,7 @@ const { ALL_PURPOSE_FLOUR, BROWN_SUGAR, DOUBLE_CRUST_PIE_CRUST, GRANNY_SMITH_APP
 module.exports = {
     cardName: 'Apple Pie',
     name: 'Apple Pie',
-    img: baked,
+    img: pie12,
     recipeAuthors: ['Moshasmama'],
     recipeFinder: 'Kevin Ung',
     available: true,
@@ -53,24 +53,24 @@ module.exports = {
         PIE_PAN,
     ],
     directions: [
-        { step: 'Peel and cut the apple into thin slices.', section: SECTIONS.MAIN, img: apples },
+        { step: 'Peel and cut the apple into thin slices.', section: SECTIONS.MAIN, img: pie1 },
         { step: 'Preheat the oven to 425ºF.', section: SECTIONS.MAIN },
-        { step: 'Over medium heat, melt butter in a saucepan.', section: SECTIONS.MAIN, img: [melting, melted] },
+        { step: 'Over medium heat, melt butter in a saucepan.', section: SECTIONS.MAIN, img: [pie2, pie3] },
         { step: 'Add flour and mix until it becomes a paste and fragrant (about 1 to 2 minutes).', section: SECTIONS.MAIN },
-        { step: 'Add sugars and water. Bring to a boil.', section: SECTIONS.MAIN, img: [sugars] },
+        { step: 'Add sugars and water. Bring to a boil.', section: SECTIONS.MAIN, img: pie4 },
         { step: 'Lower to low heat. Simmer (about 3 to 5 minutes).', section: SECTIONS.MAIN },
-        { step: 'Remove from heat.', section: SECTIONS.MAIN, img: sauce },
-        { step: 'Position one pastry crust in a pie pan.', section: SECTIONS.MAIN, img: crust },
+        { step: 'Remove from heat.', section: SECTIONS.MAIN, img: pie5 },
+        { step: 'Position one pastry crust in a pie pan.', section: SECTIONS.MAIN, img: pie6 },
         { step: 'Roll the second pastry crust so it grows about 1 inch on all sides.', section: SECTIONS.MAIN },
         { step: 'Cut the pastry into 8 1-inch strip.', section: SECTIONS.MAIN },
         { step: 'Add the apple slices to the pie pan (a slight mound is fine).', section: SECTIONS.MAIN },
         { step: 'Lay out four of the pastry strips vertically evenly spaced.', section: SECTIONS.MAIN },
         { step: 'Weave the remaining pastry strips horizontally and alternating.', section: SECTIONS.MAIN },
-        { step: 'Fold the lattice over the pie, trim any excess, and pinch.', section: SECTIONS.MAIN, img: lattice },
-        { step: 'Pour the sugar mixture over the lattice and make sure that it (1) gets onto the apples and (2) does not spill over the sides.', section: SECTIONS.MAIN, img: rawSauce },
+        { step: 'Fold the lattice over the pie, trim any excess, and pinch.', section: SECTIONS.MAIN, img: pie7 },
+        { step: 'Pour the sugar mixture over the lattice and make sure that it (1) gets onto the apples and (2) does not spill over the sides.', section: SECTIONS.MAIN, img: pie8 },
         { step: 'Bake for 15 minutes.', section: SECTIONS.MAIN },
         { step: 'Reduce to 350ºF and bake until apples are soft (about 35 to 45 minutes).', section: SECTIONS.MAIN },
-        { step: 'Enjoy a taste of America\'s traditional dessert al la mode (i.e. topped with ice cream) or all by itself.', section: SECTIONS.MAIN, img: [plated, sliced, zoomed2] },
+        { step: 'Enjoy a taste of America\'s traditional dessert al la mode (i.e. topped with ice cream) or all by itself.', section: SECTIONS.MAIN, img: [pie9, pie10, pie11] },
     ],
     notes: [
         { note: 'Slicing this pie is easier when it is cooled but it may come out crumbly but it\'s still good.' },
