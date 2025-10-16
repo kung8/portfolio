3d02ae628df48aec9f5189c196495225335f8461
Kevin Ung <ung.kevin78@gmail.com>
2025-10-15 20:38:58 -0600

    update blondie images and recipe

--- server/data/recipes/blondie.js
+++ server/data/recipes/blondie.js
@@ -1,12 +1,13 @@
-const batterPan = '../assets/Products/blondie-batter-pan.jpeg';
-const batter = '../assets/Products/blondie-batter.jpeg';
-const butter = '../assets/Products/blondie-butter.jpeg';
-const dry = '../assets/Products/blondie-dry.jpeg';
-const eggs = '../assets/Products/blondie-eggs.jpeg';
-const flour = '../assets/Products/blondie-flour.jpeg';
-const pan = '../assets/Products/blondie-pan.jpeg';
-const plated = '../assets/Products/blondie-plated.jpeg';
-const sugars = '../assets/Products/blondie-sugars.jpeg';
+const blondie1 = '../assets/Products/blondie-1.jpeg';
+const blondie2 = '../assets/Products/blondie-2.jpeg';
+const blondie3 = '../assets/Products/blondie-3.jpeg';
+const blondie4 = '../assets/Products/blondie-4.jpeg';
+const blondie5 = '../assets/Products/blondie-5.jpeg';
+const blondie6 = '../assets/Products/blondie-6.jpeg';
+const blondie7 = '../assets/Products/blondie-7.jpeg';
+const blondie8 = '../assets/Products/blondie-8.jpeg';
+const blondie9 = '../assets/Products/blondie-9.jpeg';
+
 const { ALLERGIES, CATEGORIES, DIET, GENRES, INGREDIENT_UNITS, METHODS, PROTEIN, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
 const {
     ALL_PURPOSE_FLOUR,
@@ -27,10 +28,19 @@ const {
     WHITE_SUGAR,
 } = require('./ingredients');
 
+const SUGARS_SECTION = 'Sugars';
+const WET_INGREDIENTS = 'Wet Ingredients';
+const DRY_INGREDIENTS = 'Dry Ingredients';
+const CHOCOLATE_CHIP_SECTION = 'Chocolate Chips';
+
+const CREAM_SUGARS = 'Cream Sugars and Butter';
+const ADD_WET_INGREDIENTS = 'Add Wet Ingredients';
+const MIX_DRY_INGREDIENTS = 'Mix Dry Ingredients';
+
 module.exports = {
     cardName: 'Blondie',
     name: 'Blondie',
-    img: plated,
+    img: blondie9,
     recipeAuthors: ['Sam Merritt'],
     recipeFinder: 'Kevin Ung',
     available: true,
@@ -48,19 +58,19 @@ module.exports = {
     websites: [
         { label: 'Blondies', link: "https://sugarspunrun.com/blondies-recipe/" }
     ],
-    separated: false,
+    separated: true,
     ingredients: [
-        { ...UNSALTED_BUTTER, amount: 1, unit: INGREDIENT_UNITS.CUP, additionalDetails: 'melted', section: SECTIONS.MAIN },
-        { ...BROWN_SUGAR, amount: 5 / 4, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...WHITE_SUGAR, amount: 1 / 2, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...EGG, amount: 2, unit: '', additionalDetails: '', section: SECTIONS.MAIN },
-        { ...EGG_YOLK, amount: 1, unit: '', additionalDetails: '', section: SECTIONS.MAIN },
-        { ...VANILLA_EXTRACT, amount: 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...ALL_PURPOSE_FLOUR, amount: 9 / 4, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...CORNSTARCH, amount: 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...BAKING_POWDER, amount: 1 / 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...SALT, amount: 1, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...SEMI_SWEET_CHOCOLATE_CHIPS, amount: 2 / 3, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.MAIN },
+        { ...UNSALTED_BUTTER, amount: 1, unit: INGREDIENT_UNITS.CUP, additionalDetails: 'melted', section: SUGARS_SECTION },
+        { ...BROWN_SUGAR, amount: 5 / 4, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SUGARS_SECTION },
+        { ...WHITE_SUGAR, amount: 1 / 2, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SUGARS_SECTION },
+        { ...EGG, amount: 2, unit: '', additionalDetails: '', section: WET_INGREDIENTS },
+        { ...EGG_YOLK, amount: 1, unit: '', additionalDetails: '', section: WET_INGREDIENTS },
+        { ...VANILLA_EXTRACT, amount: 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: WET_INGREDIENTS },
+        { ...ALL_PURPOSE_FLOUR, amount: 9 / 4, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: DRY_INGREDIENTS },
+        { ...CORNSTARCH, amount: 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: DRY_INGREDIENTS },
+        { ...BAKING_POWDER, amount: 1 / 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: DRY_INGREDIENTS },
+        { ...SALT, amount: 1, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: DRY_INGREDIENTS },
+        { ...SEMI_SWEET_CHOCOLATE_CHIPS, amount: 2 / 3, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: CHOCOLATE_CHIP_SECTION },
     ],
     appliances: [
         OVEN,
@@ -72,16 +82,16 @@ module.exports = {
         MEDIUM_BOWL,
     ],
     directions: [
-        { step: 'Preheat the oven to 350ºF.', section: SECTIONS.MAIN },
-        { step: 'Melt butter.', section: SECTIONS.MAIN, img: butter },
-        { step: 'In a mixing bowl, combine melted butter and both sugars until well mixed.', section: SECTIONS.MAIN, img: sugars },
-        { step: 'Add eggs, yolk and vanilla. Combine.', section: SECTIONS.MAIN, img: eggs },
-        { step: 'In a medium bowl, combine flour, cornstarch, baking powder and salt.', section: SECTIONS.MAIN, img: dry },
-        { step: 'Slowly combine the dry ingredients into the wet ingredients.', section: SECTIONS.MAIN, img: flour },
-        { step: 'Fold in chocolate chips or other desired fillings.', section: SECTIONS.MAIN, img: batter },
-        { step: 'Pour in batter in a baking pan. Line with parchment paper if using a metal pan.', section: SECTIONS.MAIN, img: batterPan },
-        { step: 'Bake for 25 to 30 minutes or until toothpick comes out clean when inserted.', section: SECTIONS.MAIN, img: pan },
-        { step: 'Enjoy these tasty, simple and light desserts.', section: SECTIONS.MAIN },
+        { step: 'Preheat the oven to 350ºF.', section: CREAM_SUGARS },
+        { step: 'Melt butter.', section: CREAM_SUGARS, img: blondie1 },
+        { step: `In a mixing bowl, combine "${SUGARS_SECTION}" section ingredients until well mixed.`, section: CREAM_SUGARS, img: blondie2 },
+        { step: `Add "${WET_INGREDIENTS}" section ingredients. Combine.`, section: ADD_WET_INGREDIENTS, img: blondie3 },
+        { step: `In a medium bowl, combine "${DRY_INGREDIENTS}" section ingredients.`, section: MIX_DRY_INGREDIENTS, img: blondie4 },
+        { step: 'Slowly combine the dry ingredients into the wet ingredients.', section: SECTIONS.ASSEMBLE, img: blondie5 },
+        { step: 'Fold in chocolate chips or other desired fillings.', section: SECTIONS.ASSEMBLE, img: blondie6 },
+        { step: 'Pour in batter in a baking pan. Line with parchment paper if using a metal pan.', section: SECTIONS.ASSEMBLE, img: blondie7 },
+        { step: 'Bake for 25 to 30 minutes or until toothpick comes out clean when inserted.', section: SECTIONS.BAKE, img: blondie8 },
+        { step: 'Enjoy these tasty, simple and light desserts.', section: SECTIONS.SERVE },
     ],
     notes: [
         { note: 'The melted butter and the extra yolk are what give these blondies their chewy texture.' },
