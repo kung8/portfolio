656b2eebd573ca755d2867bf477c23606d8f0860
Kevin Ung <ung.kevin78@gmail.com>
2025-10-16 21:59:41 -0600

    update stuffing images

--- server/data/recipes/stuffing.js
+++ server/data/recipes/stuffing.js
@@ -1,22 +1,23 @@
 const stuffing1 = '../assets/Products/stuffing-1.jpeg';
 const stuffing2 = '../assets/Products/stuffing-2.jpeg';
+const stuffing3 = '../assets/Products/stuffing-3.jpeg';
+const stuffing4 = '../assets/Products/stuffing-4.jpeg';
 const stuffing5 = '../assets/Products/stuffing-5.jpeg';
+const stuffing6 = '../assets/Products/stuffing-6.jpeg';
 const stuffing7 = '../assets/Products/stuffing-7.jpeg';
+const stuffing8 = '../assets/Products/stuffing-8.jpeg';
 const stuffing9 = '../assets/Products/stuffing-9.jpeg';
 const stuffing10 = '../assets/Products/stuffing-10.jpeg';
 const stuffing11 = '../assets/Products/stuffing-11.jpeg';
 const stuffing12 = '../assets/Products/stuffing-12.jpeg';
-const stuffing13 = '../assets/Products/stuffing-13.jpeg';
-const stuffing14 = '../assets/Products/stuffing-14.jpeg';
-const stuffing15 = '../assets/Products/stuffing-15.jpeg';
-const stuffing16 = '../assets/Products/stuffing-16.jpeg';
+
 const { ALLERGIES, CATEGORIES, DIET, GENRES, INGREDIENT_UNITS, METHODS, REHEAT_METHODS, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
 const { BAKING_PAN, BLACK_PEPPER, BREAD, CELERY, CHICKEN_BROTH, COOKING_SPRAY, EGG, FRESH_ROSEMARY, FRESH_SAGE, FRYING_PAN, GARLIC, OVEN, PARSLEY, SALT, SMALL_BOWL, STOVE, SWEET_ONION, UNSALTED_BUTTER, WHISK } = require('./ingredients');
 
 module.exports = {
     cardName: 'Stuffing',
     name: 'Stuffing',
-    img: stuffing16,
+    img: stuffing12,
     recipeAuthors: ['Jessica'],
     recipeFinder: 'Kevin Ung',
     available: true,
@@ -62,17 +63,17 @@ module.exports = {
         { step: 'Preheat the oven to 350ºF.', section: SECTIONS.MAIN },
         { step: 'Brush the baking pan with some cooking spray.', section: SECTIONS.MAIN, img: stuffing1 },
         { step: 'Place cubed bread in baking pan.', section: SECTIONS.MAIN, img: stuffing2 },
-        { step: 'Over medium heat, heat butter in a pan.', section: SECTIONS.MAIN, img: stuffing5 },
-        { step: 'Once melted, stir in onion, celery, garlic, salt and black pepper.', section: SECTIONS.MAIN, img: stuffing7 },
-        { step: 'Cook until the veggie are soft (about 8 to 10 minutes).', section: SECTIONS.MAIN, img: stuffing9 },
-        { step: 'Add in chopped fresh herbs. Mix and cook for 1 minute.', section: SECTIONS.MAIN, img: [stuffing10, stuffing11] },
-        { step: 'Stir in 1 cup of stock.', section: SECTIONS.MAIN, img: stuffing12 },
+        { step: 'Over medium heat, heat butter in a pan.', section: SECTIONS.MAIN, img: stuffing3 },
+        { step: 'Once melted, stir in onion, celery, garlic, salt and black pepper.', section: SECTIONS.MAIN, img: stuffing4 },
+        { step: 'Cook until the veggie are soft (about 8 to 10 minutes).', section: SECTIONS.MAIN, img: stuffing5 },
+        { step: 'Add in chopped fresh herbs. Mix and cook for 1 minute.', section: SECTIONS.MAIN, img: [stuffing6, stuffing7] },
+        { step: 'Stir in 1 cup of stock.', section: SECTIONS.MAIN, img: stuffing8 },
         { step: 'If you like a crispier stuffing, you can scoop out some of the bread and set it aside.', section: SECTIONS.MAIN },
-        { step: 'Pour mixture over the cubed bread. Mix until coated.', section: SECTIONS.MAIN, img: stuffing14 },
+        { step: 'Pour mixture over the cubed bread. Mix until coated.', section: SECTIONS.MAIN, img: stuffing9 },
         { step: 'If you scooped out some bread, you can add it back in now.', section: SECTIONS.MAIN },
-        { step: 'In a small bowl, combine the eggs and the remaining stock.', section: SECTIONS.MAIN, img: stuffing13 },
+        { step: 'In a small bowl, combine the eggs and the remaining stock.', section: SECTIONS.MAIN, img: stuffing10 },
         { step: 'Pour egg mixture over the cubed bread and fold until combined.', section: SECTIONS.MAIN },
-        { step: 'Bake until the internal temperature is 160ºF (about 45 to 50 minutes). If getting browned and it isn\'t finished yet, you can cover it with foil in a tent shape.', section: SECTIONS.MAIN, img: stuffing15 },
+        { step: 'Bake until the internal temperature is 160ºF (about 45 to 50 minutes). If getting browned and it isn\'t finished yet, you can cover it with foil in a tent shape.', section: SECTIONS.MAIN, img: stuffing11 },
         { step: 'Serve warm on the side of your holiday feast or on any occasion.', section: SECTIONS.MAIN },
     ],
     store: [
