9960cf08ba66e1abce34e2759c2ea4e0058c4649
Kevin Ung <ung.kevin78@gmail.com>
2025-10-19 01:18:31 -0600

    update chicken quesadilla images

--- server/data/recipes.js
+++ server/data/recipes.js
@@ -761,7 +761,6 @@ module.exports = recipes;
 // and sautéed vegetables. (32)
 
 // TODO: update the images for the following recipes:
-// - chicken-quesadilla (lower the number)
 // - chicken-stuffed-crescent-rolls
 // - chicken-tikka-masala
 // - chili
--- server/data/recipes/chicken-quesadilla.js
+++ server/data/recipes/chicken-quesadilla.js
@@ -1,16 +1,14 @@
-const quesadillas1 = '../assets/Products/chicken-quesadillas-1.jpeg';
-const quesadillas3 = '../assets/Products/chicken-quesadillas-3.jpeg';
-const quesadillas4 = '../assets/Products/chicken-quesadillas-4.jpeg';
-const quesadillas5 = '../assets/Products/chicken-quesadillas-5.jpeg';
-const quesadillas6 = '../assets/Products/chicken-quesadillas-6.jpeg';
-const quesadillas7 = '../assets/Products/chicken-quesadillas-7.jpeg';
-const quesadillas8 = '../assets/Products/chicken-quesadillas-8.jpeg';
-const quesadillas9 = '../assets/Products/chicken-quesadillas-9.jpeg';
-const quesadillas10 = '../assets/Products/chicken-quesadillas-10.jpeg';
-const quesadillas11 = '../assets/Products/chicken-quesadillas-11.jpeg';
-const quesadillas12 = '../assets/Products/chicken-quesadillas-12.jpeg';
-const quesadillas13 = '../assets/Products/chicken-quesadillas-13.jpeg';
-const quesadillas14 = '../assets/Products/chicken-quesadillas-14.jpeg';
+const quesadilla1 = '../assets/Products/chicken-quesadilla-1.jpeg';
+const quesadilla2 = '../assets/Products/chicken-quesadilla-2.jpeg';
+const quesadilla3 = '../assets/Products/chicken-quesadilla-3.jpeg';
+const quesadilla4 = '../assets/Products/chicken-quesadilla-4.jpeg';
+const quesadilla5 = '../assets/Products/chicken-quesadilla-5.jpeg';
+const quesadilla6 = '../assets/Products/chicken-quesadilla-6.jpeg';
+const quesadilla7 = '../assets/Products/chicken-quesadilla-7.jpeg';
+const quesadilla8 = '../assets/Products/chicken-quesadilla-8.jpeg';
+const quesadilla9 = '../assets/Products/chicken-quesadilla-9.jpeg';
+const quesadilla10 = '../assets/Products/chicken-quesadilla-10.jpeg';
+const quesadilla11 = '../assets/Products/chicken-quesadilla-11.jpeg';
 
 const { ALLERGIES, CATEGORIES, DIET, GENRES, INGREDIENT_UNITS, METHODS, PROTEIN, REHEAT_METHODS, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
 const { BONELESS_AND_SKINLESS_CHICKEN_BREAST, CHILI_POWDER, CUTTING_BOARD, FRYING_PAN, GARLIC_POWDER, GREEN_BELL_PEPPER, GUACAMOLE, HOT_SAUCE, KNIFE, MIXING_BOWL, MONTEREY_JACK_CHEESE, OLIVE_OIL, PAPER_TOWELS, PLATE, RED_BELL_PEPPER, SALSA, SALT, SAUCEPAN, SHARP_CHEDDAR_CHEESE, SMALL_BOWL, SOUR_CREAM, STOVE, UNSALTED_BUTTER, WHEAT_TORTILLA, YELLOW_ONION } = require('./ingredients');
@@ -21,7 +19,7 @@ const COOK_QUESADILLA = 'Cook Quesadilla';
 module.exports = {
     cardName: 'Chicken Quesadilla',
     name: 'Chicken Quesadilla',
-    img: quesadillas14,
+    img: quesadilla11,
     recipeAuthors: ['Erin Clarke'],
     recipeFinder: 'Kevin Ung',
     available: true,
@@ -76,35 +74,35 @@ module.exports = {
         KNIFE,
     ],
     directions: [
-        { step: 'In a small bowl, combine spices.', section: SECTIONS.PREP_SEASONINGS, img: quesadillas3 },
+        { step: 'In a small bowl, combine spices.', section: SECTIONS.PREP_SEASONINGS },
 
-        { step: 'In a saucepan, add the chicken breast and fill the saucepan with water just enough to cover the chicken slightly.', section: SECTIONS.PREP_CHICKEN, img: quesadillas1 },
+        { step: 'In a saucepan, add the chicken breast and fill the saucepan with water just enough to cover the chicken slightly.', section: SECTIONS.PREP_CHICKEN },
         { step: 'Bring to a boil.', section: SECTIONS.PREP_CHICKEN },
         { step: 'Reduce to a medium-low heat. Cook until chicken reaches 165ºF.', section: SECTIONS.PREP_CHICKEN },
         { step: 'Place the chicken on a cutting board and let it cool.', section: SECTIONS.PREP_CHICKEN },
-        { step: 'Cut the chicken into small 1/2 inch slices.', section: SECTIONS.PREP_CHICKEN, img: quesadillas6 },
+        { step: 'Cut the chicken into small 1/2 inch slices.', section: SECTIONS.PREP_CHICKEN, img: quesadilla1 },
         { step: 'Place the cut up chicken into a mixing bowl.', section: SECTIONS.PREP_CHICKEN },
         { step: 'Mix in half of the spice mixture into the chicken. Coating it.', section: SECTIONS.PREP_CHICKEN },
 
         { step: 'Over medium heat, heat oil in a frying pan.', section: SECTIONS.PREP_VEGGIES },
-        { step: 'Add veggies and remaining spice mixture.', section: SECTIONS.PREP_VEGGIES, img: quesadillas4 },
-        { step: 'Saute the veggies (about 6 to 8 minutes). Onions should be browned and translucent. Peppers should be crisp but tender.', section: SECTIONS.PREP_VEGGIES, img: quesadillas5 },
+        { step: 'Add veggies and remaining spice mixture.', section: SECTIONS.PREP_VEGGIES, img: quesadilla2 },
+        { step: 'Saute the veggies (about 6 to 8 minutes). Onions should be browned and translucent. Peppers should be crisp but tender.', section: SECTIONS.PREP_VEGGIES, img: quesadilla3 },
         { step: 'Optionally add hot sauce. Mix.', section: SECTIONS.PREP_VEGGIES },
-        { step: 'Transfer veggies to the bowl of chicken. Mix.', section: SECTIONS.PREP_VEGGIES, img: quesadillas7 },
+        { step: 'Transfer veggies to the bowl of chicken. Mix.', section: SECTIONS.PREP_VEGGIES, img: quesadilla4 },
 
-        { step: 'Place tortilla on a plate. On one half of each, top them with 1/8 of both cheeses and 1/4 of the chicken-veggies mixture.', section: SECTIONS.ASSEMBLE, img: quesadillas8 },
-        { step: 'Top with 1/8 of both cheeses (should be the last of the cheeses).', section: SECTIONS.ASSEMBLE, img: quesadillas9 },
-        { step: 'Fold to seal the goodness. Gently press to create a slight crease.', section: SECTIONS.ASSEMBLE, img: quesadillas10 },
+        { step: 'Place tortilla on a plate. On one half of each, top them with 1/8 of both cheeses and 1/4 of the chicken-veggies mixture.', section: SECTIONS.ASSEMBLE, img: quesadilla5 },
+        { step: 'Top with 1/8 of both cheeses (should be the last of the cheeses).', section: SECTIONS.ASSEMBLE, img: quesadilla6 },
+        { step: 'Fold to seal the goodness. Gently press to create a slight crease.', section: SECTIONS.ASSEMBLE, img: quesadilla7 },
         { step: 'Repeat filling with the remaining tortillas.', section: SECTIONS.ASSEMBLE },
 
         { step: 'Wipe off the frying pan with paper towel.', section: COOK_QUESADILLA },
         { step: 'Over medium-low heat, add a little butter to make the tortillas crisper.', section: COOK_QUESADILLA },
-        { step: 'Place 2 quesadillas in the pan with the folded edge in the center.', section: COOK_QUESADILLA, img: quesadillas11 },
+        { step: 'Place 2 quesadillas in the pan with the folded edge in the center.', section: COOK_QUESADILLA, img: quesadilla8 },
         { step: 'Press tops gently with the back of the spatula. Cheese and other content may ooze out but that\'s okay.', section: COOK_QUESADILLA },
-        { step: 'Cook until the bottom is golden and crisp (about 4 minutes).', section: COOK_QUESADILLA, img: quesadillas12 },
+        { step: 'Cook until the bottom is golden and crisp (about 4 minutes).', section: COOK_QUESADILLA, img: quesadilla9 },
         { step: 'Flip. Cook until the other side is golden and crisp (about 2 minutes).', section: COOK_QUESADILLA },
 
-        { step: 'Place cooked quesadilla on a cutting board. Cut the quesadillas into thirds.', section: SECTIONS.SERVE, img: quesadillas13 },
+        { step: 'Place cooked quesadilla on a cutting board. Cut the quesadillas into thirds.', section: SECTIONS.SERVE, img: quesadilla10 },
         { step: 'Serve hot with your favorite toppings.', section: SECTIONS.SERVE },
     ],
     store: [
