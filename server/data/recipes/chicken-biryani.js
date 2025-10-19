2af32eb6102c545243c1077d6d5b2d6d02770857
Kevin Ung <ung.kevin78@gmail.com>
2025-10-19 00:20:05 -0600

    update chicken biryani images

--- server/data/recipes/chicken-biryani.js
+++ server/data/recipes/chicken-biryani.js
@@ -1,22 +1,22 @@
-const aromatics = '../assets/Products/chicken-biryani-aromatics.jpeg';
-const boiled = '../assets/Products/chicken-biryani-boiled-rice.jpeg';
-const cooking = '../assets/Products/chicken-biryani-cooking-chicken.jpeg';
-const drained = '../assets/Products/chicken-biryani-drained-rice.jpeg';
-const flipped = '../assets/Products/chicken-biryani-flipped-chicken.jpeg';
-const layeredGhee = '../assets/Products/chicken-biryani-layered-ghee.jpeg';
-const layeredRice = '../assets/Products/chicken-biryani-layered-rice.jpeg';
-const layeredSaffron = '../assets/Products/chicken-biryani-layered-saffron.jpeg';
-const layeredVeggies = '../assets/Products/chicken-biryani-layered-veggies.jpeg';
-const marinade = '../assets/Products/chicken-biryani-marinade.jpeg';
-const marinating = '../assets/Products/chicken-biryani-marinating-chicken.jpeg';
-const partially = '../assets/Products/chicken-biryani-partially-cooked-chicken.jpeg';
-const clean = '../assets/Products/chicken-biryani-plated-clean.jpeg';
-const zoomed = '../assets/Products/chicken-biryani-plated-zoomed.jpeg';
-const sauteed = '../assets/Products/chicken-biryani-sauteed-onions.jpeg';
-const soakingAromatics = '../assets/Products/chicken-biryani-soaking-aromatics.jpeg';
-const soakingSaffron = '../assets/Products/chicken-biryani-soaking-saffron.jpeg';
-const spiceInPot = '../assets/Products/chicken-biryani-spice-marinade-in-pot.jpeg';
-const workstation = '../assets/Products/chicken-biryani-workstation.jpeg';
+const biryani1 = '../assets/Products/chicken-biryani-1.jpeg';
+const biryani2 = '../assets/Products/chicken-biryani-2.jpeg';
+const biryani3 = '../assets/Products/chicken-biryani-3.jpeg';
+const biryani4 = '../assets/Products/chicken-biryani-4.jpeg';
+const biryani5 = '../assets/Products/chicken-biryani-5.jpeg';
+const biryani6 = '../assets/Products/chicken-biryani-6.jpeg';
+const biryani7 = '../assets/Products/chicken-biryani-7.jpeg';
+const biryani8 = '../assets/Products/chicken-biryani-8.jpeg';
+const biryani9 = '../assets/Products/chicken-biryani-9.jpeg';
+const biryani10 = '../assets/Products/chicken-biryani-10.jpeg';
+const biryani11 = '../assets/Products/chicken-biryani-11.jpeg';
+const biryani12 = '../assets/Products/chicken-biryani-12.jpeg';
+const biryani13 = '../assets/Products/chicken-biryani-13.jpeg';
+const biryani14 = '../assets/Products/chicken-biryani-14.jpeg';
+const biryani15 = '../assets/Products/chicken-biryani-15.jpeg';
+const biryani16 = '../assets/Products/chicken-biryani-16.jpeg';
+const biryani17 = '../assets/Products/chicken-biryani-17.jpeg';
+const biryani18 = '../assets/Products/chicken-biryani-18.jpeg';
+const biryani19 = '../assets/Products/chicken-biryani-19.jpeg';
 
 const { ALLERGIES, CATEGORIES, DIET, GENRES, INGREDIENT_UNITS, METHODS, PROTEIN, REHEAT_METHODS, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
 const { BASMATI_RICE, BAY_LEAF, CARDAMOM, CARDAMOM_PODS, CAYENNE_PEPPER, CHICKEN_THIGH, CILANTRO, CINNAMON, CLOVES, CORIANDER, CUMIN, GARAM_MASALA, GARLIC, GHEE, GINGER, LARGE_POT, PAPER_TOWELS, PAPRIKA, PLATE, POT, SAFFRON_THREADS, SALT, SAUCEPAN, SMALL_BOWL, STAR_ANISE, STOVE, TURMERIC, VEGETABLE_OIL, WATER, YELLOW_ONION, YOGURT } = require('./ingredients');
@@ -27,7 +27,7 @@ const SAFFRON_WATER = 'Saffron Water';
 module.exports = {
     cardName: 'Chicken Biryani',
     name: 'Chicken Biryani',
-    img: clean,
+    img: biryani19,
     recipeAuthors: ['Nagi Maehashi'],
     recipeFinder: 'Jon Browne',
     available: true,
@@ -95,30 +95,30 @@ module.exports = {
         SAUCEPAN,
     ],
     directions: [
-        { step: 'In a large pot, mix the marinade ingredients.', section: SECTIONS.CHICKEN, img: [spiceInPot, marinade] },
-        { step: 'Add chicken to the marinade and coat the chicken. Marinade for at least 20 minutes to overnight.', section: SECTIONS.CHICKEN, img: marinating },
+        { step: 'In a large pot, mix the marinade ingredients.', section: SECTIONS.CHICKEN, img: [biryani1, biryani2] },
+        { step: 'Add chicken to the marinade and coat the chicken. Marinade for at least 20 minutes to overnight.', section: SECTIONS.CHICKEN, img: biryani3 },
         { step: 'Over medium-high heat, bring 3 liters of water to boil.', section: SECTIONS.RICE },
-        { step: 'Add salt and whole spices to the water.', section: SECTIONS.RICE, img: [aromatics, soakingAromatics] },
-        { step: 'Add rice and bring to a boil.', section: SECTIONS.RICE, img: boiled },
+        { step: 'Add salt and whole spices to the water.', section: SECTIONS.RICE, img: [biryani4, biryani5] },
+        { step: 'Add rice and bring to a boil.', section: SECTIONS.RICE, img: biryani6 },
         { step: 'Cook until rice is al dente (about 4 minutes).', section: SECTIONS.RICE },
-        { step: 'Drain immediately and set aside. Note that the rice will be a little salty at this point but will balance out later on.', section: SECTIONS.RICE, img: drained },
+        { step: 'Drain immediately and set aside. Note that the rice will be a little salty at this point but will balance out later on.', section: SECTIONS.RICE, img: biryani7 },
         { step: 'Line a plate with paper towels.', section: CRISPY_ONION },
         { step: 'Over medium-high heat, heat oil in a saucepan.', section: CRISPY_ONION },
-        { step: 'Add onion and cook until golden brown (don\'t burn or it will be bitter). Cook in batches as needed.', section: CRISPY_ONION, img: sauteed },
+        { step: 'Add onion and cook until golden brown (don\'t burn or it will be bitter). Cook in batches as needed.', section: CRISPY_ONION, img: biryani8 },
         { step: 'Place the onions on the paper towel lined plate to drain off some of the oil.', section: CRISPY_ONION },
-        { step: 'In a bowl, soak the saffron threads in the warm water.', section: SAFFRON_WATER, img: soakingSaffron },
-        { step: 'Over medium heat, cover with lid and cook the pot of marinading chicken for 5 minutes.', section: SECTIONS.MAIN, img: [cooking, workstation] },
-        { step: 'Uncover and cook for 5 minutes, turning chicken twice.', section: SECTIONS.MAIN, img: partially },
+        { step: 'In a bowl, soak the saffron threads in the warm water.', section: SAFFRON_WATER, img: biryani9 },
+        { step: 'Over medium heat, cover with lid and cook the pot of marinading chicken for 5 minutes.', section: SECTIONS.MAIN, img: [biryani10, biryani11] },
+        { step: 'Uncover and cook for 5 minutes, turning chicken twice.', section: SECTIONS.MAIN, img: biryani12 },
         { step: 'Remove from heat.', section: SECTIONS.MAIN },
-        { step: 'Arrange the chicken in the pot with the skin side down.', section: SECTIONS.MAIN, img: flipped },
-        { step: 'Add half of the sauteed onions and half of the cilantro.', section: SECTIONS.MAIN, img: layeredVeggies },
-        { step: 'Add the cooked rice and gently flatten.', section: SECTIONS.MAIN, img: layeredRice },
-        { step: 'Top with saffron.', section: SECTIONS.MAIN, img: layeredSaffron },
-        { step: 'Drizzle with ghee.', section: SECTIONS.MAIN, img: layeredGhee },
+        { step: 'Arrange the chicken in the pot with the skin side down.', section: SECTIONS.MAIN, img: biryani13 },
+        { step: 'Add half of the sauteed onions and half of the cilantro.', section: SECTIONS.MAIN, img: biryani14 },
+        { step: 'Add the cooked rice and gently flatten.', section: SECTIONS.MAIN, img: biryani15 },
+        { step: 'Top with saffron.', section: SECTIONS.MAIN, img: biryani16 },
+        { step: 'Drizzle with ghee.', section: SECTIONS.MAIN, img: biryani17 },
         { step: 'Cover and return to stove over medium heat.', section: SECTIONS.MAIN },
         { step: 'Once you see steam, turn down the heat to low and cook for 25 minutes.', section: SECTIONS.MAIN },
         { step: 'Remove from heat and let it rest for 10 minutes with the lid on.', section: SECTIONS.MAIN },
-        { step: 'Serve this authentic chicken rice dish with cilantro, yogurt, and sauteed onions.', section: SECTIONS.SERVE, img: zoomed },
+        { step: 'Serve this authentic chicken rice dish with cilantro, yogurt, and sauteed onions.', section: SECTIONS.SERVE, img: biryani18 },
     ],
     store: [
         {
