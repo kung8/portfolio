1d68558df4421a747d1468aa864670e850c3f934
Kevin Ung <ung.kevin78@gmail.com>
2025-10-19 00:54:08 -0600

    update chicken cordon bleu images

--- server/data/recipes.js
+++ server/data/recipes.js
@@ -761,7 +761,6 @@ module.exports = recipes;
 // and sautéed vegetables. (32)
 
 // TODO: update the images for the following recipes:
-// - chicken-cordon-bleu
 // - chicken-katsudon
 // - chicken-noodle-soup (add a number)
 // - chicken-quesadilla (lower the number)
--- server/data/recipes/chicken-cordon-bleu.js
+++ server/data/recipes/chicken-cordon-bleu.js
@@ -1,14 +1,14 @@
-const breadcrumb = '../assets/Products/chicken-cordon-bleu-breadcrumb.jpeg';
-const cookedZoomed = '../assets/Products/chicken-cordon-bleu-cooked-zoomed.jpeg';
-const hamAndCheese = '../assets/Products/chicken-cordon-bleu-ham-and-cheese.jpeg';
-const zoomed = '../assets/Products/chicken-cordon-bleu-on-plate-zoomed.jpeg';
-const rawGrouped = '../assets/Products/chicken-cordon-bleu-raw-grouped.jpeg';
-const rawSlices = '../assets/Products/chicken-cordon-bleu-raw-slices.jpeg';
-const saltAndPepper = '../assets/Products/chicken-cordon-bleu-salt-and-pepper.jpeg';
-const plated1 = '../assets/Products/chicken-cordon-bleu-plated-1.jpeg';
-const sauce1 = '../assets/Products/chicken-cordon-bleu-sauce-1.jpeg';
-const sauce3 = '../assets/Products/chicken-cordon-bleu-sauce-3.jpeg';
-const cooked1 = '../assets/Products/chicken-cordon-bleu-cooked-1.jpeg';
+const bleu1 = '../assets/Products/chicken-cordon-bleu-1.jpeg';
+const bleu2 = '../assets/Products/chicken-cordon-bleu-2.jpeg';
+const bleu3 = '../assets/Products/chicken-cordon-bleu-3.jpeg';
+const bleu4 = '../assets/Products/chicken-cordon-bleu-4.jpeg';
+const bleu5 = '../assets/Products/chicken-cordon-bleu-5.jpeg';
+const bleu6 = '../assets/Products/chicken-cordon-bleu-6.jpeg';
+const bleu7 = '../assets/Products/chicken-cordon-bleu-7.jpeg';
+const bleu8 = '../assets/Products/chicken-cordon-bleu-8.jpeg';
+const bleu9 = '../assets/Products/chicken-cordon-bleu-9.jpeg';
+const bleu10 = '../assets/Products/chicken-cordon-bleu-10.jpeg';
+const bleu11 = '../assets/Products/chicken-cordon-bleu-11.jpeg';
 
 const { ALLERGIES, CATEGORIES, DIET, GENRES, INGREDIENT_UNITS, METHODS, PROTEIN, REHEAT_METHODS, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
 const {
@@ -26,7 +26,7 @@ const {
 module.exports = {
     cardName: 'Chicken Cordon Bleu',
     name: 'Chicken Cordon Bleu',
-    img: zoomed,
+    img: bleu11,
     recipeAuthors: ['Lauren Allen'],
     recipeFinder: 'Kevin Ung',
     available: true,
@@ -68,18 +68,18 @@ module.exports = {
         { name: 'sauce pan' },
     ],
     directions: [
-        { step: 'Cut chicken breast in half horizontally so it is less thick and will be easier to work with.', section: SECTIONS.MAIN, img: rawSlices },
+        { step: 'Cut chicken breast in half horizontally so it is less thick and will be easier to work with.', section: SECTIONS.MAIN, img: bleu1 },
         { step: 'Pat the chicken dry with paper towels.', section: SECTIONS.MAIN },
-        { step: 'Lay the halved cheese slices and halved ham slices on top of the breasts.', section: SECTIONS.MAIN, img: hamAndCheese },
-        { step: 'Fold over the chicken to enclose the ham and cheese.', section: SECTIONS.MAIN, img: rawGrouped },
+        { step: 'Lay the halved cheese slices and halved ham slices on top of the breasts.', section: SECTIONS.MAIN, img: bleu2 },
+        { step: 'Fold over the chicken to enclose the ham and cheese.', section: SECTIONS.MAIN, img: bleu3 },
         { step: 'Preheat the oven to 400ºF when ready to bake.', section: SECTIONS.MAIN },
         { step: 'Crush the cornflakes cereal until it becomes fine and place in a bowl as a "dredging" station.', section: SECTIONS.MAIN },
-        { step: 'Add salt and pepper to the chicken on both sides.', section: SECTIONS.MAIN, img: saltAndPepper },
+        { step: 'Add salt and pepper to the chicken on both sides.', section: SECTIONS.MAIN, img: bleu4 },
         { step: 'Melt the butter and brush the chicken on both sides.', section: SECTIONS.MAIN },
-        { step: 'Dip the chicken into the cornflakes and place in a baking pan.', section: SECTIONS.MAIN, img: breadcrumb },
-        { step: 'Bake on the center rack for 30 minutes, or until the chicken is cooked through (160ºF).', section: SECTIONS.MAIN, img: cooked1 },
-        { step: 'Warm mayo and mustard together in a sauce pan right before you serve it with the chicken cordon bleu.', section: SECTIONS.SAUCE, img: [sauce1, sauce3] },
-        { step: 'Serve the chicken cordon bleu with the warm mayo and mustard sauce.', section: SECTIONS.SERVE, img: [cookedZoomed, plated1] },
+        { step: 'Dip the chicken into the cornflakes and place in a baking pan.', section: SECTIONS.MAIN, img: bleu5 },
+        { step: 'Bake on the center rack for 30 minutes, or until the chicken is cooked through (160ºF).', section: SECTIONS.MAIN, img: bleu6 },
+        { step: 'Warm mayo and mustard together in a sauce pan right before you serve it with the chicken cordon bleu.', section: SECTIONS.SAUCE, img: [bleu7, bleu8] },
+        { step: 'Serve the chicken cordon bleu with the warm mayo and mustard sauce.', section: SECTIONS.SERVE, img: [bleu9, bleu10] },
     ],
     notes: [
         'You can choose to freeze the chicken cordon bleu up to 3 months. Thaw in the refrigerator overnight before baking.'
