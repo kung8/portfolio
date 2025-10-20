0153abbce8673b8fe6ec757340d21db8fd543292
Kevin Ung <ung.kevin78@gmail.com>
2025-10-19 20:15:21 -0600

    update garlic chicken with herbs images

--- server/data/recipes/garlic-chicken-with-herbs.js
+++ server/data/recipes/garlic-chicken-with-herbs.js
@@ -1,15 +1,6 @@
-const butter = '../assets/Products/garlic-chicken-with-herbs-butter.jpeg';
-const cooked1 = '../assets/Products/garlic-chicken-with-herbs-cooked-chicken-1.jpeg';
-const cooked2 = '../assets/Products/garlic-chicken-with-herbs-cooked-chicken-2.jpeg';
-const chicken1 = '../assets/Products/garlic-chicken-with-herbs-cooking-chicken-1.jpeg';
-const zoomed = '../assets/Products/garlic-chicken-with-herbs-cooking-chicken-zoomed.jpeg';
-const covered2 = '../assets/Products/garlic-chicken-with-herbs-covered-2.jpeg';
-const flipped = '../assets/Products/garlic-chicken-with-herbs-flipped-chicken.jpeg';
-const plated = '../assets/Products/garlic-chicken-with-herbs-plated.jpeg';
-const roasting = '../assets/Products/garlic-chicken-with-herbs-roasting-garlic.jpeg';
-const sauteed = '../assets/Products/garlic-chicken-with-herbs-sauteed-fragrant.jpeg';
-const seasoned = '../assets/Products/garlic-chicken-with-herbs-seasoned-chicken.jpeg';
-const wine = '../assets/Products/garlic-chicken-with-herbs-white-wine.jpeg';
+const chicken1 = '../assets/Products/garlic-chicken-with-herbs-1.jpeg';
+const chicken2 = '../assets/Products/garlic-chicken-with-herbs-2.jpeg';
+const chicken3 = '../assets/Products/garlic-chicken-with-herbs-3.jpeg';
 
 const { ALLERGIES, CATEGORIES, DIET, GENRES, INGREDIENT_UNITS, METHODS, PROTEIN, REHEAT_METHODS, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
 const { BLACK_PEPPER, BONELESS_AND_SKINLESS_CHICKEN_THIGH, CHICKEN_BROTH, FRESH_ROSEMARY, FRESH_SAGE, GARLIC, JASMINE_RICE, LARGE_PAN, SALT, STOVE, UNSALTED_BUTTER, WHITE_WINE } = require('./ingredients');
@@ -17,7 +8,7 @@ const { BLACK_PEPPER, BONELESS_AND_SKINLESS_CHICKEN_THIGH, CHICKEN_BROTH, FRESH_
 module.exports = {
     cardName: 'Garlic Chicken w/ Herbs',
     name: 'Garlic Chicken with Herbs',
-    img: plated,
+    img: chicken3,
     recipeAuthors: ['Jan Valdez'],
     recipeFinder: 'Kevin Ung',
     available: true,
@@ -54,17 +45,17 @@ module.exports = {
         LARGE_PAN,
     ],
     directions: [
-        { step: 'Season chicken with salt and pepper.', section: SECTIONS.CHICKEN, img: seasoned },
-        { step: 'Over medium-high heat, heat butter in a pan.', section: SECTIONS.CHICKEN, img: butter },
-        { step: 'Add chicken and brown on both sides.', section: SECTIONS.CHICKEN, img: [chicken1, flipped] },
-        { step: 'Remove chicken and set aside.', section: SECTIONS.CHICKEN, img: cooked1 },
-        { step: 'Over medium-high heat, saute garlic in the same pan.', section: SECTIONS.MAIN, img: [roasting] },
+        { step: 'Season chicken with salt and pepper.', section: SECTIONS.CHICKEN },
+        { step: 'Over medium-high heat, heat butter in a pan.', section: SECTIONS.CHICKEN },
+        { step: 'Add chicken and brown on both sides.', section: SECTIONS.CHICKEN },
+        { step: 'Remove chicken and set aside.', section: SECTIONS.CHICKEN, img: chicken1 },
+        { step: 'Over medium-high heat, saute garlic in the same pan.', section: SECTIONS.MAIN },
         { step: 'Cook until golden brown.', section: SECTIONS.MAIN },
-        { step: 'Add wine and herbs. Bring to a boil and stir.', section: SECTIONS.MAIN, img: [wine, sauteed] },
+        { step: 'Add wine and herbs. Bring to a boil and stir.', section: SECTIONS.MAIN },
         { step: 'Cook until wine is almost evaporated.', section: SECTIONS.MAIN },
-        { step: 'Add chicken broth and chicken. Bring to a boil.', section: SECTIONS.MAIN, img: cooked2 },
-        { step: 'Reduce heat to low. Cover and simmer (about 10 to 12 minutes).', section: SECTIONS.MAIN, img: covered2 },
-        { step: 'Cook until the internal temperature of the chicken is at least 165ºF.', section: SECTIONS.MAIN, img: zoomed },
+        { step: 'Add chicken broth and chicken. Bring to a boil.', section: SECTIONS.MAIN, img: chicken2 },
+        { step: 'Reduce heat to low. Cover and simmer (about 10 to 12 minutes).', section: SECTIONS.MAIN },
+        { step: 'Cook until the internal temperature of the chicken is at least 165ºF.', section: SECTIONS.MAIN },
         { step: 'Spoon juices over chicken and rice.', section: SECTIONS.SERVE },
     ],
     store: [
