aa39331fdabf9f5b5f3fd0293a83dc63d5717a17
Kevin Ung <ung.kevin78@gmail.com>
2025-10-30 20:30:00 -0600

    update garlic butter recipe

--- server/data/recipes/garlic-butter.js
+++ server/data/recipes/garlic-butter.js
@@ -1,6 +1,8 @@
 const butter1 = '../assets/Products/garlic-butter-1.jpeg';
-const { ALLERGIES, CATEGORIES, DIET, GENRES, INGREDIENT_UNITS, METHODS, REHEAT_METHODS, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
+const { ALLERGIES, CATEGORIES, DIET, GENRES, INGREDIENT_UNITS, METHODS, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
 const { CHIVE, GARLIC, LEMON_JUICE, PARSLEY, UNSALTED_BUTTER } = require('./ingredients');
+
+const GARLIC_BUTTER = 'Garlic Butter';
 
 module.exports = {
     cardName: 'Garlic Butter',
@@ -26,19 +28,19 @@ module.exports = {
             finder: 'Kevin Ung', 
         }
     ],
-    separated: false,
+    separated: true,
     ingredients: [
-        { ...UNSALTED_BUTTER, amount: 1, unit: INGREDIENT_UNITS.CUP, additionalDetails: 'softened', section: SECTIONS.MAIN },
-        { ...GARLIC, amount: 2, unit: INGREDIENT_UNITS.CLOVE, additionalDetails: 'minced', section: SECTIONS.MAIN },
-        { ...PARSLEY, amount: 2, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: 'chopped', section: SECTIONS.MAIN },
-        { ...CHIVE, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: 'chopped', section: SECTIONS.MAIN },
-        { ...LEMON_JUICE, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.MAIN },
+        { ...UNSALTED_BUTTER, amount: 1, unit: INGREDIENT_UNITS.CUP, additionalDetails: 'softened', section: GARLIC_BUTTER },
+        { ...GARLIC, amount: 2, unit: INGREDIENT_UNITS.CLOVE, additionalDetails: 'minced', section: GARLIC_BUTTER },
+        { ...PARSLEY, amount: 2, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: 'chopped', section: GARLIC_BUTTER },
+        { ...CHIVE, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: 'chopped', section: GARLIC_BUTTER },
+        { ...LEMON_JUICE, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: GARLIC_BUTTER },
     ],
     supplies: [
         { name: 'medium bowl' },
     ],
     directions: [
-        { step: 'In a medium bowl, combine all the ingredients.', section: SECTIONS.MIX },
+        { step: `In a medium bowl, combine all the "${GARLIC_BUTTER}" section ingredients.`, section: SECTIONS.MIX },
         { step: 'Enjoy this with your favorite bread, pasta or vegetables.', section: SECTIONS.SERVE },
     ],
     store: [
@@ -47,12 +49,6 @@ module.exports = {
             location: STORAGE_LOCATION.FRIDGE,
             container: STORAGE_CONTAINER.AIRTIGHT,
         },
-    ],
-    reheat: [
-        // {
-        //     method: REHEAT_METHODS.BAKE,
-        //     instruction: '',
-        // },
     ],
     mealPrep: true,
 };
