f1e1eecd6493e34bb2883fb3bd800ea1530940e9
Kevin Ung <ung.kevin78@gmail.com>
2025-10-30 19:47:53 -0600

    update cashew chicken recipe

--- server/data/recipes/chinese-cashew-chicken.js
+++ server/data/recipes/chinese-cashew-chicken.js
@@ -33,7 +33,10 @@ const {
     SESAME_SEEDS,
     STOVE,
     WOK,
+    GARLIC,
 } = require('./ingredients');
+
+const OILS_SECTION = 'Oils';
 
 module.exports = {
     cardName: 'Cashew Chicken',
@@ -52,11 +55,11 @@ module.exports = {
     prepTime: { amount: 10, unit: TIME_UNITS.MINUTE },
     cookTime: { amount: 20, unit: TIME_UNITS.MINUTE },
     websites: [
-        { 
-            label: 'Cashew Chicken', 
+        {
+            label: 'Cashew Chicken',
             link: "https://www.averiecooks.com/better-than-takeout-cashew-chicken",
             authors: ['Averie Sunshine'],
-            finder: 'Kevin Ung', 
+            finder: 'Kevin Ung',
         }
     ],
     separated: true,
@@ -66,16 +69,22 @@ module.exports = {
         { ...RICE_WINE_VINEGAR, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.SAUCE },
         { ...ASIAN_CHILI_GARLIC_SAUCE, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.SAUCE },
         { ...GROUND_GINGER, amount: 3 / 4, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.SAUCE },
+
         { ...BONELESS_AND_SKINLESS_CHICKEN_BREAST, amount: 3 / 2, unit: INGREDIENT_UNITS.POUND, additionalDetails: '1 inch cubes', section: SECTIONS.CHICKEN },
         { ...CORNSTARCH, amount: 3, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.CHICKEN },
         { ...SALT, amount: 1 / 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.CHICKEN },
         { ...BLACK_PEPPER, amount: 1 / 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.CHICKEN },
-        { ...SESAME_OIL, amount: 2, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...OLIVE_OIL, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...BROCCOLI, amount: 1, unit: '', additionalDetails: '', section: SECTIONS.MAIN },
-        { ...RED_BELL_PEPPER, amount: 1, unit: '', additionalDetails: '', section: SECTIONS.MAIN },
-        { ...EDAMAME, amount: 1, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.MAIN },
+
+        { ...SESAME_OIL, amount: 2, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: OILS_SECTION },
+        { ...OLIVE_OIL, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: OILS_SECTION },
+
+        { ...BROCCOLI, amount: 1, unit: '', additionalDetails: 'cut into florets', section: SECTIONS.VEGGIES },
+        { ...RED_BELL_PEPPER, amount: 1, unit: '', additionalDetails: 'diced', section: SECTIONS.VEGGIES },
+        { ...EDAMAME, amount: 1, unit: INGREDIENT_UNITS.CUP, additionalDetails: 'frozen', section: SECTIONS.VEGGIES },
+        { ...GARLIC, amount: 2, unit: INGREDIENT_UNITS.CLOVE, additionalDetails: 'minced', section: SECTIONS.VEGGIES },
+
         { ...CASHEWS, amount: 1, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.MAIN },
+
         { ...GREEN_ONION, amount: 4, unit: '', additionalDetails: 'chopped diagonally', section: SECTIONS.TOPPINGS },
         { ...SESAME_SEEDS, amount: '', unit: '', additionalDetails: 'optional', section: SECTIONS.TOPPINGS },
     ],
@@ -88,16 +97,19 @@ module.exports = {
         { ...LARGE_BOWL, amount: 2 },
     ],
     directions: [
-        { step: 'In a medium bowl, combine the sauce ingredients. Mix well.', section: SECTIONS.SAUCE, img: chicken1 },
-        { step: 'In a large bowl, combine diced chicken, salt, pepper and cornstarch. Mix well until chicken is well coated.', section: SECTIONS.CHICKEN, img: chicken2 },
+        { step: `In a medium bowl, combine the "${SECTIONS.SAUCE}" section ingredients. Mix well.`, section: SECTIONS.SAUCE, img: chicken1 },
+
+        { step: `In a large bowl, combine the "${SECTIONS.CHICKEN}" section ingredients. Mix well until chicken is well coated.`, section: SECTIONS.CHICKEN, img: chicken2 },
         { step: 'Over medium-high heat, add the combination of oils in a wok.', section: SECTIONS.CHICKEN, img: chicken3 },
         { step: 'Add the chicken and cook until the chicken is about 80% cooked through (about 3 to 4 minutes on each side).', section: SECTIONS.CHICKEN, img: chicken4 },
         { step: 'Remove chicken from the wok and transfer to a clean large bowl.', section: SECTIONS.CHICKEN, img: chicken5 },
-        { step: 'Add the broccoli, bell peppers, frozen edamame, and garlic to the wok. Cook until the vegetables are tender (about 3 to 4 minutes).', section: SECTIONS.MAIN, img: chicken6 },
+
+        { step: `In the wok, add the "${SECTIONS.VEGGIES}" section ingredients. Cook until the vegetables are tender (about 3 to 4 minutes).`, section: SECTIONS.MAIN, img: chicken6 },
         { step: 'Add the cashews. Stir until mixed.', section: SECTIONS.MAIN, img: chicken7 },
         { step: 'Re-add the chicken. Stir until mixed and chicken is cooked through.', section: SECTIONS.MAIN, img: chicken8 },
         { step: 'Add the sauce and mix until well coated. Cook for a minute or two.', section: SECTIONS.MAIN, img: chicken9 },
         { step: 'Turn off the stove and remove from heat.', section: SECTIONS.MAIN, img: chicken10 },
+
         { step: 'Top this dish with green onions and sesame seeds and serve wth steamed rice.', section: SECTIONS.SERVE },
     ],
     store: [
