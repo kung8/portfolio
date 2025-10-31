663997cc03958cf7a2e4fc4adeec28c7a9fa5eef
Kevin Ung <ung.kevin78@gmail.com>
2025-10-30 20:26:20 -0600

    update buttered corn recipe

--- server/data/recipes/buttered-corn.js
+++ server/data/recipes/buttered-corn.js
@@ -3,6 +3,14 @@ const corn1 = '../assets/Products/buttered-corn-1.jpeg';
 const { ALLERGIES, CATEGORIES, DIET, GENRES, INGREDIENT_UNITS, METHODS, REHEAT_METHODS, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
 const { BLACK_PEPPER, CANNED_SWEET_CORN, HEAVY_CREAM, HONEY, SALT, SAUCEPAN, STOVE, UNSALTED_BUTTER } = require('./ingredients');
 
+const CORN_SECTION = 'Corn';
+const CREAM_SECTION = 'Cream';
+const HONEY_SECTION = 'Honey';
+
+const COOK_CORN = 'Cook Corn';
+const COOK_CREAM = 'Cook Cream';
+const COOK_HONEY = 'Cook Honey';
+
 module.exports = {
     cardName: 'Buttered Corn',
     name: 'Buttered Corn',
@@ -26,14 +34,14 @@ module.exports = {
             finder: 'Kevin Ung', 
         }
     ],
-    separated: false,
+    separated: true,
     ingredients: [
-        { ...UNSALTED_BUTTER, amount: 4, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...CANNED_SWEET_CORN, amount: 58, unit: INGREDIENT_UNITS.OUNCE, additionalDetails: 'drained', section: SECTIONS.MAIN },
-        { ...SALT, amount: 1 / 4, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...BLACK_PEPPER, amount: 1 / 4, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...HEAVY_CREAM, amount: 1 / 4, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...HONEY, amount: 1 / 4, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.MAIN },
+        { ...UNSALTED_BUTTER, amount: 4, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: CORN_SECTION },
+        { ...CANNED_SWEET_CORN, amount: 58, unit: INGREDIENT_UNITS.OUNCE, additionalDetails: 'drained', section: CORN_SECTION },
+        { ...SALT, amount: 1 / 4, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: CORN_SECTION },
+        { ...BLACK_PEPPER, amount: 1 / 4, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: CORN_SECTION },
+        { ...HEAVY_CREAM, amount: 1 / 4, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: CREAM_SECTION },
+        { ...HONEY, amount: 1 / 4, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: HONEY_SECTION },
     ],
     appliances: [
         STOVE,
@@ -42,12 +50,15 @@ module.exports = {
         SAUCEPAN,
     ],
     directions: [
-        { step: 'Over medium heat, melt butter in a saucepan.', section: SECTIONS.MAIN },
-        { step: 'Add corn, salt and pepper. Cook for 5 to 10 minutes. Stirring occasionally.', section: SECTIONS.MAIN },
-        { step: 'Add heavy cream. Reduce heat.', section: SECTIONS.MAIN },
-        { step: 'Cook until sauce has reduced slightly (about 5 minutes).', section: SECTIONS.MAIN },
-        { step: 'Add honey. Cook for 2 minutes.', section: SECTIONS.MAIN },
-        { step: 'Serve warm and enjoy this sweet appetizer.', section: SECTIONS.MAIN },
+        { step: 'Over medium heat, melt butter in a saucepan.', section: COOK_CORN },
+        { step: 'Add corn, salt and pepper. Cook for 5 to 10 minutes. Stirring occasionally.', section: COOK_CORN },
+
+        { step: 'Add heavy cream. Reduce heat.', section: COOK_CREAM },
+        { step: 'Cook until sauce has reduced slightly (about 5 minutes).', section: COOK_CREAM },
+
+        { step: 'Add honey. Cook for 2 minutes.', section: COOK_HONEY },
+
+        { step: 'Serve warm and enjoy this sweet appetizer.', section: SECTIONS.SERVE },
     ],
     store: [
         {
