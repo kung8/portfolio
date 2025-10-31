76a4b548880bd4882eb01a000f2d3b875c25476c
Kevin Ung <ung.kevin78@gmail.com>
2025-10-31 00:02:48 -0600

    update buttermilk syrup recipe

--- server/data/recipes/buttermilk-syrup.js
+++ server/data/recipes/buttermilk-syrup.js
@@ -13,6 +13,12 @@ const {
     WHITE_SUGAR,
 } = require('./ingredients');
 
+const SUGAR_MIXTURE = 'Sugar Mixture';
+const FOAM_MIXTURE = 'Foam Mixture';
+
+const COOK_SUGAR_MIXTURE = 'Cook Sugar Mixture';
+const MIX_FOAM_MIXTURE = 'Mix Foam Mixture';
+
 module.exports = {
     cardName: 'Buttermilk Syrup',
     name: 'Buttermilk Syrup',
@@ -29,20 +35,21 @@ module.exports = {
     prepTime: { amount: 5, unit: TIME_UNITS.MINUTE },
     cookTime: { amount: 5, unit: TIME_UNITS.MINUTE },
     websites: [
-        { 
-            label: 'Buttermilk Syrup', 
+        {
+            label: 'Buttermilk Syrup',
             link: 'https://www.allrecipes.com/recipe/239478/grandmas-buttermilk-syrup/',
             authors: ['Katie Madrian'],
-            finder: 'Camille Pockrus', 
+            finder: 'Camille Pockrus',
         }
     ],
-    separated: false,
+    separated: true,
     ingredients: [
-        { ...WHITE_SUGAR, amount: 1, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.SAUCE },
-        { ...BUTTERMILK, amount: 1 / 2, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.SAUCE },
-        { ...UNSALTED_BUTTER, amount: 1 / 2, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.SAUCE },
-        { ...BAKING_SODA, amount: 1, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.SAUCE },
-        { ...VANILLA_EXTRACT, amount: 1, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.SAUCE },
+        { ...WHITE_SUGAR, amount: 1, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SUGAR_MIXTURE },
+        { ...BUTTERMILK, amount: 1 / 2, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SUGAR_MIXTURE },
+        { ...UNSALTED_BUTTER, amount: 1 / 2, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SUGAR_MIXTURE },
+
+        { ...BAKING_SODA, amount: 1, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: FOAM_MIXTURE },
+        { ...VANILLA_EXTRACT, amount: 1, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: FOAM_MIXTURE },
     ],
     appliances: [
         STOVE,
@@ -52,8 +59,13 @@ module.exports = {
         SPATULA,
     ],
     directions: [
-        { step: 'Combine sugar, buttermilk, and butter in a saucepan. And heat over medium-high heat until it boils, about 5 minutes. Stir occasionally.', section: SECTIONS.SAUCE, img: syrup1 },
-        { step: 'Remove from heat, and add baking soda and vanilla. And stir.', section: SECTIONS.SAUCE },
+        { step: `In a saucepan, combine "${SUGAR_MIXTURE}" section ingredients.`, section: COOK_SUGAR_MIXTURE, img: syrup1 },
+        { step: 'Over medium-high heat, bring to a boil (about 5 minutes). Stir occasionally.', section: COOK_SUGAR_MIXTURE, img: syrup1 },
+        { step: 'Remove from heat.', section: COOK_SUGAR_MIXTURE },
+
+        { step: 'Add baking soda and vanilla. Stir.', section: MIX_FOAM_MIXTURE },
+
+        { step: 'Enjoy this over your favorite breakfast foods.', section: SECTIONS.SERVE },
     ],
     store: [
         {
