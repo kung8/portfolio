f291b9fb5ba9213daac432a2f52ee5ce16bc625c
Kevin Ung <ung.kevin78@gmail.com>
2025-10-31 21:02:48 -0600

    update bbq chicken wings recipe

--- server/data/recipes/bbq-chicken-wings.js
+++ server/data/recipes/bbq-chicken-wings.js
@@ -11,7 +11,6 @@ const {
     GARLIC,
     HONEY,
     KETCHUP,
-    LARGE_BOWL,
     LARGE_PAN,
     LIGHT_SOY_SAUCE,
     PAPRIKA,
@@ -20,7 +19,11 @@ const {
     STOVE,
     UNSALTED_BUTTER,
     YELLOW_MUSTARD,
+    MIXING_BOWL,
 } = require('./ingredients');
+
+const CONDIMENTS_SECTION = 'Condiments';
+const ADD_CONDIMENTS = 'Add Condiments';
 
 module.exports = {
     cardName: 'BBQ Chicken Wings',
@@ -45,39 +48,46 @@ module.exports = {
             finder: 'Kevin Ung'
         }
     ],
-    separated: false,
+    separated: true,
     ingredients: [
-        { ...CHICKEN_DRUMETTES, amount: 14, unit: INGREDIENT_UNITS.PIECE, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...BAKING_SODA, amount: 1, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...ROOT_BEER, amount: 300, unit: INGREDIENT_UNITS.MILLILITER, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...UNSALTED_BUTTER, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...SALT, amount: 1, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...PAPRIKA, amount: 1, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...BLACK_PEPPER, amount: 1 / 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...GARLIC, amount: 4, unit: INGREDIENT_UNITS.CLOVE, additionalDetails: 'minced', section: SECTIONS.MAIN },
-        { ...LIGHT_SOY_SAUCE, amount: 2, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...HONEY, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...KETCHUP, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.MAIN },
-        { ...YELLOW_MUSTARD, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.MAIN },
+        { ...CHICKEN_DRUMETTES, amount: 14, unit: INGREDIENT_UNITS.PIECE, additionalDetails: '', section: SECTIONS.MARINADE },
+        { ...BAKING_SODA, amount: 1, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MARINADE },
+
+        { ...ROOT_BEER, amount: 300, unit: INGREDIENT_UNITS.MILLILITER, additionalDetails: '', section: SECTIONS.SAUCE },
+        { ...UNSALTED_BUTTER, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.SAUCE },
+        { ...SALT, amount: 1, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.SAUCE },
+        { ...PAPRIKA, amount: 1, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.SAUCE },
+        { ...BLACK_PEPPER, amount: 1 / 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.SAUCE },
+        { ...GARLIC, amount: 4, unit: INGREDIENT_UNITS.CLOVE, additionalDetails: 'minced', section: SECTIONS.SAUCE },
+        { ...LIGHT_SOY_SAUCE, amount: 2, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.SAUCE },
+
+        { ...HONEY, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: CONDIMENTS_SECTION },
+        { ...KETCHUP, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: CONDIMENTS_SECTION },
+        { ...YELLOW_MUSTARD, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: CONDIMENTS_SECTION },
     ],
     appliances: [
         STOVE,
     ],
     supplies: [
-        LARGE_BOWL,
+        MIXING_BOWL,
         { ...LARGE_PAN, additionalDetails: 'with lid' },
     ],
     directions: [
-        { step: 'Marinate chicken in baking soda and water.', section: SECTIONS.MARINADE, img: wings1 },
-        { step: 'Let it rest for 15 minutes', section: SECTIONS.MARINADE },
-        { step: 'Put in pan in a concentric circle and combine root beer, butter, salt, paprika, black pepper, garlic and soy sauce.', section: SECTIONS.MAIN, img: wings2 },
-        { step: 'Cover with a lid and cook for 5 minutes.', section: SECTIONS.MAIN },
-        { step: 'Uncover and skim off the froth.', section: SECTIONS.MAIN },
-        { step: 'Re-cover the pan and continue cooking for 10 minutes.', section: SECTIONS.MAIN },
-        { step: 'Uncover and stir.', section: SECTIONS.MAIN },
-        { step: 'Re-cover the pan and continue cooking for 5 minutes.', section: SECTIONS.MAIN },
-        { step: 'Drain some of the residual liquid.', section: SECTIONS.MAIN },
-        { step: 'Add the honey, ketchup and mustard in the pan. Stir.', section: SECTIONS.MAIN, img: wings3 },
+        { step: 'In a mixing bowl, add chicken and baking soda.', section: SECTIONS.MARINATE_CHICKEN },
+        { step: 'Fill the bowl with water to cover the chicken.', section: SECTIONS.MARINATE_CHICKEN },
+        { step: 'Marinate chicken for 15 minutes.', section: SECTIONS.MARINATE_CHICKEN, img: wings1 },
+
+        { step: 'Place chicken in a circle.', section: SECTIONS.COOK_CHICKEN, img: wings2 },
+        { step: `In mixing bowl, combine the "${SECTIONS.SAUCE}" section ingredients.`, section: SECTIONS.COOK_CHICKEN, img: wings2 },
+        { step: 'Cover with a lid and cook for 5 minutes.', section: SECTIONS.COOK_CHICKEN },
+        { step: 'Uncover and skim off the froth.', section: SECTIONS.COOK_CHICKEN },
+        { step: 'Re-cover the pan and continue cooking for 10 minutes.', section: SECTIONS.COOK_CHICKEN },
+        { step: 'Uncover and stir.', section: SECTIONS.COOK_CHICKEN },
+        { step: 'Re-cover the pan and continue cooking for 5 minutes.', section: SECTIONS.COOK_CHICKEN },
+        { step: 'Drain some of the residual liquid.', section: SECTIONS.COOK_CHICKEN },
+
+        { step: `Add the "${CONDIMENTS_SECTION}" section ingredients. Stir.`, section: ADD_CONDIMENTS, img: wings3 },
+
         { step: 'Enjoy this light appetizer. Feel free to add some spice if you like a kick.', section: SECTIONS.SERVE },
     ],
     store: [
