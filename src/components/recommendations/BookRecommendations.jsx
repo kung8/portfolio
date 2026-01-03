bc4d83b09c0f6a2e85416c27c345fa441557c3d1
Kevin Ung <ung.kevin78@gmail.com>
2026-01-02 21:48:48 -0700

    add year

--- src/components/recommendations/BookRecommendations.jsx
+++ src/components/recommendations/BookRecommendations.jsx
@@ -44,6 +44,11 @@ const bookFilterOptions = {
         options: [
             { label: 'All', value: 'all' },
             { label: 'N/A', value: 'NO DATE' },
+            // { label: '2030', value: 2030 },
+            // { label: '2029', value: 2029 },
+            // { label: '2028', value: 2028 },
+            // { label: '2027', value: 2027 },
+            { label: '2026', value: 2026 },
             { label: '2025', value: 2025 },
             { label: '2024', value: 2024 },
             { label: '2023', value: 2023 },
