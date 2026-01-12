f7c7662fa0b0235428501c90c9ddbfba676d2e16
Kevin Ung <ung.kevin78@gmail.com>
2026-01-11 22:41:05 -0700

    add coconut lime shortbread images

--- server/data/recipes/coconut-lime-shortbread.js
+++ server/data/recipes/coconut-lime-shortbread.js
@@ -1,4 +1,10 @@
-// const example1 = '../assets/Products/example-1.jpeg';
+const shortbread1 = '../assets/Products/coconut-lime-shortbread-1.jpeg';
+const shortbread2 = '../assets/Products/coconut-lime-shortbread-2.jpeg';
+const shortbread3 = '../assets/Products/coconut-lime-shortbread-3.jpeg';
+const shortbread4 = '../assets/Products/coconut-lime-shortbread-4.jpeg';
+const shortbread5 = '../assets/Products/coconut-lime-shortbread-5.jpeg';
+const shortbread6 = '../assets/Products/coconut-lime-shortbread-6.jpeg';
+
 const {
     CATEGORIES,
     GENRES,
@@ -21,12 +27,11 @@ const FLAVORINGS = 'Flavorings';
 const PREP_COOKIES = 'Prep Cookies';
 
 module.exports = {
-    wip: true,
     cardName: 'Coconut Lime Shortbread',
     name: 'Coconut Lime Shortbread',
-    img: '',
+    img: shortbread6,
     available: true,
-    recommended: false,
+    recommended: true,
     category: [CATEGORIES.DESSERT],
     genre: [GENRES.AMERICAN],
     method: [METHODS.BAKE],
@@ -83,20 +88,20 @@ module.exports = {
         { step: `Add the "${SUGAR_AND_EXTRACTS}" section ingredients. Beat on medium-high until combined and creamy (about 2 minutes). Scrape down as needed.`, section: SECTIONS.PREP_BATTER },
         { step: `Slowly add flour, 1/4 cup at a time, while beating on low. Beat until incorporated.`, section: SECTIONS.PREP_BATTER },
         { step: `Add "${FLAVORINGS}" section ingredients.`, section: SECTIONS.PREP_BATTER },
-        { step: `Beat on high until dough comes together and becomes smooth. Add water if it is not smooth after 2 minutes of beating.`, section: SECTIONS.PREP_BATTER },
+        { step: `Beat on high until dough comes together and becomes smooth. Add water if it is not smooth after 2 minutes of beating.`, section: SECTIONS.PREP_BATTER, img: shortbread1 },
 
         { step: `Lightly flour a flat surface.`, section: PREP_COOKIES },
         { step: `Transfer the dough to the floured surface.`, section: PREP_COOKIES },
-        { step: `Roll out dough until it is 1/4 inch thick.`, section: PREP_COOKIES },
+        { step: `Roll out dough until it is 1/4 inch thick.`, section: PREP_COOKIES, img: shortbread2 },
         { step: `Use a cookie cutter to cut dough into shapes. Reroll remainder and cut them into shapes.`, section: PREP_COOKIES },
-        { step: `Place each cookie 3 inch apart on the baking sheets.`, section: PREP_COOKIES },
+        { step: `Place each cookie 3 inch apart on the baking sheets.`, section: PREP_COOKIES, img: shortbread3 },
 
-        { step: `Bake until the edges are lightly browned (about 12 to 14 minutes).`, section: SECTIONS.BAKE },
+        { step: `Bake until the edges are lightly browned (about 12 to 14 minutes).`, section: SECTIONS.BAKE, img: shortbread4 },
 
-        { step: `Let them cool on the baking sheet for 5 minutes before transferring them to a cooling rack to cool completely.`, section: SECTIONS.COOL },
+        { step: `Let them cool on the baking sheet for 5 minutes before transferring them to a cooling rack to cool completely.`, section: SECTIONS.COOL, img: shortbread5 },
 
         { step: `Set up a double boiler on the stove and melt white chocolate chips until smooth.`, section: SECTIONS.DECORATE },
-        { step: `Dip cookies halfway.`, section: SECTIONS.DECORATE },
+        { step: `Dip cookies halfway or spread melted chocolate over the top.`, section: SECTIONS.DECORATE },
         { step: `Let them completely set (about 30 minutes).`, section: SECTIONS.DECORATE },
 
         { step: `Enjoy these sweet treats!`, section: SECTIONS.SERVE },
