d45e1596371f6218ed5c45e6e3d83d853caafea8
Kevin Ung <ung.kevin78@gmail.com>
2026-01-18 18:48:34 -0700

    update kolo mee images

--- server/data/recipes/kolo-mee.js
+++ server/data/recipes/kolo-mee.js
@@ -1,12 +1,13 @@
 const kolo1 = '../assets/Products/kolo-mee-1.jpeg';
 const kolo2 = '../assets/Products/kolo-mee-2.jpeg';
 const kolo3 = '../assets/Products/kolo-mee-3.jpeg';
-const kolo4 = '../assets/Products/kolo-mee-4.jpeg';
-const kolo5 = '../assets/Products/kolo-mee-5.jpeg';
+
 const { CATEGORIES, GENRES, INGREDIENT_UNITS, METHODS, PROTEIN, REHEAT_METHODS, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
 const { BABY_BOK_CHOY, CHOW_MEIN_NOODLES, DARK_SOY_SAUCE, FRIED_SHALLOTS, GARLIC, GREEN_ONION, GROUND_PORK, LARGE_POT, LIGHT_SOY_SAUCE, ONION_POWDER, OYSTER_SAUCE, SESAME_OIL, STOVE, THAI_CHILI_PEPPER, VEGETABLE_OIL, WHITE_PEPPER, WOK } = require('./ingredients');
+const roastedPorkBelly = require('./roasted-pork-belly');
 
 const MINCED_MEAT_SECTION = 'Minced Meat';
+const ROASTED_PORK_BELLY_SECTION = 'Roasted Pork Belly';
 const BOK_CHOY_SECTION = 'Bok Choy';
 const COOK_MINCED_MEAT = 'Cook Minced Meat';
 const COOK_BOK_CHOY = 'Cook Bok Choy';
@@ -14,7 +15,7 @@ const COOK_BOK_CHOY = 'Cook Bok Choy';
 module.exports = {
     cardName: 'Kolo Mee',
     name: 'Kolo Mee',
-    img: kolo5,
+    img: kolo3,
     available: true,
     recommended: true,
     category: [CATEGORIES.DINNER, CATEGORIES.LUNCH],
@@ -41,6 +42,8 @@ module.exports = {
         { ...OYSTER_SAUCE, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: MINCED_MEAT_SECTION },
         { ...SESAME_OIL, amount: 1 / 4, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: MINCED_MEAT_SECTION },
         { ...WHITE_PEPPER, amount: 1 / 4, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: MINCED_MEAT_SECTION },
+
+        { name: 'roasted pork belly', amount: 1, unit: INGREDIENT_UNITS.POUND, additionalDetails: '', section: ROASTED_PORK_BELLY_SECTION, link: { id: roastedPorkBelly.name, url: `recipes/${roastedPorkBelly.name}` } },
 
         { ...CHOW_MEIN_NOODLES, amount: 8, unit: INGREDIENT_UNITS.OUNCE, additionalDetails: '', section: SECTIONS.NOODLES },
 
@@ -69,11 +72,11 @@ module.exports = {
 
         { step: `Cook the chow mein noodles according to package instructions. As it loosens and is almost cooked, remove the noodles and place it in a cold water bath.`, section: SECTIONS.COOK_NOODLES },
         { step: `Re-add the cooled noodles back to the boiling water until it is cooked.`, section: SECTIONS.COOK_NOODLES },
-        { step: `Drain. Set the noodles on a plate.`, section: SECTIONS.COOK_NOODLES, img: kolo3 },
+        { step: `Drain. Set the noodles on a plate.`, section: SECTIONS.COOK_NOODLES },
 
         { step: `Over medium-high heat, boil a pot of water.`, section: COOK_BOK_CHOY },
         { step: `Add bok choy. Cook until tender (about 1 to 2 minutes).`, section: COOK_BOK_CHOY },
-        { step: `Drain. Set aside.`, section: COOK_BOK_CHOY, img: kolo4 },
+        { step: `Drain. Set aside.`, section: COOK_BOK_CHOY },
 
         { step: `To the plate of noodles, add the "${SECTIONS.SAUCE}" section ingredients. Mix.`, section: SECTIONS.ASSEMBLE },
         { step: `Top the dish with the minced meat, bok choy and any other preferred toppings.`, section: SECTIONS.ASSEMBLE, img: kolo2 },
@@ -81,7 +84,8 @@ module.exports = {
         { step: `Enjoy one of my favorite noodle dishes from where a piece of my heart is.`, section: SECTIONS.SERVE },
     ],
     notes: [
-        { note: 'Fresh egg noodles is preferred but you can use dried noodles.' }
+        { note: `Fresh chow mein noodles is preferred but you can use dried noodles.` },
+        { note: `Choose your form of protein: minced pork or roasted pork belly.` },
     ],
     store: [
         {
