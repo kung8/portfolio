3c6736265456e99efd48f95433385db72cbcd418
Kevin Ung <ung.kevin78@gmail.com>
2026-01-18 23:23:53 -0700

    remove avocado

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,9 @@
-import { Avocado } from './loaders/Avocado';
+// import { Avocado } from './loaders/Avocado';
 import { Watermelon } from './loaders/Watermelon';
 
 export const Loader = () => {
-    const loaders = [Avocado, Watermelon];
-    const randomLoader = loaders[Math.floor(Math.random() * loaders.length)];
-    return randomLoader();
+    // const loaders = [Avocado, Watermelon];
+    // const randomLoader = loaders[Math.floor(Math.random() * loaders.length)];
+    // return randomLoader();
+    return <Watermelon />;
 }
