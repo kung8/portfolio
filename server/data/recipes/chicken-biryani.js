ef771c482208df65a7c12f08b745bb4fe1bc6766
Kevin Ung <ung.kevin78@gmail.com>
2026-03-28 23:19:26 -0600

    update some biryani sections

--- server/data/recipes/chicken-biryani.js
+++ server/data/recipes/chicken-biryani.js
@@ -24,6 +24,10 @@ const { BASMATI_RICE, BAY_LEAF, CARDAMOM, CARDAMOM_PODS, CAYENNE_PEPPER, CHICKEN
 const CRISPY_ONION = 'Crispy Onion';
 const SAFFRON_WATER = 'Saffron Water';
 
+const PREP_SAFFRON_WATER = 'Prep Saffron Water';
+const SAUTE_ONION = 'Saute Onion';
+const COOK_BIRYANI = 'Cook Biryani';
+
 module.exports = {
     cardName: 'Chicken Biryani',
     name: 'Chicken Biryani',
@@ -48,21 +52,22 @@ module.exports = {
         }
     ],
     ingredients: [
+        { ...YOGURT, amount: 2 / 3, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.MARINADE },
+        { ...WATER, amount: 1 / 2, unit: INGREDIENT_UNITS.CUP, additionalDetails: 'cold', section: SECTIONS.MARINADE },
+        { ...VEGETABLE_OIL, amount: 2, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.MARINADE },
+        { ...GARLIC, amount: 6, unit: INGREDIENT_UNITS.CLOVE, additionalDetails: '', section: SECTIONS.MARINADE },
+        { ...GINGER, amount: 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MARINADE },
+        { ...TURMERIC, amount: 1 / 8, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MARINADE },
+        { ...CINNAMON, amount: 1 / 4, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MARINADE },
+        { ...CAYENNE_PEPPER, amount: 1 / 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MARINADE },
+        { ...CARDAMOM, amount: 1 / 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MARINADE },
+        { ...GARAM_MASALA, amount: 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MARINADE },
+        { ...CORIANDER, amount: 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MARINADE },
+        { ...CUMIN, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.MARINADE },
+        { ...PAPRIKA, amount: 2, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.MARINADE },
+        { ...SALT, amount: 7 / 4, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.MARINADE },
+
         { ...CHICKEN_THIGH, amount: 3 / 2, unit: INGREDIENT_UNITS.POUND, additionalDetails: 'halved', section: SECTIONS.CHICKEN },
-        { ...YOGURT, amount: 2 / 3, unit: INGREDIENT_UNITS.CUP, additionalDetails: '', section: SECTIONS.CHICKEN },
-        { ...WATER, amount: 1 / 2, unit: INGREDIENT_UNITS.CUP, additionalDetails: 'cold', section: SECTIONS.CHICKEN },
-        { ...VEGETABLE_OIL, amount: 2, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.CHICKEN },
-        { ...GARLIC, amount: 6, unit: INGREDIENT_UNITS.CLOVE, additionalDetails: '', section: SECTIONS.CHICKEN },
-        { ...GINGER, amount: 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.CHICKEN },
-        { ...TURMERIC, amount: 1 / 8, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.CHICKEN },
-        { ...CINNAMON, amount: 1 / 4, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.CHICKEN },
-        { ...CAYENNE_PEPPER, amount: 1 / 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.CHICKEN },
-        { ...CARDAMOM, amount: 1 / 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.CHICKEN },
-        { ...GARAM_MASALA, amount: 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.CHICKEN },
-        { ...CORIANDER, amount: 2, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.CHICKEN },
-        { ...CUMIN, amount: 1, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.CHICKEN },
-        { ...PAPRIKA, amount: 2, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.CHICKEN },
-        { ...SALT, amount: 7 / 4, unit: INGREDIENT_UNITS.TEASPOON, additionalDetails: '', section: SECTIONS.CHICKEN },
 
         { ...SALT, amount: 2, unit: INGREDIENT_UNITS.TABLESPOON, additionalDetails: '', section: SECTIONS.RICE },
         { ...CLOVES, amount: 10, unit: '', additionalDetails: '', section: SECTIONS.RICE },
@@ -95,29 +100,34 @@ module.exports = {
         SAUCEPAN,
     ],
     directions: [
-        { step: `In a large pot, mix the marinade ingredients.`, section: SECTIONS.CHICKEN, img: [biryani1, biryani2] },
-        { step: `Add chicken to the marinade and coat the chicken. Marinade for at least 20 minutes to overnight.`, section: SECTIONS.CHICKEN, img: biryani3 },
-        { step: `Over medium-high heat, bring 3 liters of water to boil.`, section: SECTIONS.RICE },
-        { step: `Add salt and whole spices to the water.`, section: SECTIONS.RICE, img: [biryani4, biryani5] },
-        { step: `Add rice and bring to a boil.`, section: SECTIONS.RICE, img: biryani6 },
-        { step: `Cook until rice is al dente (about 4 minutes).`, section: SECTIONS.RICE },
-        { step: `Drain immediately and set aside. Note that the rice will be a little salty at this point but will balance out later on.`, section: SECTIONS.RICE, img: biryani7 },
-        { step: `Line a plate with paper towels.`, section: CRISPY_ONION },
-        { step: `Over medium-high heat, heat oil in a saucepan.`, section: CRISPY_ONION },
-        { step: `Add onion and cook until golden brown (don't burn or it will be bitter). Cook in batches as needed.`, section: CRISPY_ONION, img: biryani8 },
-        { step: `Place the onions on the paper towel lined plate to drain off some of the oil.`, section: CRISPY_ONION },
-        { step: `In a bowl, soak the saffron threads in the warm water.`, section: SAFFRON_WATER, img: biryani9 },
-        { step: `Over medium heat, cover with lid and cook the pot of marinading chicken for 5 minutes.`, section: SECTIONS.MAIN, img: [biryani10, biryani11] },
-        { step: `Uncover and cook for 5 minutes, turning chicken twice.`, section: SECTIONS.MAIN, img: biryani12 },
-        { step: `Remove from heat.`, section: SECTIONS.MAIN },
-        { step: `Arrange the chicken in the pot with the skin side down.`, section: SECTIONS.MAIN, img: biryani13 },
-        { step: `Add half of the sauteed onions and half of the cilantro.`, section: SECTIONS.MAIN, img: biryani14 },
-        { step: `Add the cooked rice and gently flatten.`, section: SECTIONS.MAIN, img: biryani15 },
-        { step: `Top with saffron.`, section: SECTIONS.MAIN, img: biryani16 },
-        { step: `Drizzle with ghee.`, section: SECTIONS.MAIN, img: biryani17 },
-        { step: `Cover and return to stove over medium heat.`, section: SECTIONS.MAIN },
-        { step: `Once you see steam, turn down the heat to low and cook for 25 minutes.`, section: SECTIONS.MAIN },
-        { step: `Remove from heat and let it rest for 10 minutes with the lid on.`, section: SECTIONS.MAIN },
+        { step: `In a large pot, mix the "${SECTIONS.MARINADE}" ingredients.`, section: SECTIONS.MARINATE_CHICKEN, img: [biryani1, biryani2] },
+        { step: `Add chicken to the marinade and coat the chicken. Marinate for at least 20 minutes to overnight.`, section: SECTIONS.MARINATE_CHICKEN, img: biryani3 },
+
+        { step: `Over medium-high heat, bring 3 liters of water to boil.`, section: SECTIONS.COOK_RICE },
+        { step: `Add salt and whole spices to the water.`, section: SECTIONS.COOK_RICE, img: [biryani4, biryani5] },
+        { step: `Add rice and bring to a boil.`, section: SECTIONS.COOK_RICE, img: biryani6 },
+        { step: `Cook until rice is al dente (about 4 minutes).`, section: SECTIONS.COOK_RICE },
+        { step: `Drain immediately and set aside. Note that the rice will be a little salty at this point but will balance out later on.`, section: SECTIONS.COOK_RICE, img: biryani7 },
+
+        { step: `Line a plate with paper towels.`, section: SAUTE_ONION },
+        { step: `Over medium-high heat, heat oil in a saucepan.`, section: SAUTE_ONION },
+        { step: `Add onion and cook until golden brown (don't burn or it will be bitter). Cook in batches as needed.`, section: SAUTE_ONION, img: biryani8 },
+        { step: `Place the onions on the paper towel lined plate to drain off some of the oil.`, section: SAUTE_ONION },
+
+        { step: `In a bowl, soak the saffron threads in the warm water.`, section: PREP_SAFFRON_WATER, img: biryani9 },
+    
+        { step: `Over medium heat, cover with lid and cook the pot of chicken for 5 minutes.`, section: COOK_BIRYANI, img: [biryani10, biryani11] },
+        { step: `Uncover and cook for 5 minutes, turning chicken twice.`, section: COOK_BIRYANI, img: biryani12 },
+        { step: `Remove from heat.`, section: COOK_BIRYANI },
+        { step: `Arrange the chicken in the pot with the skin side down.`, section: COOK_BIRYANI, img: biryani13 },
+        { step: `Add half of the sauteed onions and half of the cilantro.`, section: COOK_BIRYANI, img: biryani14 },
+        { step: `Add the cooked rice and gently flatten.`, section: COOK_BIRYANI, img: biryani15 },
+        { step: `Top with saffron.`, section: COOK_BIRYANI, img: biryani16 },
+        { step: `Drizzle with ghee.`, section: COOK_BIRYANI, img: biryani17 },
+        { step: `Cover and return to stove over medium heat.`, section: COOK_BIRYANI },
+        { step: `Once you see steam, turn down the heat to low and cook for 25 minutes.`, section: COOK_BIRYANI },
+        { step: `Remove from heat and let it rest for 10 minutes with the lid on.`, section: COOK_BIRYANI },
+
         { step: `Serve this authentic chicken rice dish with cilantro, yogurt, and sauteed onions.`, section: SECTIONS.SERVE, img: biryani18 },
     ],
     store: [
