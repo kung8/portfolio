ee7ce707fe9f4f3cc3bbbd4e4e380c8cc81fa837
Kevin Ung <ung.kevin78@gmail.com>
2026-04-04 00:44:54 -0600

    add kalua pig images

--- server/data/recipes/kalua-pig.js
+++ server/data/recipes/kalua-pig.js
@@ -1,12 +1,16 @@
-// const example = '../assets/Products/example.jpeg';
+const kalua1 = '../assets/Products/kalua-pig-1.jpeg';
+const kalua2 = '../assets/Products/kalua-pig-2.jpeg';
+const kalua3 = '../assets/Products/kalua-pig-3.jpeg';
+const kalua4 = '../assets/Products/kalua-pig-4.jpeg';
+const kalua5 = '../assets/Products/kalua-pig-5.jpeg';
+
 const { CATEGORIES, GENRES, INGREDIENT_UNITS, METHODS, PROTEIN, REHEAT_METHODS, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
 const { FORK, FRYING_PAN, GARLIC, LIQUID_SMOKE, PORK_BUTT, SALT, SLOW_COOKER, STOVE } = require('./ingredients');
 
 module.exports = {
-    wip: true,
     cardName: 'Kalua Pig',
     name: 'Kalua Pig',
-    img: '',
+    img: kalua4,
     available: true,
     recommended: false,
     category: [CATEGORIES.LUNCH, CATEGORIES.DINNER],
@@ -24,7 +28,13 @@ module.exports = {
             link: null,
             authors: ['Andrew Luke'],
             finder: 'Andrew Luke'
-        }
+        },
+        {
+            label: 'Kalua Pig',
+            link: 'https://www.lecremedelacrumb.com/slow-cooker-kalua-pork/',
+            authors: ['Tiffany'],
+            finder: 'Kevin Ung'
+        },
     ],
     ingredients: [
         { ...PORK_BUTT, amount: 4, unit: INGREDIENT_UNITS.POUND, additionalDetails: 'or shoulder', section: SECTIONS.PORK },
@@ -41,16 +51,18 @@ module.exports = {
         FORK,
     ],
     directions: [
-        { step: `Over high heat, sear all sides of a roast until browned in a pan.`, section: SECTIONS.PREP_PORK },
+        { step: `Over high heat, sear all sides of a roast until browned in a pan.`, section: SECTIONS.PREP_PORK, img: kalua1 },
         { step: `Poke holes into the pork with a fork.`, section: SECTIONS.PREP_PORK },
-        { step: `Cut slits into the pork. Stuff the slits with cloves of garlic.`, section: SECTIONS.PREP_PORK },
+        { step: `Cut slits into the pork. Stuff the slits with cloves of garlic.`, section: SECTIONS.PREP_PORK, img: kalua2 },
         { step: `Place roast in slow cooker.`, section: SECTIONS.PREP_PORK },
         { step: `Pour liquid smoke over pork.`, section: SECTIONS.PREP_PORK },
-        { step: `Roast until cooked (about 8 to 12 hours).`, section: SECTIONS.COOK_PORK },
-        { step: `Remove some of the liquid. Set aside.`, section: SECTIONS.COOK_PORK },
-        { step: `Shred pork.`, section: SECTIONS.COOK_PORK },
-        { step: `Re-add the liquid.`, section: SECTIONS.COOK_PORK },
-        { step: `Enjoy this taste of Hawaii.`, section: SECTIONS.SERVE },
+
+        { step: `Roast on low until cooked (about 8 to 12 hours).`, section: SECTIONS.COOK_PORK, img: kalua3 },
+        { step: `Shred pork with forks.`, section: SECTIONS.COOK_PORK },
+        { step: `Enjoy this taste of Hawaii.`, section: SECTIONS.SERVE, img: kalua5 },
+    ],
+    notes: [
+        { note: `You do not need to add more liquid smoke than the recipe calls for.` },
     ],
     store: [
         {
