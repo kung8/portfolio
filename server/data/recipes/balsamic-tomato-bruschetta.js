0b9dd907a698621491edcf572a6cc14bdc5ebc29
Kevin Ung <ung.kevin78@gmail.com>
2026-05-31 18:59:43 -0600

    reduce bruschetta images

--- server/data/recipes/balsamic-tomato-bruschetta.js
+++ server/data/recipes/balsamic-tomato-bruschetta.js
@@ -1,21 +1,17 @@
 const bruschetta1 = '../assets/Products/bruschetta-1.jpeg';
 const bruschetta2 = '../assets/Products/bruschetta-2.jpeg';
-const bruschetta3 = '../assets/Products/bruschetta-3.jpeg';
-const bruschetta4 = '../assets/Products/bruschetta-4.jpeg';
-const bruschetta5 = '../assets/Products/bruschetta-5.jpeg';
 
 const { CATEGORIES, GENRES, INGREDIENT_UNITS, METHODS, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
 const { BAGUETTE, BAKING_SHEET, BALSAMIC_VINEGAR, BASIL_LEAF, BLACK_PEPPER, CHERRY_TOMATO, GARLIC, MEDIUM_BOWL, OLIVE_OIL, OVEN, SALT, SHALLOT } = require('./ingredients');
 
 const BAGUETTE_SECTION = 'Baguette';
 const BRUSCHETTA_SECTION = 'Bruschetta';
-const BAKE_BAGUETTE = 'Bake Baguette';
 const PREP_BRUSCHETTA = 'Prep Bruschetta';
 
 module.exports = {
     cardName: 'Bruschetta',
     name: 'Balsamic Tomato Bruschetta',
-    img: bruschetta4,
+    img: bruschetta2,
     available: true,
     recommended: true,
     category: [CATEGORIES.APPETIZER, CATEGORIES.SIDE_DISH, CATEGORIES.SNACK],
@@ -59,16 +55,16 @@ module.exports = {
     directions: [
         { step: `Preheat the oven to 375ºF.`, section: SECTIONS.PREHEAT_OVEN },
 
-        { step: `Place sliced baguette on baking sheet.`, section: SECTIONS.BAKE, img: bruschetta2 },
+        { step: `Place sliced baguette on baking sheet.`, section: SECTIONS.BAKE },
         { step: `Drizzle olive oil over bread (do not drench).`, section: SECTIONS.BAKE },
         { step: `Season with salt and pepper.`, section: SECTIONS.BAKE },
         { step: `Flip slices over and repeat with the other side.`, section: SECTIONS.BAKE },
         { step: `Bake until golden brown and crispy (about 5 to 7 minutes on each side). Be careful not to burn them.`, section: SECTIONS.BAKE },
 
-        { step: `Drain the diced tomatoes as needed. Optionally add a pinch of salt.`, section: PREP_BRUSCHETTA, img: bruschetta1 },
-        { step: `In a medium bowl, combine the "${BRUSCHETTA_SECTION}" section ingredients.`, section: PREP_BRUSCHETTA, img: bruschetta3 },
+        { step: `Drain the diced tomatoes as needed. Optionally add a pinch of salt.`, section: PREP_BRUSCHETTA },
+        { step: `In a medium bowl, combine the "${BRUSCHETTA_SECTION}" section ingredients.`, section: PREP_BRUSCHETTA, img: bruschetta1 },
 
-        { step: `Top each slice with about 2 tablespoons of the bruschetta mixture.`, section: SECTIONS.ASSEMBLE, img: bruschetta5 },
+        { step: `Top each slice with about 2 tablespoons of the bruschetta mixture.`, section: SECTIONS.ASSEMBLE },
         
         { step: `Serve this appetizer immediately topped with more basil.`, section: SECTIONS.SERVE },
     ],
