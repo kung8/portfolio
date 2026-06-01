51b539b8e2b5976713869743a72035cda10cc181
Kevin Ung <ung.kevin78@gmail.com>
2026-05-31 20:50:58 -0600

    reduce chicken biryani recipe images

--- server/data/recipes/chicken-biryani.js
+++ server/data/recipes/chicken-biryani.js
@@ -5,17 +5,6 @@ const biryani4 = '../assets/Products/chicken-biryani-4.jpeg';
 const biryani5 = '../assets/Products/chicken-biryani-5.jpeg';
 const biryani6 = '../assets/Products/chicken-biryani-6.jpeg';
 const biryani7 = '../assets/Products/chicken-biryani-7.jpeg';
-const biryani8 = '../assets/Products/chicken-biryani-8.jpeg';
-const biryani9 = '../assets/Products/chicken-biryani-9.jpeg';
-const biryani10 = '../assets/Products/chicken-biryani-10.jpeg';
-const biryani11 = '../assets/Products/chicken-biryani-11.jpeg';
-const biryani12 = '../assets/Products/chicken-biryani-12.jpeg';
-const biryani13 = '../assets/Products/chicken-biryani-13.jpeg';
-const biryani14 = '../assets/Products/chicken-biryani-14.jpeg';
-const biryani15 = '../assets/Products/chicken-biryani-15.jpeg';
-const biryani16 = '../assets/Products/chicken-biryani-16.jpeg';
-const biryani17 = '../assets/Products/chicken-biryani-17.jpeg';
-const biryani18 = '../assets/Products/chicken-biryani-18.jpeg';
 
 const { CATEGORIES, GENRES, INGREDIENT_UNITS, METHODS, PROTEIN, REHEAT_METHODS, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
 const { BASMATI_RICE, BAY_LEAF, CARDAMOM, CARDAMOM_PODS, CAYENNE_PEPPER, CHICKEN_THIGH, CILANTRO, CINNAMON, CLOVES, CORIANDER, CUMIN, GARAM_MASALA, GARLIC, GHEE, GINGER, LARGE_POT, PAPER_TOWELS, PAPRIKA, PLATE, POT, SAFFRON_THREADS, SALT, SAUCEPAN, SMALL_BOWL, STAR_ANISE, STOVE, TURMERIC, VEGETABLE_OIL, WATER, YELLOW_ONION, YOGURT } = require('./ingredients');
@@ -30,7 +19,7 @@ const COOK_BIRYANI = 'Cook Biryani';
 module.exports = {
     cardName: 'Chicken Biryani',
     name: 'Chicken Biryani',
-    img: biryani18,
+    img: biryani7,
     available: true,
     recommended: true,
     category: [CATEGORIES.LUNCH, CATEGORIES.DINNER],
@@ -99,30 +88,30 @@ module.exports = {
         SAUCEPAN,
     ],
     directions: [
-        { step: `In a large pot, mix the "${SECTIONS.MARINADE}" ingredients.`, section: SECTIONS.MARINATE_CHICKEN, img: [biryani1, biryani2] },
-        { step: `Add chicken to the marinade and coat the chicken. Marinate for at least 20 minutes to overnight.`, section: SECTIONS.MARINATE_CHICKEN, img: biryani3 },
+        { step: `In a large pot, mix the "${SECTIONS.MARINADE}" ingredients.`, section: SECTIONS.MARINATE_CHICKEN },
+        { step: `Add chicken to the marinade and coat the chicken. Marinate for at least 20 minutes to overnight.`, section: SECTIONS.MARINATE_CHICKEN },
 
         { step: `Over medium-high heat, bring 3 liters of water to boil.`, section: SECTIONS.COOK_RICE },
-        { step: `Add salt and whole spices to the water.`, section: SECTIONS.COOK_RICE, img: [biryani4, biryani5] },
-        { step: `Add rice and bring to a boil.`, section: SECTIONS.COOK_RICE, img: biryani6 },
+        { step: `Add salt and whole spices to the water.`, section: SECTIONS.COOK_RICE },
+        { step: `Add rice and bring to a boil.`, section: SECTIONS.COOK_RICE },
         { step: `Cook until rice is al dente (about 4 minutes).`, section: SECTIONS.COOK_RICE },
-        { step: `Drain immediately and set aside. Note that the rice will be a little salty at this point but will balance out later on.`, section: SECTIONS.COOK_RICE, img: biryani7 },
+        { step: `Drain immediately and set aside. Note that the rice will be a little salty at this point but will balance out later on.`, section: SECTIONS.COOK_RICE },
 
         { step: `Line a plate with paper towels.`, section: SAUTE_ONION },
         { step: `Over medium-high heat, heat oil in a saucepan.`, section: SAUTE_ONION },
-        { step: `Add onion and cook until golden brown (don't burn or it will be bitter). Cook in batches as needed.`, section: SAUTE_ONION, img: biryani8 },
+        { step: `Add onion and cook until golden brown (don't burn or it will be bitter). Cook in batches as needed.`, section: SAUTE_ONION },
         { step: `Place the onions on the paper towel lined plate to drain off some of the oil.`, section: SAUTE_ONION },
 
-        { step: `In a bowl, soak the saffron threads in the warm water.`, section: PREP_SAFFRON_WATER, img: biryani9 },
+        { step: `In a bowl, soak the saffron threads in the warm water.`, section: PREP_SAFFRON_WATER, img: biryani1 },
     
-        { step: `Over medium heat, cover with lid and cook the pot of chicken for 5 minutes.`, section: COOK_BIRYANI, img: [biryani10, biryani11] },
-        { step: `Uncover and cook for 5 minutes, turning chicken twice.`, section: COOK_BIRYANI, img: biryani12 },
+        { step: `Over medium heat, cover with lid and cook the pot of chicken for 5 minutes.`, section: COOK_BIRYANI },
+        { step: `Uncover and cook for 5 minutes, turning chicken twice.`, section: COOK_BIRYANI },
         { step: `Remove from heat.`, section: COOK_BIRYANI },
-        { step: `Arrange the chicken in the pot with the skin side down.`, section: COOK_BIRYANI, img: biryani13 },
-        { step: `Add half of the sauteed onions and half of the cilantro.`, section: COOK_BIRYANI, img: biryani14 },
-        { step: `Add the cooked rice and gently flatten.`, section: COOK_BIRYANI, img: biryani15 },
-        { step: `Top with saffron.`, section: COOK_BIRYANI, img: biryani16 },
-        { step: `Drizzle with ghee.`, section: COOK_BIRYANI, img: biryani17 },
+        { step: `Arrange the chicken in the pot with the skin side down.`, section: COOK_BIRYANI, img: biryani2 },
+        { step: `Add half of the sauteed onions and half of the cilantro.`, section: COOK_BIRYANI, img: biryani3 },
+        { step: `Add the cooked rice and gently flatten.`, section: COOK_BIRYANI, img: biryani4 },
+        { step: `Top with saffron.`, section: COOK_BIRYANI, img: biryani5 },
+        { step: `Drizzle with ghee.`, section: COOK_BIRYANI, img: biryani6 },
         { step: `Cover and return to stove over medium heat.`, section: COOK_BIRYANI },
         { step: `Once you see steam, turn down the heat to low and cook for 25 minutes.`, section: COOK_BIRYANI },
         { step: `Remove from heat and let it rest for 10 minutes with the lid on.`, section: COOK_BIRYANI },
