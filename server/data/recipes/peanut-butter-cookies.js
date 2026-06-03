032152735cad52688eaea7f1f108641049e16d12
Kevin Ung <ung.kevin78@gmail.com>
2026-06-02 22:17:08 -0600

    reduce peanut butter cookies images

--- server/data/recipes/peanut-butter-cookies.js
+++ server/data/recipes/peanut-butter-cookies.js
@@ -2,15 +2,6 @@ const cookies1 = '../assets/Products/peanut-butter-cookies-1.jpeg';
 const cookies2 = '../assets/Products/peanut-butter-cookies-2.jpeg';
 const cookies3 = '../assets/Products/peanut-butter-cookies-3.jpeg';
 const cookies4 = '../assets/Products/peanut-butter-cookies-4.jpeg';
-const cookies5 = '../assets/Products/peanut-butter-cookies-5.jpeg';
-const cookies6 = '../assets/Products/peanut-butter-cookies-6.jpeg';
-const cookies8 = '../assets/Products/peanut-butter-cookies-8.jpeg';
-const cookies9 = '../assets/Products/peanut-butter-cookies-9.jpeg';
-const cookies10 = '../assets/Products/peanut-butter-cookies-10.jpeg';
-const cookies11 = '../assets/Products/peanut-butter-cookies-11.jpeg';
-const cookies12 = '../assets/Products/peanut-butter-cookies-12.jpeg';
-const cookies13 = '../assets/Products/peanut-butter-cookies-13.jpeg';
-const cookies14 = '../assets/Products/peanut-butter-cookies-14.jpeg';
 
 const { CATEGORIES, GENRES, INGREDIENT_UNITS, METHODS, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
 const { ALL_PURPOSE_FLOUR, BAKING_POWDER, BAKING_SHEET, BROWN_SUGAR, EGG, OVEN, PEANUT_BUTTER, SALT, SILICONE_BAKING_MAT, STAND_MIXER, UNSALTED_BUTTER, VANILLA_EXTRACT, WHITE_SUGAR } = require('./ingredients');
@@ -18,7 +9,7 @@ const { ALL_PURPOSE_FLOUR, BAKING_POWDER, BAKING_SHEET, BROWN_SUGAR, EGG, OVEN, 
 module.exports = {
     cardName: 'Peanut Butter Cookies',
     name: 'Peanut Butter Cookies',
-    img: cookies13,
+    img: cookies4,
     available: true,
     recommended: false,
     category: [CATEGORIES.DESSERT],
@@ -57,15 +48,15 @@ module.exports = {
     ],
     directions: [
         { step: `Preheat the oven to 350ºF.`, section: SECTIONS.PREHEAT_OVEN },
-        { step: `In a stand mixer, cream together butter, white sugar, and brown sugar.`, section: SECTIONS.MAIN, img: [cookies1, cookies2] },
-        { step: `Mix in salt.`, section: SECTIONS.MAIN, img: cookies3 },
-        { step: `Mix in egg and vanilla.`, section: SECTIONS.MAIN, img: cookies4 },
-        { step: `Mix in peanut butter.`, section: SECTIONS.MAIN, img: [cookies5, cookies6] },
-        { step: `Mix in flour and baking powder.`, section: SECTIONS.MAIN, img: [cookies8, cookies9] },
+        { step: `In a stand mixer, cream together butter, white sugar, and brown sugar.`, section: SECTIONS.MAIN },
+        { step: `Mix in salt.`, section: SECTIONS.MAIN },
+        { step: `Mix in egg and vanilla.`, section: SECTIONS.MAIN },
+        { step: `Mix in peanut butter.`, section: SECTIONS.MAIN },
+        { step: `Mix in flour and baking powder.`, section: SECTIONS.MAIN },
         { step: `Place the silicon baking mat on the baking sheet.`, section: SECTIONS.MAIN },
-        { step: `Form one inch dough balls and place on the mats.`, section: SECTIONS.MAIN, img: cookies10 },
-        { step: `Flatten the cookies with a fork to form a crisscross pattern.`, section: SECTIONS.MAIN, img: [cookies11, cookies12] },
-        { step: `Bake for 10 minutes.`, section: SECTIONS.MAIN, img: cookies14 },
+        { step: `Form one inch dough balls and place on the mats.`, section: SECTIONS.MAIN, img: cookies1 },
+        { step: `Flatten the cookies with a fork to form a crisscross pattern.`, section: SECTIONS.MAIN, img: [cookies2, cookies3] },
+        { step: `Bake for 10 minutes.`, section: SECTIONS.MAIN },
         { step: `Let cookies completely cool before transferring. They will continue to set while on the baking sheet.`, section: SECTIONS.MAIN },
         { step: `Enjoy these light, delicious cookies.`, section: SECTIONS.SERVE },
     ],
