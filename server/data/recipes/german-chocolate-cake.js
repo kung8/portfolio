d4a751ff2e043db0512552ac72483be4ed4fe8f8
Kevin Ung <ung.kevin78@gmail.com>
2026-06-02 20:57:22 -0600

    reduce german chocolate cake images

--- server/data/recipes/german-chocolate-cake.js
+++ server/data/recipes/german-chocolate-cake.js
@@ -5,16 +5,6 @@ const cake4 = '../assets/Products/german-chocolate-cake-4.jpeg';
 const cake5 = '../assets/Products/german-chocolate-cake-5.jpeg';
 const cake6 = '../assets/Products/german-chocolate-cake-6.jpeg';
 const cake7 = '../assets/Products/german-chocolate-cake-7.jpeg';
-const cake8 = '../assets/Products/german-chocolate-cake-8.jpeg';
-const cake9 = '../assets/Products/german-chocolate-cake-9.jpeg';
-const cake10 = '../assets/Products/german-chocolate-cake-10.jpeg';
-const cake11 = '../assets/Products/german-chocolate-cake-11.jpeg';
-const cake12 = '../assets/Products/german-chocolate-cake-12.jpeg';
-const cake13 = '../assets/Products/german-chocolate-cake-13.jpeg';
-const cake14 = '../assets/Products/german-chocolate-cake-14.jpeg';
-const cake15 = '../assets/Products/german-chocolate-cake-15.jpeg';
-const cake16 = '../assets/Products/german-chocolate-cake-16.jpeg';
-const cake17 = '../assets/Products/german-chocolate-cake-17.jpeg';
 
 const { CATEGORIES, GENRES, INGREDIENT_UNITS, METHODS, SECTIONS, STORAGE_CONTAINER, STORAGE_DURATION_UNIT, STORAGE_LOCATION, TIME_UNITS, TYPES, YIELD_UNITS } = require('./constants');
 const { ALL_PURPOSE_FLOUR, BAKING_POWDER, BAKING_SODA, BROWN_SUGAR, BUTTERMILK, CAKE_PAN, COCOA_POWDER, COOLING_RACK, EGG, EGG_YOLK, EVAPORATED_MILK, MEDIUM_BOWL, MIXING_BOWL, OVEN, PARCHMENT_PAPER, PECANS, POWDERED_SUGAR, SALT, SAUCEPAN, SHREDDED_COCONUT, STOVE, UNSALTED_BUTTER, VANILLA_EXTRACT, VEGETABLE_OIL, WATER, WHITE_SUGAR } = require('./ingredients');
@@ -32,7 +22,7 @@ const COOK_CHOCOLATE_BUTTERCREAM_FROSTING = 'Cook Chocolate Buttercream Frosting
 module.exports = {
     cardName: 'German Chocolate Cake',
     name: 'German Chocolate Cake',
-    img: cake17,
+    img: cake7,
     available: true,
     recommended: true,
     category: [CATEGORIES.DESSERT],
@@ -96,31 +86,31 @@ module.exports = {
 
         { step: `In a mixing bowl, combine the "${CHOCOLATE_CAKE_DRY_INGREDIENTS_SECTION}" section ingredients.`, section: SECTIONS.PREP_BATTER },
         { step: `In a medium bowl, combine the "${CHOCOLATE_CAKE_WET_INGREDIENTS_SECTION}" section ingredients.`, section: SECTIONS.PREP_BATTER },
-        { step: `Add the wet ingredients to the dry ingredients. Mix.`, section: SECTIONS.PREP_BATTER, img: cake1 },
-        { step: `Slowly stir in the boiling water.`, section: SECTIONS.PREP_BATTER, img: cake2 },
-        { step: `Pour batter into baking pans.`, section: SECTIONS.PREP_BATTER, img: cake3 },
+        { step: `Add the wet ingredients to the dry ingredients. Mix.`, section: SECTIONS.PREP_BATTER },
+        { step: `Slowly stir in the boiling water.`, section: SECTIONS.PREP_BATTER },
+        { step: `Pour batter into baking pans.`, section: SECTIONS.PREP_BATTER },
 
         { step: `Bake until an inserted toothpick comes out clean (about 25 to 35 minutes).`, section: SECTIONS.BAKE },
 
-        { step: `Cool for 5 minutes in the pan.`, section: SECTIONS.COOL, img: cake9 },
+        { step: `Cool for 5 minutes in the pan.`, section: SECTIONS.COOL },
         { step: `Transfer to wire rack by just inverting it. Let it completely cool.`, section: SECTIONS.COOL },
 
-        { step: `In a saucepan, combine "${COCONUT_FROSTING_HEATED_INGREDIENTS_SECTION}" section ingredients.`, section: COOK_COCONUT_FROSTING, img: cake4 },
-        { step: `Over medium heat, bring the sauce to a low boil. Stir constantly until mixture begins to thicken.`, section: COOK_COCONUT_FROSTING, img: [cake5, cake6] },
+        { step: `In a saucepan, combine "${COCONUT_FROSTING_HEATED_INGREDIENTS_SECTION}" section ingredients.`, section: COOK_COCONUT_FROSTING },
+        { step: `Over medium heat, bring the sauce to a low boil. Stir constantly until mixture begins to thicken.`, section: COOK_COCONUT_FROSTING },
         { step: `Remove saucepan from heat.`, section: COOK_COCONUT_FROSTING },
-        { step: `Add the "${COCONUT_FROSTING_ADDITIONAL_INGREDIENTS_SECTION}" section ingredients. Stir.`, section: COOK_COCONUT_FROSTING, img: cake7 },
-        { step: `Let it completely cool.`, section: COOK_COCONUT_FROSTING, img: cake8 },
+        { step: `Add the "${COCONUT_FROSTING_ADDITIONAL_INGREDIENTS_SECTION}" section ingredients. Stir.`, section: COOK_COCONUT_FROSTING },
+        { step: `Let it completely cool.`, section: COOK_COCONUT_FROSTING, img: cake1 },
 
         { step: `In a microwavable mixing bowl, melt butter.`, section: COOK_CHOCOLATE_BUTTERCREAM_FROSTING },
-        { step: `Stir in cocoa powder.`, section: COOK_CHOCOLATE_BUTTERCREAM_FROSTING, img: cake10 },
-        { step: `Alternate adding powdered sugar and milk. Beat as added.`, section: COOK_CHOCOLATE_BUTTERCREAM_FROSTING, img: cake11 },
-        { step: `Add vanilla. Mix.`, section: COOK_CHOCOLATE_BUTTERCREAM_FROSTING, img: cake12 },
+        { step: `Stir in cocoa powder.`, section: COOK_CHOCOLATE_BUTTERCREAM_FROSTING },
+        { step: `Alternate adding powdered sugar and milk. Beat as added.`, section: COOK_CHOCOLATE_BUTTERCREAM_FROSTING },
+        { step: `Add vanilla. Mix.`, section: COOK_CHOCOLATE_BUTTERCREAM_FROSTING, img: cake2 },
 
         { step: `Place one cake layer on your serving plate.`, section: SECTIONS.ASSEMBLE },
-        { step: `Spread a thin layer of chocolate frosting.`, section: SECTIONS.ASSEMBLE, img: cake13 },
-        { step: `Spread half the coconut frosting.`, section: SECTIONS.ASSEMBLE, img: cake14 },
-        { step: `Place the second cake layer.`, section: SECTIONS.ASSEMBLE, img: cake15 },
-        { step: `Spread chocolate frosting all over.`, section: SECTIONS.ASSEMBLE, img: cake16 },
+        { step: `Spread a thin layer of chocolate frosting.`, section: SECTIONS.ASSEMBLE, img: cake3 },
+        { step: `Spread half the coconut frosting.`, section: SECTIONS.ASSEMBLE, img: cake4 },
+        { step: `Place the second cake layer.`, section: SECTIONS.ASSEMBLE, img: cake5 },
+        { step: `Spread chocolate frosting all over.`, section: SECTIONS.ASSEMBLE, img: cake6 },
         { step: `Spread the rest of the coconut frosting.`, section: SECTIONS.ASSEMBLE },
 
         { step: `Enjoy this rich dessert!`, section: SECTIONS.SERVE },
